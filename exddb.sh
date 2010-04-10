401d271d92e06ab25d4ae5b6d28603e5913bdb3a
ciax <ciax@ciax.sum.naoj.org>
2010-04-09 16:44:49 -1000

    update:exddb.sh/ test with error input by 3rd parameter "err"

--- exddb.sh
+++ exddb.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 dev=${1:-k3n} cmd=${2:-getstat}
-input="$HOME/.var/${dev}_${cmd}.bin"
+ext=${3:-bin}
+input="$HOME/.var/${dev}_${cmd}.${ext}"
 output="$HOME/.var/${dev}.mar"
 devcmd $dev $cmd|visi && 
 devstat $dev $cmd < $input > $output &&
 mar $output
-
-

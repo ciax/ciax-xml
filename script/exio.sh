698729bfece5fa37788dae43d3be5808f64204b5
ciax <ciax@ciax.sum.naoj.org>
2010-05-05 15:27:58 -1000

    update:remove object related files

--- script/exio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read i
-echo "`echo -n "$i"|visi` =>" >&2
-file=~/.var/$1_getstat.bin
-[ -e "$file" ] || exit
-(echo -n "=> ";cat "$file"|visi) >&2
-cat "$file" 
--- script/exodb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. ~/se/lib/libdb.sh cx_object
-obj=${1:-cci}
-cmd=${2:-upd}
-cls=$(lookup $obj cls) || _usage_key
-objcmd $obj $cmd || exit
-[ "$cmd" = upd ] || exit
-objstat $obj | { [ "$VER" ] && mar || stv; }
-

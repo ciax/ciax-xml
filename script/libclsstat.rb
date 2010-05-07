d5be228a884ac301cc7bf17aca995d339f689e52
ciax <ciax@ciax.sum.naoj.org>
2010-05-06 16:17:44 -1000

    update:libclsstat/ move initialize,symbol from libmodstat to libclsstat

--- script/libclsstat.rb
+++ script/libclsstat.rb
@@ -1,8 +1,18 @@
 #!/usr/bin/ruby
 require "libxmldb"
-require "libmodstat"
+require "libmodfile"
 class ClsStat < XmlDb
-  include ModStat
+  include ModFile
+  def initialize(doc)
+    super(doc,'//status')
+    begin
+      @stat=load_stat(@property['id']) || raise
+    rescue
+      warn $!
+      @stat=Hash.new
+    end
+  end
+
   attr_reader :stat,:field
 
   def clsstat(fields)
@@ -52,6 +62,35 @@ class ClsStat < XmlDb
       c.symbol(val,set)
       set.delete('id')
       @stat[c['id']]=set
+    end
+  end
+
+  def symbol(val,set)
+    node_with_name('symbol') do |d|
+      case d['type']
+      when 'min_base'
+        msg "Compare by Minimum Base for [#{val}]"
+        d.each_node do |e|
+          base=e.text
+          msg("Greater than [#{base}]?",1)
+          next if base.to_f > val.to_f
+          e.add_attr(set)
+          break
+        end
+      when 'max_base'
+        msg "Compare by Maximum Base for [#{val}]"
+        d.each_node do |e|
+          base=e.text
+          msg("Less than [#{base}]?",1)
+          next if base.to_f < val.to_f
+          e.add_attr(set)
+          break
+        end
+      else
+        d.node_with_text(val) do |e|
+          e.add_attr(set)
+        end
+      end
     end
   end
 

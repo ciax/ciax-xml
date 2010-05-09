2711f165ad5695e6e55bc0e95c663ec3ad17a89b
ciax <ciax@mario.hilo.hawaii.rr.com>
2010-05-09 00:38:19 -1000

    update:objcmd/ error if no status in mar file

--- script/objcmd.rb
+++ script/objcmd.rb
@@ -9,7 +9,8 @@ warn "Usage: objcmd [obj] [cmd]" if ARGV.size < 1
 begin
   doc=XmlDoc.new('odb',ARGV.shift)
   c=ObjCmd.new(doc).node_with_id(ARGV.shift)
-  c.set_var!(load_stat(c.property['device']))
+  field=load_stat(c.property['device']) || raise("No status in File")
+  c.set_var!(field)
   c.objcmd {}
 rescue RuntimeError
   abort $!.to_s

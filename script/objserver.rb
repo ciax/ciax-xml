6da28a2e0110509fa4d5ced70cda25b8b18a5b87
ciax <ciax@ciax.sum.naoj.org>
2010-05-11 11:01:31 -1000

    update:objserver/works well

--- script/objserver.rb
+++ script/objserver.rb
@@ -6,25 +6,31 @@ require "libiocmd"
 warn "Usage: objserver [obj] [port] [iocmd]" if ARGV.size < 2
 
 obj=ARGV.shift
-odb=Obj.new(obj)
+@odb=Obj.new(obj)
 port=ARGV.shift
 srv=IoCmd.new("socat - udp-l:#{port},reuseaddr,fork")
-ddb=Dev.new(odb.property['device'],ARGV.shift)
+@ddb=Dev.new(@odb.property['device'],ARGV.shift)
 
 line='upd'
-loop do 
-  if line=srv.rcv
-    cmd,par=line.split(' ')
-  else
-    cmd='upd'
-  end
+
+def session(line)
+  cmd,par=line.split(' ')
   begin
-    odb.objcom(cmd,par) do |c,p|
-      ddb.devcom(c,p)
+    @odb.objcom(cmd,par) do |c,p|
+      @ddb.devcom(c,p)
     end
   rescue
-    srv.snd=$!
+    $!
+  end
+  nil
+end
+
+
+loop do 
+  if line=srv.rcv
+    resp=session(line) || "#{obj}>"
+    srv.snd(resp)
   else
-    srv.snd "#{obj}>"if line
+    warn session('upd')
   end
 end

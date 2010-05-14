3de8893f57ff3ebed876dfca6b7f25e41d837a21
ciax <ciax@ciax.sum.naoj.org>
2010-05-13 14:51:23 -1000

    update:remove devcom.sh

--- script/devcom.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. ~/se/lib/libdb.sh cx_object
-obj="$1"
-iskey $obj || _usage_key
-iocmd=`lookup "$obj" iocmd` || _die "No entry in iocmd field"
-file=~/.var/$obj.bin
-echo " [$iocmd]" >&2 
-exec $iocmd | tee $file

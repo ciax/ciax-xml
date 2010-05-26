cee0232fa50353e2a95c30daa202560220ae8ac6
ciax <ciax@ciax.sum.naoj.org>
2010-05-26 11:18:48 -1000

    add:a2t.rb/convert attribute to text

---
+++ script/a2t.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+require "optparse"
+require "rexml/document"
+include REXML
+
+abort "Usage: a2t [xpath] [attr] < xml" if ARGV.size < 2
+
+xpath=ARGV.shift
+attr=ARGV.shift
+doc=Document.new(gets(nil))
+doc.each_element(xpath) {|e|
+  str=e.attributes[attr]
+  e.delete_attribute(attr)
+  e.text=str
+}
+puts doc

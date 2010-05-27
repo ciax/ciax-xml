c096f2017f4e4f14d71586ef4cdec3df8bcfc76a
ciax <ciax@ciax.sum.naoj.org>
2010-05-27 11:59:53 -1000

    update:libxmldev/ add xpath to title of verbose

--- script/libxmldev.rb
+++ script/libxmldev.rb
@@ -5,7 +5,7 @@ class XmlDev < XmlDb
   attr_reader :property
   def initialize(doc,xpath)
     id=doc.property['id']
-    super(doc,xpath,"#{doc.root.name}/#{id}".upcase)
+    super(doc,xpath,"#{doc.root.name}/#{id}/#{xpath.delete('/')}".upcase)
     @property={'id'=>id}
     @var=Hash.new
   end

387faf0c297b5121420508cf68c7ee9591a2a767
ciax <ciax@mario.hilo.hawaii.rr.com>
2010-05-31 19:27:41 -1000

    update:libdev2/add 'hexstr' to unpack string to convert hex strings to dec numbers

--- script/devcmd2.rb
+++ script/devcmd2.rb
@@ -5,7 +5,7 @@ warn "Usage: devcmd [dev] [cmd]" if ARGV.size < 1
 
 begin
   c=Dev.new(ARGV.shift,'')
-  c.devcom(ARGV.shift||'upd')
+print  c.devcom(ARGV.shift||'upd')
 rescue RuntimeError
   abort $!.to_s
 end
--- script/libdev2.rb
+++ script/libdev2.rb
@@ -31,18 +31,22 @@ class Dev
     session=@doc.select_id('//session',cmd)
     setpar(par)
     warn session.attributes['label']
+    i=0
     session.each_element {|io|
       @sel=io
       case io.name
       when 'send'
-print        sndstr=cmdframe
-#        @ic.snd(sndstr,['snd',cmd,par])
+        print sndstr=cmdframe
+#        @ic.snd(sndstr,['snd',i,cmd,par])
       when 'recv'
-#        rcvstr=@ic.rcv(['rcv',cmd])
+        rcvstr=gets(nil)
+#        rcvstr=@ic.rcv(['rcv',i,cmd])
 #        @stat['time']="%.3f" % @ic.time.to_f
-#        rspframe(rcvstr)
+        rspframe(rcvstr)
       end
+      i+=1
     }
+    @stat
   end
 
   # Rsp Methods
@@ -51,38 +55,67 @@ print        sndstr=cmdframe
     @frame=frame
     getstat(@doc.elements['//rspframe'])
     if @stat['cc']
-      @v.err('RSP:CC Mismatch') unless @stat['cc'] === @var[:ccc]
+      if @stat['cc'] === @var[:cc]
+        @v.msg("RSP:Verify:CC OK")
+      else
+        @v.err("RSP:Verifu:CC Mismatch[#{@stat['cc']}]!=[#{@var[:cc]}]") 
+      end
       @stat.delete('cc')
     end
   end
 
   def getstat(e)
-    str=String.new
+    frame=String.new
     e.each_element { |c|
       case c.name
       when 'ccrange'
         @v.msg("RSP:Entering Ceck Code Node")
-        @var[:ccc] = checkcode(c,getstat(c))
+        @var[:cc] = checkcode(c,getstat(c))
       when 'select'
         @v.msg("RSP:Entering Selected Node")
-        str << getstat(@sel)
+        frame << getstat(@sel)
+      when 'verify'
+        @v.msg("RSP:Verify:#{c.attributes['label']}[#{c.text}]")
+        frame << s=cut_frame(c)
+        @v.err("RSP:Verify Mismatch") if c.text != decode(c,s)
+      when 'cc_rsp'
+        @v.msg("RSP:Store:#{c.attributes['label']}")
+        frame << s=cut_frame(c)
+        @stat['cc']=decode(c,s)
       when 'assign'
         @v.msg("RSP:Assign:#{c.attributes['label']}")
-        str << @stat[c.text]=cut_frame(c)
-      when 'verify'
-        @v.msg("RSP:Verify:#{c.attributes['label']}")
-        str << s=cut_frame(c)
-        @v.err("RSP:Verify Mismatch") if c.text != s
+        frame << s=cut_frame(c)
+        @stat[c.text]=decode(c,s)
       when 'repeat_assign'
         min=c.attributes['min']||0
-        max=e.attributes['max']
-        @v.msg("RSP:Repeat Assign:#{c.attributes['label']}")
+        max=c.attributes['max']
         (min.to_i .. max.to_i).each { |n|
-          str << @stat[c.text % n]=cut_frame(c)
+          @v.msg("RSP:Repeat Assign:#{c.attributes['label']}(#{n})]")
+          frame << s=cut_frame(c)
+          @stat[c.text % n]=decode(c,s)
         }
+      when 'rspcode'
+        frame << s=cut_frame(c)
+        label="ResponseCode:#{c.attributes['label']}:"
+        str=decode(c,s)
+        c.each_element {|g| #Match each case
+          next if g.text && g.text != str
+          msg=label+g.attributes['msg']+" [#{str}]"
+          case g.attributes['type']
+          when 'pass'
+            @v.msg(msg)
+          when 'warn'
+            @v.wrn(msg)
+          when 'error'
+            @v.err(msg)
+          end
+          @sel=@doc.select_id('//session/recv',a) if a=e.attributes['option']
+          break
+        }
+#        @v.wrn(label+":Unknown code [#{str}]")
       end
     }
-    str
+    frame
   end
 
   def cut_frame(e)
@@ -111,46 +144,46 @@ print        sndstr=cmdframe
   end
 
   def getframe(e)
-    str=String.new
+    frame=String.new
     e.each_element { |c|
       case c.name
       when 'select'
         @v.msg("CMD:Entering Selected Node")
-        str << getframe(@sel)
+        frame << getframe(@sel)
         @v.msg("CMD:Exitting Selected Node")
       when 'data'
-        str << encode(c,c.text)
+        frame << encode(c,c.text)
       else
-        str << encode(c,@var[c.name])
+        frame << encode(c,@var[c.name])
       end
-      @v.msg("CMD:GetFrame:#{c.attributes['label']} [#{str}]")
+      @v.msg("CMD:GetFrame:#{c.attributes['label']} [#{frame}]")
     }
-    str
+    frame
   end
 
   # Common Method
-  def checkcode(e,str)
+  def checkcode(e,frame)
     chk=0
     if method=e.attributes['method']
       case method
       when 'len'
-        chk=str.length
+        chk=frame.length
       when 'bcc'
-        str.each_byte {|c| chk ^= c }
+        frame.each_byte {|c| chk ^= c }
       else
         @v.err "No such CC method #{method}"
       end
       @v.msg("Calc:CC [#{ method.upcase}] -> [#{chk}]")
-      return chk
+      return chk.to_s
     end
     @v.err "CC No method"
   end
 
-  Pack={'hex'=>'H*','chr'=>'C','bew'=>'n','lew'=>'v'}
+  Pack={'hexstr'=>'hex','hex'=>'H*','chr'=>'C','bew'=>'n','lew'=>'v'}
 
   def decode(e,code)
     if upk=Pack[e.attributes['unpack']]
-      str=code.unpack(upk).first
+      str=(upk == 'hex') ? code.hex : code.unpack(upk).first
       @v.msg("Decode:unpack(#{upk}) [#{code}] -> [#{str}]")
       code=str
     end
@@ -170,7 +203,7 @@ print        sndstr=cmdframe
   def format(e,code)
     if fmt=e.attributes['format']
       str=fmt % code
-      $ver.msg("Formatted code(#{fmt}) [#{code}] -> [#{str}]")
+      @v.msg("Formatted code(#{fmt}) [#{code}] -> [#{str}]")
       code=str
     end
     code.to_s

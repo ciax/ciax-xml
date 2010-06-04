8b97b1a9874ce4a018658466f9817f61249e13cf
ciax <ciax@ciax.sum.naoj.org>
2010-06-03 16:00:39 -1000

    update:libnumrange/ take it INF or -INF if no num, i.e. (:0) => X<=0

--- script/libnumrange.rb
+++ script/libnumrange.rb
@@ -3,22 +3,26 @@ class NumRange
   # Range format "X","X:Y","X<:Y","X:<Y","X<:<:Y"
   def initialize(str)
     @range=Hash.new
-    min,max=str.split(':')
-    if min.sub!(/<$/,'')
-      @range[:gt]=s2i(min)
+    if /:/ =~ str
+      min,max=str.split(':')
+      if /<$/ =~ min
+        @range[:gt]=s2i($`)
+      elsif min !=''
+        @range[:ge]=s2i(min)
+      end
+      if /^</ =~ max
+        @range[:lt]=s2i($')
+      elsif max
+        @range[:le]=s2i(max)
+      end
     else
-      @range[:ge]=s2i(min)
-    end
-    max=min unless max
-    if max.sub!(/^</,'')
-      @range[:lt]=s2i(max)
-    else
-      @range[:le]=s2i(max)
+      @range[:eq]=s2i(str)
     end
   end
 
   def include?(str)
     num=s2i(str)
+    return false if @range[:eq] && @range[:eq] != num
     return false if @range[:gt] && @range[:gt] >= num
     return false if @range[:ge] && @range[:ge] > num
     return false if @range[:lt] && @range[:lt] <= num

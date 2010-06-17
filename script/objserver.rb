51e6ac7882645071f326f5213c6eabf906bc73fd
ciax <ciax@ciax.sum.naoj.org>
2010-06-17 09:17:17 -1000

    update:objserver/ show stat if no input, regular update gets into another thread

--- script/objserver.rb
+++ script/objserver.rb
@@ -9,42 +9,53 @@ warn "Usage: objserver [obj]" if ARGV.size < 1
 obj=ARGV.shift
 @odb=Obj.new(obj)
 server=@odb.odb['server']
-srv=IoCmd.new(server,"server_#{obj}",0,10)
+srv=IoCmd.new(server,"server_#{obj}",0,nil)
+dev=@odb.odb['device']
+client=@odb.odb['client']
+@ddb=DevCom.new(dev,client,obj)
 warn server
 @q=Queue.new
 
 Thread.new {
-  dev=@odb.odb['device']
-  client=@odb.odb['client']
-  @ddb=DevCom.new(dev,client,obj)
   loop {
+    c,p=@q.pop
     begin
-      @odb.objcom(@q.pop) {|c,p|
-        begin
-          @ddb.setpar(p)
-          @ddb.setcmd(c)
-          @stat=@ddb.devcom
-        rescue
-        end
-      }
+      @ddb.setpar(p)
+      @ddb.setcmd(c)
+      @ddb.devcom
     rescue
-      $!.to_s+"\n"
-    else
-      "Accept\n"
+      warn $!
     end
   }
 }
 
-loop{
-  if line=srv.rcv
-    line.chomp!
-    if line == ''
-      srv.snd(@stat.to_s+"\n")
-    else
-      @q.push(line)
-    end
-    srv.snd("#{obj}>")
-  else
-    @q.push('upd')
-  end
+Thread.new {
+  loop{
+    session('upd')
+    sleep 10
+  }
 }
+
+def session(line)
+  begin
+    @odb.objcom(line) {|c,p|
+      @q.push([c,p])
+      @ddb.field
+    }
+  rescue
+    $!.to_s+"\n"
+  else
+    "Accept\n"
+  end
+end
+
+while line=srv.rcv
+  line.chomp!
+  case line
+  when ''
+    srv.snd(@odb.stat.inspect+"\n")
+  else
+    srv.snd(session(line))
+  end
+  srv.snd("#{obj}>")
+end

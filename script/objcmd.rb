bf417b2771ac08307d51d883f7f858ae22e8b302
ciax <ciax@ciax.sum.naoj.org>
2010-06-18 16:02:04 -1000

    debug:obj*/ property is gotten by Obj directly

--- script/objcmd.rb
+++ script/objcmd.rb
@@ -6,9 +6,8 @@ warn "Usage: objcmd [obj] [cmd]" if ARGV.size < 1
 
 begin
   c=Obj.new(ARGV.shift)
-  field=IoFile.new(c.odb['device']).load_stat
+  field=IoFile.new(c['device']).load_stat
   c.objcom(ARGV.shift||''){field}
 rescue RuntimeError
   abort $!.to_s
 end
-

fbbdfbc03a9cb7275896cce19f768b7e605556a7
ciax <ciax@ciax.sum.naoj.org>
2010-06-28 11:23:12 -1000

    update:libnumrange/use === intead of =~

--- script/libnumrange.rb
+++ script/libnumrange.rb
@@ -3,7 +3,7 @@ class NumRange
   include Comparable
   # Range format "X","X:Y","X<:Y","X:<Y","X<:",":Y"
   def initialize(str)
-    if /:/ =~ str
+    if /:/ === str
       min,max=str.split(':')
       if min != ''
         @min_ex=1 if min.sub!(/<$/,'')
@@ -32,9 +32,9 @@ class NumRange
   # Accepts int,float,hexstr
   def s2i(str)
     return str if str.kind_of? Numeric
-    if /^0[Xx][0-9a-fA-F]+$/ =~ str
+    if /^0[Xx][0-9a-fA-F]+$/ === str
       str.to_i(0)
-    elsif /^-?[0-9]+(\.[0-9]+)?$/ =~ str
+    elsif /^-?[0-9]+(\.[0-9]+)?$/ === str
       str.to_f
     else
       abort("Not Number [#{str}]")

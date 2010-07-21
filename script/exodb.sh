8472c9e3f2a1e8b6f7115be9868211b2ba1eb452
ciax <ciax@ciax.sum.naoj.org>
2010-07-21 06:19:16 -1000

    update:exodb/add map

--- script/exodb.sh
+++ script/exodb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ~/se/lib/libdb.sh cx_object
-for obj in ${1:-crt det dts cci mt3 mh1 cf1 mix}; do
+for obj in ${1:-crt det dts cci mt3 mh1 cf1 mix map}; do
     echo "#### $obj ####"
     dev=$(lookup $obj dev) || _usage_key
     file=$HOME/.var/field_$obj.mar

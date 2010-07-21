547e4d713f1eea6a9fbf225295ea00a82f9618d9
ciax <ciax@ciax.sum.naoj.org>
2010-07-20 16:04:52 -1000

    update:ddb-ixe,odb-mix/ read every pulses

--- script/exodb.sh
+++ script/exodb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 . ~/se/lib/libdb.sh cx_object
-for obj in ${1:-crt det dts cci mt3 mh1 cf1}; do
+for obj in ${1:-crt det dts cci mt3 mh1 cf1 mix}; do
+    echo "#### $obj ####"
     dev=$(lookup $obj dev) || _usage_key
     file=$HOME/.var/field_$obj.mar
     VER=${VER:-exec} objcmd $obj upd < $file

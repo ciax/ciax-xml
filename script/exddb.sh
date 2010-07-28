c217a3a681bb6b0b8b8e558eb25c1c274ff6a122
ciax <ciax@ciax.sum.naoj.org>
2010-07-28 09:45:32 -1000

    udpate:exddb/ using obj for log file name from which it gets data

--- script/exddb.sh
+++ script/exddb.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-var="$HOME/.var"
-dev="${1:-mel oss srm k3n bbe tpg lk8 ixe psp oma}"
+. ~/se/lib/libdb.sh cx_object
 cmd="${2:-getstat}"
-for d in $dev; do
-    input="$var/device_${d}_2010.log"
-    echo "#[$d]#"
-    devcmd $d $cmd || exit 1
-    echo
-    grep "rcv:$cmd" $input|tail -1| devstat $d $cmd|mar
-done  | visi
+for obj in ${1:-cf1 crt det dts cci mh1 mt3 mix map mma}; do
+    dev=$(lookup $obj dev) || _usage_key
+    echo "#### $dev ####"
+    input="$HOME/.var/device_${dev}_2010.log"
+    if [ $dev = slo ] ; then cmd=bs ;fi
+    str=`devcmd $dev $cmd` || exit 1
+    echo "$str" | visi
+    grep "rcv:$cmd" $input|tail -1| devstat $dev $cmd|mar
+done

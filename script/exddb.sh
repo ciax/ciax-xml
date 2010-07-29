98ad0cbb4d8e53c32fdf218a762acc8f1bd55463
ciax <ciax@ciax.sum.naoj.org>
2010-07-28 16:55:39 -1000

    update:exddb/takes command param and lookup propar line with them

--- script/exddb.sh
+++ script/exddb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 . ~/se/lib/libdb.sh cx_object
-cmd="${2:-getstat}"
-for obj in ${1:-cf1 crt det dts cci mh1 mt3 mix map mma}; do
+devices=${1:-cf1 crt det dts cci mh1 mt3 mix map mma};shift
+cmd="${*:-getstat}"
+for obj in $devices; do
     dev=$(lookup $obj dev) || _usage_key
     echo "#### $dev ####"
     input="$HOME/.var/device_${obj}_2010.log"
     if [ $dev = slo ] ; then cmd=bs ;fi
     str=`devcmd $dev $cmd` || exit 1
     echo "$str" | visi
-    grep "rcv:$cmd" $input|tail -1| devstat $dev $cmd|mar
+    grep "rcv:${cmd// /:}" $input|tail -1| devstat $dev $cmd|mar
 done

b722a192546272cb6cfcde2b6770a137df799289
ciax <ciax@ciax.sum.naoj.org>
2010-09-22 19:46:56 -1000

    add testutil:exddb_ary/ for test array field

--- script/exddb_ary.sh
+++ script/exddb_ary.sh
@@ -2,16 +2,30 @@
 rmfld(){
     rm "$HOME/.var/field_${1}.mar"
 }
-
-rmfld mix
-for i in 1 2 3 4 5 6; do
-    exddb mix chkr $i
-    exddb mix getp $i
+for obj in ${*:-mma mix crt ml1} ; do
+    rmfld $obj
+    case $obj in
+        mma)
+            for i in 1 2 3 4 5; do
+                exddb mma in $i
+            done;;
+        mix)
+            for i in 1 2 3 4 5 6; do
+                exddb mix chkr $i
+                exddb mix getp $i
+            done;;
+        crt)
+            for i in 0 1 2 3 4 5; do
+                for j in 0 1 2 3; do
+                    exddb crt get_tbl $i $j
+                done
+            done;;
+        dts)
+            for i in get ist jak inr log; do
+                exddb dts ${i}stat
+            done;;
+        *)
+            exddb $obj;;
+    esac
 done
 
-rmfld crt
-for i in 0 1 2 3 4 5; do
-    for j in 0 1 2 3; do
-        exddb crt get_tbl $i $j
-    done
-done

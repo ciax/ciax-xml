d9c9cd8dd6d963c71ec9bd0ded04311dbe2e8ac9
ciax <ciax@ciax.sum.naoj.org>
2010-10-15 16:18:40 -1000

    rename: old libvar -> lib0var, create new libvar

---
+++ script/libvar.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/ruby
+# XML Common Method
+require 'librerange'
+require 'libverbose'
+class Var < Hash
+  attr_accessor :stat
+
+  def initialize
+    @v=Verbose.new
+    @stat={}
+  end
+
+  def repeat(e)
+    a=e.attributes
+    fmt=a['format'] || '%d'
+    counter=a['counter'] || '_'
+    counter.next! while self[counter]
+    @v.msg(1){"Repeat:Counter[\$#{counter}]/Range[#{a['from']}-#{a['to']}]/Format[#{fmt}]"}
+    begin
+      Range.new(a['from'],a['to']).each { |n|
+        self[counter]=fmt % n
+        e.each_element { |d| yield d}
+      }
+      self.delete(counter)
+    ensure
+      @v.msg(-1){"Repeat:Close"}
+    end
+  end
+
+  def setpar(par)
+    par.each_with_index{|s,n| self[(n+1).to_s]=s }
+  end
+
+  def sub_var(str)
+    return str unless /\$/ === str
+    @v.msg(1){"Substitute from [#{str}]"}
+    begin
+      # Sub $key => self[key]
+      str=str.gsub(/\$([\w]+)/){ self[$1] }
+      # Sub ${key1:key2:idx} => hash[key1][key2][idx]
+      # output csv if array
+      str=str.gsub(/\$\{(.+)\}/) {
+        [*acc_array($1,@stat)].join(',')
+      }
+      raise if str == ''
+      str
+    ensure
+      @v.msg(-1){"Substitute to [#{str}]"}
+    end
+  end
+
+  def acc_stat(key)
+    acc_array(key,@stat)
+  end
+
+  def acc_array(key,h)
+    return h unless key
+    key.split(':').each {|i|
+      @v.msg{"Var:Type[#{h.class}] Name[#{i}]"}
+      i=eval(i) if Array === h
+      h=h[i]||raise("No such Value [#{i}]")
+    }
+    h
+  end
+end

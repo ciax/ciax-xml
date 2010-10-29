b11f9668de1cb77b94b808fa82ab39f6d2472cee
ciax <ciax@ciax.sum.naoj.org>
2010-10-29 11:41:46 -1000

    update:liblssrv/ make thread for class and no more use device thread

--- script/libclssrv.rb
+++ script/libclssrv.rb
@@ -2,7 +2,7 @@
 require "libxmldoc"
 require "libclscmd"
 require "libclsstat"
-require "libdevbg"
+require "libdev"
 require "thread"
 require "libclsauto"
 require "libclsevent"
@@ -11,16 +11,19 @@ class ClsSrv
 
   def initialize(cls,id,iocmd)
     @cls=cls
+    @q=Queue.new
+    @issue=''
+    @errmsg=[]
     cdb=XmlDoc.new('cdb',cls)
     @cmd=ClsCmd.new(cdb)
     @stat=ClsStat.new(cdb,id)
-    @ddb=DevBg.new(cdb['device'],id,iocmd){|s| @stat.get_stat(s) }
-    @errmsg=@ddb.errmsg
+    @ddb=Dev.new(cdb['device'],id,iocmd)
     @stat.get_stat(@ddb.field)
     @input=proc{|c| yield c}
     @output=proc{|s| @ddb.push(s) if @ddb.empty? }
     @auto=ClsAuto.new(@input,@output)
     @event=ClsEvent.new(cdb)
+    session_thread
     event_thread unless @event.empty?
     sleep 0.01
   end
@@ -28,7 +31,7 @@ class ClsSrv
   def prompt
     prom = @auto.auto.alive? ? '&' : ''
     prom << @cls
-    prom << @ddb.issue
+    prom << @issue
     prom << (@event.active? ? '!' : '')
     prom << ">"
   end
@@ -41,21 +44,40 @@ class ClsSrv
     return @errmsg.shift unless @errmsg.empty?
     return if stm.empty?
     return "Blocking" if @event.blocking?(stm)
-    @cmd.session(@input.call(stm)) {|c| @ddb.push(c)}
+    @cmd.session(@input.call(stm)){}
+    @q.push(stm)
     "Accepted"
   rescue SelectID
     @auto.auto_upd(stm){|i,o| @cmd.session(i,&o) }
   end
 
   private
+  def session_thread
+    Thread.new{
+      loop{
+        stm=@q.pop
+        @issue='*'
+        begin
+          @cmd.session(@input.call(stm)) {|c|
+            @ddb.devcom(c)
+            @stat.get_stat(@ddb.field)
+          }
+        rescue
+          @errmsg << $!.to_s
+        ensure
+          @issue=''
+        end
+      }
+    }
+  end
 
   def event_thread
     Thread.new{
       loop{
         @event.update{|k| @stat.stat(k)}
         @event.cmd('execution').each{|cmd|
-          @cmd.session(cmd.split(" ")){|c| @ddb.push(c)}
-        } if @ddb.empty?
+          @q.push(cmd.split(" "))
+        } if @q.empty?
         sleep @event.interval
       }
     }

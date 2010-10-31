d3be7a821377785c878ea6a052c1f81f823b2d86
ciax <ciax@ciax.sum.naoj.org>
2010-10-30 22:11:52 -1000

    update: add devsim/ for simulate from log; devsh/ add -d option for using devsim

--- script/devsh.sh
+++ script/devsh.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
+[ "$1" = "-d" ] && { dmy=1;shift; }
 id="$1"
-setfld -s $id || _usage_key
+setfld -s $id || _usage_key "(-d)"
 [ "$iodst" ] || _die "No entry in iodst field"
 echo " [$iodst]" >&2
-devshell $dev $id "socat - $iodst"
+echo " [$id]" >&2
+if [ "$dmy" ] ; then
+    devshell $dev dmy-$id "devsim $id"
+else
+    devshell $dev $id "socat - $iodst"
+fi
---
+++ script/devsim.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+abort "Usage: devsim [obj]" if ARGV.size < 1
+obj=ARGV.shift
+ARGV.clear
+open(ENV['HOME']+"/.var/device_#{obj}_2010.log"){|fd|
+  loop{
+    select([STDIN])
+    input=STDIN.sysread(1024)
+    while line=fd.gets
+      cl=line.split("\t")
+      if eval(cl[2]) == input
+        nl=fd.gets.split("\t")
+        if /rcv/ === nl[1]
+          sleep nl[0].to_i-cl[0].to_i
+          STDOUT.syswrite(eval(nl[2]))
+        end
+        break 1
+      end
+    end || fd.rewind
+  }
+}

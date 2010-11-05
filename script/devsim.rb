127d2ccc4d1e7f6b88b06d8bc90c605652e2f344
ciax <ciax@ciax.sum.naoj.org>
2010-11-05 11:24:45 -1000

    update:devsim/ accept Interrupt,EOFError

--- script/devsim.rb
+++ script/devsim.rb
@@ -2,20 +2,24 @@
 abort "Usage: devsim [obj]" if ARGV.size < 1
 obj=ARGV.shift
 ARGV.clear
-open(ENV['HOME']+"/.var/device_#{obj}_2010.log"){|fd|
-  loop{
-    select([STDIN])
-    input=STDIN.sysread(1024)
-    while line=fd.gets
-      cl=line.split("\t")
-      if eval(cl[2]) == input
-        nl=fd.gets.split("\t")
-        if /rcv/ === nl[1]
-          sleep nl[0].to_i-cl[0].to_i
-          STDOUT.syswrite(eval(nl[2]))
+begin
+  open(ENV['HOME']+"/.var/device_#{obj}_2010.log"){|fd|
+    loop{
+      select([STDIN])
+      input=STDIN.sysread(1024)
+      while line=fd.gets
+        cl=line.split("\t")
+        if eval(cl[2]) == input
+          nl=fd.gets.split("\t")
+          if /rcv/ === nl[1]
+            sleep nl[0].to_i-cl[0].to_i
+            STDOUT.syswrite(eval(nl[2]))
+          end
+          break 1
         end
-        break 1
-      end
-    end || fd.rewind
+      end || fd.rewind
+    }
   }
-}
+rescue Exception
+  exit
+end

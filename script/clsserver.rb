6e4156b25f25b4b8759268dbe55644939a0d2fd1
ciax <ciax@ciax.sum.naoj.org>
2010-11-17 15:52:12 -1000

    add:clsserver,clssv

---
+++ script/clsserver.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+require "libcls"
+require "socket"
+
+warn "Usage: clsserver [cls] [id] [port] [iocmd]" if ARGV.size < 1
+
+cls=ARGV.shift
+id=ARGV.shift
+port=ARGV.shift
+iocmd=ARGV.shift
+cdb=Cls.new(cls,id,iocmd)
+UDPSocket.open{ |udp|
+  udp.bind("0.0.0.0",port)
+  loop {
+    msg=''
+    begin
+      select([udp])
+      line,addr=udp.recvfrom(1024)
+      case line.chomp!
+      when ''
+        msg=cdb.stat.inspect+"\n"
+      when /stop/
+        msg=cdb.interrupt+"\n"
+      else
+        line.split(';').each{|stm|
+          cdb.dispatch(stm.split(' ')){|s|s}
+        }
+      end
+    rescue RuntimeError
+      msg=$!.to_s
+    end
+    udp.send(msg+cdb.prompt,0,addr[2],addr[1])
+  }
+}
---
+++ script/clssv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+. ~/lib/libcsv.sh
+cmd="$1";shift
+[ "$1" = "-d" ] && { dmy=1;shift; }
+id="$1"
+setfld -s $id || _usage_key "[start|stop] (-d)"
+[ "$iodst" ] || _die "No entry in iodst field"
+echo "Listen port [udp:$port]" >&2
+echo "Connect to [$iodst]" >&2
+if [ "$dmy" ] ; then
+    iocmd="devsim $id"
+    id="dmy-$id"
+else
+    iocmd="socat - $iodst"
+fi
+case $cmd  in
+    start) opt=-r ;;
+    stop) opt=-k ;;
+    *) exit ;;
+esac
+d $opt clsserver $cls $id $port "$iocmd"
+

3d3af66f7236dc998142caa302ea74de3203a290
ciax <ciax@ciax.sum.naoj.org>
2010-11-22 10:02:33 -1000

    update:a2t/ add reverse mode

--- script/a2t.rb
+++ script/a2t.rb
@@ -3,14 +3,24 @@ require "optparse"
 require "rexml/document"
 include REXML
 
-abort "Usage: a2t [xpath] [attr] < xml" if ARGV.size < 2
+abort "Usage: a2t (-r) [xpath] [attr] < xml" if ARGV.size < 2
 
 xpath=ARGV.shift
-attr=ARGV.shift
-doc=Document.new(gets(nil))
-doc.each_element(xpath) {|e|
-  str=e.attributes[attr] || next
-  e.delete_attribute(attr)
-  e.text=str
-}
+if /-r/ === xpath
+  xpath=ARGV.shift
+  attr=ARGV.shift || abort("No attr")
+  doc=Document.new(gets(nil))
+  doc.each_element(xpath) {|e|
+    e.add_attribute(attr,e.text)
+    e.text=nil
+  }
+else
+  attr=ARGV.shift
+  doc=Document.new(gets(nil))
+  doc.each_element(xpath) {|e|
+    str=e.attributes[attr] || next
+    e.delete_attribute(attr)
+    e.text=str
+  }
+end
 puts doc

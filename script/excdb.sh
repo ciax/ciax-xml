1c9c707fefa6410073861595f1322d30be750ac7
CIAX <devnull@localhost>
2011-02-18 09:29:06 -1000

    update:ex?db/ using symconv

--- script/excdb.sh
+++ script/excdb.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
-[ "$1" = '-v' ] && { visi=1; shift; }
+[ "$1" = '-s' ] && { sym=1; shift; }
 devices=${1:-`ls ~/.var/field_???.json|cut -d_ -f2|cut -d. -f1`};shift
 cmd=${1:-upd};shift
 par="$*"
 for id in $devices; do
-    setfld $id || _usage_key
+    setfld $id || _usage_key "(-s)"
     echo "#### $cls($id) ####"
     file=$HOME/.var/field_$id.json
     VER=${VER:-exec(cdb)} clscmd $cls $cmd $par < $file
     [ $cmd = 'upd' ] || continue
     echo " *** Status ***"
-    if [ "$visi" ] ; then
-        clsstat $cls < $file | stview
+    if [ "$sym" ] ; then
+        clsstat $cls < $file | symconv
     else
         clsstat $cls < $file
         echo
--- script/exddb.sh
+++ script/exddb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
 [ "$1" = "-r" ] && { shift; clear=1; }
-[ "$1" = "-v" ] && { shift; view=1; }
+[ "$1" = "-s" ] && { shift; sym=1; }
 devices=${1:-`ls ~/.var/device_???_*|cut -d_ -f2`};shift
 default="${*:-getstat}"
 for id in $devices; do
-    setfld $id || _usage_key
+    setfld $id || _usage_key "(-r|-s)"
     echo "#### $dev($id) ####"
     input="$HOME/.var/device_${id}_*.log"
     output="$HOME/.var/field_${id}.json"
@@ -15,12 +15,11 @@ for id in $devices; do
     stat="`grep rcv:${cmd// /:} $input|tail -1`"
     if [ "$stat" ] ; then
         echo " *** Stat ***"
-        if [ "$view" ] ; then
-            echo "$stat" | devstat $dev $id | stview
+        if [ "$sym" ] ; then
+            echo "$stat" | devstat $dev $id | symconv
         else
             echo "$stat" | devstat $dev $id
             echo
         fi
     fi
-    read -n 1
 done

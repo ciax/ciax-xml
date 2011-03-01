64e423f9bf5574d34b92beb4d0838855f01e808e
CIAX <devnull@localhost>
2011-03-01 09:19:16 -1000

    update:a2t/ using libxml

--- script/a2t.rb
+++ script/a2t.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 require "optparse"
-require "rexml/document"
-include REXML
+require "xml"
+include XML
 
 if ARGV.size < 2
   abort <<EOF
@@ -9,22 +9,19 @@ Usage: a2t (-r) [xpath] [attr] < xml
        //xpath@attr <-> //xpath.text()
 EOF
 end
+opt= ARGV[0] == '-r' ? ARGV.shift : nil
 xpath=ARGV.shift
-if /-r/ === xpath
-  xpath=ARGV.shift
-  attr=ARGV.shift || abort("No attr")
-  doc=Document.new(gets(nil))
-  doc.each_element(xpath) {|e|
-    e.add_attribute(attr,e.text)
-    e.text=nil
-  }
-else
-  attr=ARGV.shift
-  doc=Document.new(gets(nil))
-  doc.each_element(xpath) {|e|
-    str=e.attributes[attr] || next
-    e.delete_attribute(attr)
-    e.text=str
-  }
-end
+attr=ARGV.shift || abort("No attr")
+doc=Document.io(STDIN)
+ns=doc.root.namespaces.namespace.to_s
+  doc.find("//ns:#{xpath}","ns:#{ns}").each {|e|
+  if opt == '-r'
+    e[attr]=e.content
+    e.content=''
+  else
+    str=e[attr] || next
+    e.content=str
+    e.attributes.get_attribute(attr).remove!
+  end
+}
 puts doc

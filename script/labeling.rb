ece062098c2e6f7c41e9176b4d51300d70c4fcb8
CIAX <devnull@localhost>
2011-02-28 16:36:23 -1000

    update: add labeling for status data

---
+++ script/labeling.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+require "json"
+require "libxmldoc"
+require "liblabel"
+
+#abort "Usage: labering < file" if ARGV.size < 1
+
+stat=JSON.load(gets(nil))
+if type=stat['frame']
+  fdb=XmlDoc.new('fdb',type)
+  dv=Label.new(fdb,'rspframe','field')
+elsif type=stat['class']
+  cdb=XmlDoc.new('cdb',type)
+  dv=Label.new(cdb,'status')
+end
+puts JSON.dump(dv.merge(stat))
---
+++ script/liblabel.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+require "librepeat"
+
+class Label
+  attr_reader :label
+  def initialize(db,domain,xpath=nil)
+    @label={}
+    if xpath
+      db.find_each(domain,xpath){|e|
+        sym=e['label'] || next
+        @label[e['id']]=sym
+      }
+    else
+      rep=Repeat.new
+      rep.each(db[domain]){|e|
+        sym=e['label'] || next
+        @label[rep.subst(e['id'])]=rep.subst(sym)
+      }
+    end
+  end
+
+  def merge(stat)
+    @label.each{|id,label|
+      case stat[id]
+      when Hash
+        stat[id]['label']=@label[id]
+      else
+        stat[id]={'label'=>@label[id],'val'=>stat[id]}
+      end
+    }
+    stat
+  end
+end

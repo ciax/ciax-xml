c436554ce384c34142d1671f9ed74ce8210da69b
CIAX <devnull@localhost>
2011-04-14 10:35:44 -1000

    update:libclsstat/ xml will be deploied to hash in initialize; update format:cdb//value/float[text()] -> @formula

--- script/libclsstat.rb
+++ script/libclsstat.rb
@@ -10,38 +10,54 @@ class ClsStat
     @stat.update({'time' => Time.now.to_s,'class' => cls })
     @v=Verbose.new("cdb/#{cls}/stat".upcase)
     @rep=Repeat.new
-    set_stat{''}
+    @list=init_stat
   end
   
   public
   def get_stat
-    set_stat{|e0| get_val(e0)}
+    @list.each{|line|
+      begin
+        @v.msg(1){"STAT:GetStatus:[#{line['id']}]"}
+        get_val(line)
+      ensure
+        @v.msg(-1){"STAT:GetStatus:#{line['id']}=[#{@stat[line['id']]}]"}
+      end
+    }
     @stat['time']=Time.at(@field['time'].to_f).to_s
     self
   end
   
   private
-  def set_stat
+  def init_stat
+    list=[]
     @rep.each(@doc['status']){|e0|
-      begin
-        id=@rep.format(e0['id'])
-        @v.msg(1){"STAT:GetStatus:[#{id}]"}
-        @stat[id]=yield e0
-      ensure
-        @v.msg(-1){"STAT:GetStatus:#{id}=[#{@stat[id]}]"}
-      end
+      line={ :val => [] }
+      e0.to_h.each{|k,v|
+        line[k]=@rep.format(v)
+      }
+      e0.each{|e1|
+        st={:type => e1.name}
+        e1.to_h.each{|k,v|
+          st[k] = @rep.subst(v)
+        }
+        line[:val] << st
+      }
+      @stat[line['id']]=''
+      @v.msg{"STAT:Init #{line}"}
+      list << line
     }
-    self
+    list
   end
 
   def get_val(e0)
+    id=e0['id']
     str=''
-    e0.each{|e1| #element(split and concat)
-      fld=@rep.subst(e1['ref']) || raise("No field Key")
+    e0[:val].each{|e1| #element(split and concat)
+      fld=e1['ref'] || raise("No field Key")
       data=@field.get(fld) || raise("No field Value[#{fld}]")
-      case e1.name
+      case e1[:type]
       when 'binary'
-        loc=eval(@rep.subst(e1['bit']))
+        loc=eval(e1['bit'])
         bit=(data.to_i >> loc & 1)
         bit = -(bit-1) if /true|1/ === e1['inv']
         @v.msg{"GetBit[#{bit}]"}
@@ -58,8 +74,8 @@ class ClsStat
         end
         data=data.to_f
         data= -data if sign
-        if e1.text
-          f=e1.text.gsub(/\$#/,data.to_s)
+        if e1['formula']
+          f=e1['formula'].gsub(/\$#/,data.to_s)
           data=eval(f)
           @v.msg{"Formula:#{f}(#{data})"}
         end
@@ -76,6 +92,6 @@ class ClsStat
         str << data
       end
     }
-    str
+    @stat[id]=str
   end
 end

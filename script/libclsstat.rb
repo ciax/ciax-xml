df0eb647b7db6d8a538316a7734ef9a4da24b71b
CIAX <devnull@localhost>
2011-04-15 10:00:05 -1000

    refactoring:libclsstat

--- script/libclsstat.rb
+++ script/libclsstat.rb
@@ -57,41 +57,53 @@ class ClsStat
       data=@field.get(fld) || raise("No field Value[#{fld}]")
       case e1[:type]
       when 'binary'
-        loc=eval(e1['bit'])
-        bit=(data.to_i >> loc & 1)
-        bit = -(bit-1) if /true|1/ === e1['inv']
-        @v.msg{"GetBit[#{bit}]"}
-        str << bit.to_s
+        str << binary(e1,data)
       when 'float'
-        sign=nil
-        if /true|1/ === e1['signed']
-          sign=(data[0] == "8")
-          data=data[1..-1]
-        end
-        if n=e1['decimal']
-          n=n.to_i
-          data=data[0..(-1-n)]+'.'+data[-n..-1]
-        end
-        data=data.to_f
-        data= -data if sign
-        if e1['formula']
-          f=e1['formula'].gsub(/\$#/,data.to_s)
-          data=eval(f)
-          @v.msg{"Formula:#{f}(#{data})"}
-        end
-        fmt=e1['format'] || "%f"
-        str << fmt % data
+        str << float(e1,data)
       when 'int'
-        data=data.to_i
-        if /true|1/ === e1['signed']
-          data= data > 0x7fff ? data - 0x10000 : data
-        end
-        fmt=e1['format'] || "%d"
-        str << fmt % data
+        str << int(e1,data)
       else
         str << data
       end
     }
     @stat[id]=str
+  end
+
+  def binary(e1,data)
+    loc=eval(e1['bit'])
+    bit=(data.to_i >> loc & 1)
+    bit = -(bit-1) if /true|1/ === e1['inv']
+    @v.msg{"GetBit[#{bit}]"}
+    bit.to_s
+  end
+
+  def float(e1,data)
+    sign=nil
+    if /true|1/ === e1['signed']
+      sign=(data[0] == "8")
+      data=data[1..-1]
+    end
+    if n=e1['decimal']
+      n=n.to_i
+      data=data[0..(-1-n)]+'.'+data[-n..-1]
+    end
+    data=data.to_f
+    data= -data if sign
+    if e1['formula']
+      f=e1['formula'].gsub(/\$#/,data.to_s)
+      data=eval(f)
+      @v.msg{"Formula:#{f}(#{data})"}
+    end
+    fmt=e1['format'] || "%f"
+    fmt % data
+  end
+
+  def int(e1,data)
+    data=data.to_i
+    if /true|1/ === e1['signed']
+      data= data > 0x7fff ? data - 0x10000 : data
+    end
+    fmt=e1['format'] || "%d"
+    fmt % data
   end
 end

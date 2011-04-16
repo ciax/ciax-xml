2cbb268faa8ed4dd82d05718976ba057ff1db5f9
CIAX <devnull@localhost>
2011-04-15 14:15:24 -1000

    update:libclscms/ refactoring

--- script/libclscmd.rb
+++ script/libclscmd.rb
@@ -8,38 +8,57 @@ class ClsCmd
 
   def initialize(doc)
     raise "Init Param must be XmlDoc" unless XmlDoc === doc
-    @doc=doc
     @v=Verbose.new("doc/#{doc['id']}/cmd".upcase)
-    @rep=Repeat.new
     @par=Param.new
-    @session=init_cmd
+    @session=init_cmd(doc)
   end
 
   def setcmd(ssn)
-    @cid=ssn.first
-    @sel=@session[@cid] || list_cmd
+    @sel=@session[ssn.first] || list_cmd
     @par.setpar(@sel,ssn)
     self
   end
 
   def statements
     @v.msg{"Exec(CDB):#{@sel['label']}"}
-    get_cmd(@sel)
+    stma=[]
+    @sel['statements'].each{|e1|
+      stm=[]
+      @v.msg(1){"GetCmd(DDB):#{e1.first}"}
+      begin
+        e1.each{|e2| # //argv
+          case e2
+          when String
+            stm << e2
+          when Hash
+            str=@par.subst(e2['val'])
+            str=e2['format'] % eval(str) if e2['format']
+            @v.msg{"Calculated [#{str}]"}
+            stm << str
+          end
+        }
+        stma << stm
+      ensure
+        @v.msg(-1){"Exec(DDB):#{stm}"}
+      end
+    }
+    stma
   end
 
   private
-  def init_cmd
+  def init_cmd(doc)
     session={}
-    @doc['commands'].each{|e0|
+    rep=Repeat.new
+    doc['commands'].each{|e0|
       id=e0['id']
       session[id]={'label'=>e0['label'],'statements'=>[]}
-      @rep.each(e0){|e1|
+      rep.each(e0){|e1|
         case e1.name
         when 'statement'
           command=[e1['command']]
           e1.each{|e2|
             argv=e2.to_h
-            argv['val'] = @rep.subst(e2.text)
+            argv['val'] = rep.subst(e2.text)
             command << argv.freeze
           }
           session[id]['statements'] << command.freeze
@@ -63,31 +82,5 @@ class ClsCmd
       err << (" %-10s: %s" % [key,val['label']]) if val['label']
     }
     raise SelectID,err.join("\n")
-  end
-
-  #Cmd Method
-  def get_cmd(e0) # //session
-    stma=[]
-    e0['statements'].each{|e1|
-      stm=[]
-      @v.msg(1){"GetCmd(DDB):#{e1.first}"}
-      begin
-        e1.each{|e2| # //argv
-          case e2
-          when String
-            stm << e2
-          when Hash
-            str=@par.subst(e2['val'])
-            str=e2['format'] % eval(str) if e2['format']
-            @v.msg{"Calculated [#{str}]"}
-            stm << str
-          end
-        }
-        stma << stm
-      ensure
-        @v.msg(-1){"Exec(DDB):#{stm}"}
-      end
-    }
-    stma
   end
 end

ad9ad54b559fae2211f070471697c149aa7d48ff
CIAX <devnull@localhost>
2011-04-20 09:16:42 -1000

    update:exfdb/ split to exfdbc, exfdbs

---
+++ script/exfdbc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+. ~/lib/libcsv.sh
+devices=${1:-`ls ~/.var/device_???_*|cut -d_ -f2`};shift
+cmd="${*:-getstat}"
+for id in $devices; do
+    setfld $id || _usage_key "(-r|-s)"
+    echo "#### $dev($id) ####"
+    frmcmd $dev $id $cmd | visi
+done
--- script/exfdbs.sh
+++ script/exfdbs.sh
@@ -5,14 +5,11 @@
 [ "$1" = "-l" ] && { shift; label=1; }
 [ "$1" = "-p" ] && { shift; print=1; }
 devices=${1:-`ls ~/.var/device_???_*|cut -d_ -f2`};shift
-default="${*:-getstat}"
 for id in $devices; do
     setfld $id || _usage_key "(-r|-s)"
     echo "#### $dev($id) ####"
     input="$HOME/.var/device_${id}_*.log"
     output="$HOME/.var/field_${id}.json"
-    cmd=$default
-    { frmcmd $dev $id $cmd || exit 1; } | visi
     [ "$clear" ] && [ -e $output ] && rm $output
     stat="`grep rcv:${cmd// /:} $input|tail -1`"
     if [ "$stat" ] ; then

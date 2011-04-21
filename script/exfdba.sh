0b10bc0acd801d1e2864a0834433fbbac1df4e7f
CIAX <devnull@localhost>
2011-04-20 17:48:23 -1000

    update:exfdba/ rename exfdb_ary -> exfdba, use only exfdbs to get array status

---
+++ script/exfdba.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+for obj in ${*:-mma mix crt ml1 dts} ; do
+    exfdbc $obj getstat
+    case $obj in
+        mma)
+            for i in 1 2 3 4 5; do
+                exfdbs mma in $i
+            done;;
+        mix)
+            for i in 1 2 3 4 5 6; do
+                exfdbs mix chkrun $i
+                exfdbs mix getp $i
+                exfdbs mix getspd $i
+                exfdbs mix getofs $i
+            done;;
+        crt)
+            for i in 0 1 2 3 4 5; do
+                for j in 0 1 2 3; do
+                    exfdbs crt get_tbl $i $j
+                done
+            done;;
+        dts)
+            for i in get ist jak inr log; do
+                exfdbs dts ${i}stat
+            done;;
+        ds*)
+            exfdbs $obj getzerr
+            exfdbs $obj getzlen
+            ;;
+        *)
+            exfdbs $obj;;
+    esac
+    read -n 1
+done
+

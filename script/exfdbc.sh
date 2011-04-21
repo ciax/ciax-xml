ad9f890f50ea14d2dac91f2d45b3da3d6fe52f41
CIAX <devnull@localhost>
2011-04-20 14:33:40 -1000

    update:exfdbc/ test all commands

--- script/exfdbc.sh
+++ script/exfdbc.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
 devices=${1:-`ls ~/.var/device_???_*|cut -d_ -f2`};shift
-cmd="${*:-getstat}"
 for id in $devices; do
     setfld $id || _usage_key
     echo "#### $dev($id) ####"
-    frmcmd $dev $id $cmd | visi
+    if [ "$1" ] ; then
+        frmcmd $dev $id $* | visi
+    else
+        frmcmd $dev $id 2>&1 |grep : | while read line; do
+            cmd=${line%:*}
+            echo -n "   $cmd ";frmcmd $dev $id $cmd 1 0| visi
+        done
+    fi
 done

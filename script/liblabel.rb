f94fb73663a67110a1fe20248478336fea6a4770
CIAX <devnull@localhost>
2011-04-29 09:48:38 -1000

    update:liblabel/ add id and time for label

--- script/liblabel.rb
+++ script/liblabel.rb
@@ -4,7 +4,8 @@ require "librepeat"
 class Label
   attr_reader :label
   def initialize(db,domain,key,xpath=nil)
-    @label={}
+    @label={'id' => {'label' => 'OBJECT','group' => '0'},
+      'time' => {'label' => 'TIMESTAMP','group' => '0' }}
     if xpath
       db.find_each(domain,xpath){|e|
         sym=e['label'] || next

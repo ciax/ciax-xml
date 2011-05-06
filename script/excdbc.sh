3aa3f1dfb638c7cf8d2340721dacca1793f2edb0
CIAX <devnull@localhost>
2011-05-06 06:40:47 -1000

    update:excdbc/ colored

--- script/excdbc.sh
+++ script/excdbc.sh
@@ -2,14 +2,14 @@
 . ~/lib/libcsv.sh
 devices=${1:-`lookup all cls`};shift
 for cls in $devices; do
-    echo "#### $cls ####"
+    echo "$C2#### $cls ####$C0"
     if [ "$1" ] ; then
         clscmd $cls $*
     else
-        clscmd $cls 2>&1 |grep : | while read line; do
+        clscmd $cls 2>&1 |grep " : "| while read line; do
             cmd=${line%:*}
-            echo " ** $cmd **"
-            clscmd $cls $cmd 1 0
+            echo "$C3$cmd$C0"
+            clscmd $cls $cmd 1 0 || break
         done
     fi
     read

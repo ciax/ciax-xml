e786ff8771c9f9aeb8c7161eea9f050bb4c2d249
CIAX <devnull@localhost>
2011-05-09 11:28:58 -1000

    debug:exfdbs/ get param for limit the picked up line

--- script/exfdbs.sh
+++ script/exfdbs.sh
@@ -4,25 +4,31 @@
 [ "$1" = "-s" ] && { shift; sym=1; }
 [ "$1" = "-l" ] && { shift; label=1; }
 [ "$1" = "-p" ] && { shift; print=1; }
+
+getstat(){
+    cmd=$1
+    stat="`egrep \"rcv:$cmd\" $input|tail -1`"
+    if [ "$stat" ] ; then
+        echo "process for $cmd"
+        echo "$stat" | frmstat $dev $id
+    fi
+}
+
 devices=${1:-`ls ~/.var/device_???_*|cut -d_ -f2`};shift
-cmd=$*
+par=$*
 for id in $devices; do
     setfld $id || _usage_key "(-r|-s)"
     echo "$C2#### $dev($id) ####$C0"
     input="$HOME/.var/device_${id}_*.log"
     output="$HOME/.var/field_${id}.json"
     [ "$clear" ] && [ -e $output ] && rm $output
-    if [ "$cmd" ] ; then
-        echo ${cmd// /:}
+    if [ "$par" ] ; then
+        getstat ${par// /:}
     else
-        frmcmd $dev $id 2>&1 |grep " : "
-    fi | while read cmd dmy; do
-        stat="`egrep 'rcv:$cmd[ :]' $input|tail -1`"
-        if [ "$stat" ] ; then
-            echo "process for $cmd ($stat)"
-            echo "$stat" | frmstat $dev $id
-        fi
-    done
+        frmcmd $dev $id 2>&1 |grep ' : '|while read cmd dmy; do
+            getstat $cmd
+        done
+    fi
     if [ "$print" ] ; then
         < $output symboling | labeling | stprint
     elif [ "$label" ] ; then

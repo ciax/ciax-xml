c9b87b6da73793fa496be4c7bc600add8c05996d
CIAX <devnull@localhost>
2011-05-17 12:43:27 -1000

    udpate:libalias/ debug

--- script/libalias.rb
+++ script/libalias.rb
@@ -11,12 +11,13 @@ class Alias
   public
   def alias(ssn)
     raise unless Array === ssn
+    @v.msg{"Command:#{ssn}"}
     return ssn unless @odb
     id=ssn.first
-    if ref=@odb[:ref][id] && ! ssn.empty?
+    if (ref=@odb[:ref][id]) && ! ssn.empty?
       @v.msg{"Before:#{ssn}(#{@odb[:label][id]})"}
-      ssn[1..-1].unshift(ref)
-      @v.msg{"After:#{ssn}"}
+      ssn=ssn[1..-1].unshift(ref)
+      @v.msg{"After:#{ref}/#{ssn}"}
     else
       @v.list(@odb[:label],"=== Command List ===")
     end

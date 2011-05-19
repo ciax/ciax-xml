3622d4c4719071690ebbec40f83e926a9a0d5c26
CIAX <devnull@localhost>
2011-05-19 09:50:42 -1000

    update:exfdbs/ using logline

--- script/exfdbs.sh
+++ script/exfdbs.sh
@@ -6,16 +6,13 @@
 [ "$1" = "-p" ] && { shift; print=1; }
 
 getstat(){
-    cmd=$1
-    stat="`egrep \"rcv:$cmd\" $input|tail -1`"
-    if [ "$stat" ] ; then
-        echo "${C3}process for $cmd$C0"
-        echo "$stat" | frmstat $dev | merging $output
-    fi
+    cmd="$*"
+    echo "${C3}process for $cmd$C0"
+    logline $id $cmd | frmstat $dev | merging $output
 }
 
 devices=${1:-`ls ~/.var/device_???_*|cut -d_ -f2`};shift
-par=$*
+par="$*"
 for id in $devices; do
     setfld $id || _usage_key "(-rslp)"
     echo "$C2#### $dev($id) ####$C0"
@@ -23,7 +20,7 @@ for id in $devices; do
     output="$HOME/.var/field_${id}.json"
     [ "$clear" ] && [ -e $output ] && rm $output
     if [ "$par" ] ; then
-        getstat ${par// /:}
+        getstat $par
     else
         frmcmd $dev $id 2>&1 |grep ' : '|while read cmd dmy; do
             getstat $cmd

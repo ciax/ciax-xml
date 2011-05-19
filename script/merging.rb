6821f7f995850a8c2d7b63d43bc0d711c27bb634
CIAX <devnull@localhost>
2011-05-19 08:51:43 -1000

    update:merging/ file creation, deep merging

--- script/merging.rb
+++ script/merging.rb
@@ -1,16 +1,36 @@
 #!/usr/bin/ruby
 require "json"
 
+def merge(a,b)
+  case a
+  when Hash
+    b||={}
+    a.each_key{|k|
+      b[k]=merge(a[k],b[k])
+    }
+  when Array
+    b||=[]
+    a.each_index{|i|
+      b[i]=merge(a[i],b[i])
+    }
+  else
+    b=a
+  end
+  b
+end
+
 abort "Usage: merging [orgfile] < input\n#{$!}" if STDIN.tty? && ARGV.size < 1
 
 file=ARGV.shift
-h={}
+output={}
 open(file){|f|
-  h=JSON.load(f.gets(nil))
-}
+  output=JSON.load(f.gets(nil))
+} if test(?r,file)
 str=STDIN.gets(nil) || exit
 input=JSON.load(str)
-h.update(input)
+output=merge(input,output)
 open(file,'w'){|f|
-  f.puts(JSON.dump(h))
+  f.puts(JSON.dump(output))
 }
+
+

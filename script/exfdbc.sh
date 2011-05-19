3d692f0eea01b3e5cefe642db1c7bd289119c6ca
CIAX <devnull@localhost>
2011-05-19 12:25:41 -1000

    update:frmcmd/ no need id, but need status input instead

--- script/exfdbc.sh
+++ script/exfdbc.sh
@@ -4,11 +4,12 @@ devices=${1:-`ls ~/.var/device_???_*|cut -d_ -f2`};shift
 for id in $devices; do
     setfld $id || _usage_key
     echo "$C2#### $dev($id) ####$C0"
+    input="$HOME/.var/field_$id.json"
     if [ "$1" ] ; then
-        frmcmd $dev $id $* | visi
+        frmcmd $dev $* | visi
     else
-        frmcmd $dev $id 2>&1 |grep " : "| while read cmd dmy; do
-            echo -n "   $C3$cmd$C0 ";frmcmd $dev $id $cmd 1 0| visi
+        frmcmd $dev 2>&1 |grep " : "| while read cmd dmy; do
+            echo -n "   $C3$cmd$C0 ";frmcmd $dev $cmd 1 0 < $input| visi
         done
     fi
 done
--- script/frmcmd.rb
+++ script/frmcmd.rb
@@ -1,17 +1,20 @@
 #!/usr/bin/ruby
+require "json"
 require "libstat"
 require "libfrmcmd"
 require "libfrmdb"
 
 dev=ARGV.shift
-id=ARGV.shift
 cmd=ARGV
 begin
   fdb=FrmDb.new(dev)
-  st=Stat.new(id,"field")
+  st=Stat.new
   c=FrmCmd.new(fdb,st)
   c.setcmd(cmd)
+  if ! STDIN.tty? && str=STDIN.gets(nil)
+    st.update(JSON.load(str))
+  end
   print c.getframe
 rescue UserError
-  abort "Usage: frmcmd [dev] [id] [cmd] (par)\n#{$!}"
+  abort "Usage: frmcmd [dev] [cmd] (par) < statfile \n#{$!}"
 end

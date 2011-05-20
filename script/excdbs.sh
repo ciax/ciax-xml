acac31314c59f71536e354ffbb9067672d957e62
CIAX <devnull@localhost>
2011-05-19 17:03:02 -1000

    udpate:excdbs/ using viewing

--- script/excdbs.sh
+++ script/excdbs.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
-[ "$1" = '-s' ] && { sym=1; shift; }
-[ "$1" = '-l' ] && { label=1; shift; }
-[ "$1" = '-r' ] && { reverse=1; shift; }
-[ "$1" = '-p' ] && { print=1; shift; }
+[[ "$1" == -* ]] && { opt=$1; shift; }
 devices=${1:-`ls ~/.var/field_???.json|cut -d_ -f2|cut -d. -f1`};shift
 par="$*"
 for id in $devices; do
-    setfld $id || _usage_key "(-slrp)"
+    setfld $id || _usage_key "(-lgs)"
     echo "$C2#### $cls($id) ####$C0"
     file=$HOME/.var/field_$id.json
-    if [ "$print" ] ; then
-        clsstat $cls < $file | symboling $obj | labeling $obj | grouping $obj | stprint
-    elif [ "$reverse" ] ; then
-        clsstat $cls < $file | labeling $obj | symboling $obj | v2s
-    elif [ "$label" ] ; then
-        clsstat $cls < $file | labeling $obj | grouping $obj| v2s
-    elif [ "$sym" ] ; then
-        clsstat $cls < $file | symboling $obj | v2s
-    else
-        clsstat $cls < $file | h2s
-    fi
+    clsstat $cls < $file | viewing $opt $obj | stprint
 done

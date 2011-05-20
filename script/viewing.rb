9cd0643f1f8091ed0db92ec24ed81870d7442ac3
CIAX <devnull@localhost>
2011-05-19 16:49:35 -1000

    add:viewing/ replace of labeling,grouping,symboling

---
+++ script/viewing.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/ruby
+require "json"
+require "libview"
+require "liblabel"
+require "libgroup"
+require "libsym"
+require "libsymdb"
+
+abort "Usage: viewing (-lgs) (obj) < [file]" if STDIN.tty?
+
+obj=ARGV.shift
+opt='lgs'
+if (/^-/ === obj)
+  opt=obj.delete('-')
+  obj=ARGV.shift
+end
+str=STDIN.gets(nil) || exit
+view=View.new(JSON.load(str))
+if type=view['frame']
+  require "libfrmdb"
+  db=FrmDb.new(type)
+elsif type=view['class']
+  require "libobjdb"
+  db=ObjDb.new(obj,type)
+else
+  raise "NO ID in View"
+end
+opt.split('').each{|s|
+  case s
+  when 'l'
+    label=Label.new(db.status[:label])
+    view.convert('label',label)
+  when 'g'
+    group=Group.new(db.status[:group])
+    view.convert('group',group)
+  when 's'
+    sdb=SymDb.new
+    sdb.update(db.symtbl)
+    sym=Sym.new(sdb,db.status[:symbol])
+    view=sym.convert(view)
+  end
+}
+puts JSON.dump(view)

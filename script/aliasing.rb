8a0bc29224abb6e204e3bbe0a62b0afb2134dbfe
CIAX <devnull@localhost>
2011-05-20 17:27:56 -1000

    update:aliasing/ show list and exit 1 if obj exists and wrong command

--- script/aliasing.rb
+++ script/aliasing.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/ruby
 require "libalias"
 
-abort "Usage: aliasing [obj] [cmd] (par)\n#{$!}" if ARGV.size < 2
+abort "Usage: aliasing [obj] [cmd] (par)\n#{$!}" if ARGV.size < 1
 obj=ARGV.shift
 cmd=ARGV.dup
-odb=Alias.new(obj)
-puts odb.alias(cmd)
+begin
+  odb=Alias.new(obj)
+  puts odb.alias(cmd)
+rescue SelectID
+  abort $!.to_s
+end

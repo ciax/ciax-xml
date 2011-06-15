4e2d2ea6c548322889e0582e16054a6651c0dfa7
CIAX <devnull@localhost>
2011-06-15 13:34:10 -1000

    update:liblabel/ add "CLASS ID", "FRAME ID" at init

--- script/liblabel.rb
+++ script/liblabel.rb
@@ -3,6 +3,8 @@ class Label < Hash
   def initialize(hash)
     raise "Label have to be given Db" unless hash.kind_of?(Db)
     self['time'] = 'TIMESTAMP'
+    self['class'] = 'CLASS ID'
+    self['frame'] = 'FLAME ID'
     update(hash.status[:label]||{})
   end
 

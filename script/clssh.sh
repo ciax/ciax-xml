ea81c97b00b76268a99bef87ebfd4bffb42dcb7c
CIAX <devnull@localhost>
2011-06-16 14:49:11 -1000

    update:cls,frmsh.sh/ make viewing option effective

--- script/clssh.sh
+++ script/clssh.sh
@@ -4,7 +4,7 @@
 [[ "$1" == -* ]] && { opt=$1;shift; }
 
 id="$1"
-setfld $id || _usage_key "(-dcp)"
+setfld $id || _usage_key "(-dcplgs)"
 [ "$iodst" ] || _die "No entry in iodst field"
 echo " [$iodst]" >&2
 iocmd="socat - $iodst"
@@ -12,7 +12,7 @@ output="viewing $obj|stprint"
 case "$opt" in
     -d) iocmd="frmsim $id";id="dmy-$id";;
     -c) output="ascpck $id";;
-    -r) output='';;
+    -*) output="viewing $opt $obj|v2s";;
     *);;
 esac
 clsshell $cls $id "$iocmd" "$output"
--- script/frmsh.sh
+++ script/frmsh.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
-[ "$1" = "-d" ] && { dmy=1;shift; }
-[ "$1" = "-s" ] && { output="symboling";shift; }
-[ "$1" = "-l" ] && { output="symboling|labeling";shift; }
-[ "$1" = "-p" ] && { output="symboling|labeling|stprint";shift; }
+[[ "$1" == -* ]] && { opt=$1;shift; }
 id="$1"
-setfld $id || _usage_key "(-d)"
+setfld $id || _usage_key "(-dlgs)"
 [ "$iodst" ] || _die "No entry in iodst field"
 echo " [$iodst]" >&2
-if [ "$dmy" ] ; then
-    fh="$HOME/.var/field_"
-    iocmd="frmsim $id"
-    org=$id
-    id="dmy-$id"
-    cp $fh$org.json $fh$id.json
-else
-    iocmd="socat - $iodst"
-fi
-frmshell $cls $id "$iocmd" "$output"
+iocmd="socat - $iodst"
+output="h2s"
+case "$opt" in
+    -d)
+        fh="$HOME/.var/field_"
+        iocmd="frmsim $id"
+        org=$id
+        id="dmy-$id"
+        cp $fh$org.json $fh$id.json
+        ;;
+    -*) output="viewing $opt $obj|v2s"
+        ;;
+    *) ;;
+esac
+frmshell $dev $id "$iocmd" "$output"

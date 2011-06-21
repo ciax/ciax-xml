4ebbc6e26c0841ada9648eb0fdf4b4b778d92af7
CIAX <devnull@localhost>
2011-06-20 18:10:42 -1000

    update:clsshell/ filter of other process -> ruby classes

--- script/clssh.sh
+++ script/clssh.sh
@@ -8,11 +8,8 @@ setfld $id || _usage_key "(-dcplgs)"
 [ "$iodst" ] || _die "No entry in iodst field"
 echo " [$iodst]" >&2
 iocmd="socat - $iodst"
-output="viewing $obj|stprint"
 case "$opt" in
     -d) iocmd="frmsim $id";id="dmy-$id";;
-    -c) output="ascpck $id";;
-    -*) output="viewing $opt $obj|v2s";;
     *);;
 esac
-clsshell $cls $id "$iocmd" "$output"
+clsshell $opt $cls $id "$iocmd"
--- script/clsshell.rb
+++ script/clsshell.rb
@@ -2,19 +2,49 @@
 require "json"
 require "libcls"
 require "libfrm"
+require "libobjdb"
 require "libclsdb"
 require "libfrmdb"
 require "libalias"
 require "libfilter"
+require "libview"
 require "libshell"
+require "libprint"
+
+def filters(opt,odb)
+  filters=[]
+  opt.split('').each{|s|
+    case s
+    when 'l'
+      require "liblabel"
+      filters << Label.new(odb)
+    when 'g'
+      require "libgroup"
+      filters << Group.new(odb)
+    when 's'
+      require "libsymdb"
+      require "libsym"
+      sym=SymDb.new
+      sym.update(odb.symtbl)
+      filters << Sym.new(sym,odb)
+    end
+  }
+  filters
+end
 
 cls=ARGV.shift
+opt='lgs'
+if (/^-/ === cls)
+  opt=cls.delete('-')
+  cls=ARGV.shift
+end
 id=ARGV.shift
 iocmd=ARGV.shift
 filter=ARGV.shift
 al=Alias.new(id)
 out=Filter.new(filter)
 begin
+  odb=ObjDb.new(id,cls)
   cdb=ClsDb.new(cls)
   fdb=FrmDb.new(cdb['frame'])
   fobj=Frm.new(fdb,id,iocmd)
@@ -22,13 +52,17 @@ begin
     fobj.request(stm)
     fobj.stat
   }
+  view=View.new(cobj.stat)
+  pr=Print.new
+  fi=filters(opt,odb)
 rescue SelectID
-  abort "Usage: clsshell [cls] [id] [iocmd] (outcmd)\n#{$!}"
+  abort "Usage: clsshell (-lgs) [cls] [id] [iocmd]\n#{$!}"
 end
 inf=proc{|cmd|
   cobj.dispatch(al.alias(cmd))
 }
 outf=proc{|stat|
-  out.filter(JSON.dump(stat))
+  fi.each{|f| f.convert(view)}
+  opt=='lgs' ? pr.print(view) : view
 }
 Shell.new(cobj,inf,outf)

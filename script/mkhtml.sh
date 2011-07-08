77d086b389e5476d7cc1580b0986f6eaa34ec310
CIAX <devnull@localhost>
2011-07-07 16:10:32 -1000

    update:mkhtml/ use Ajax to update data

--- script/mkhtml.sh
+++ script/mkhtml.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
-cat <<EOF
+id=$1;shift
+setfld $id || _usage_key
+file=$HOME/.var/json/first.html
+cat > $file <<EOF
 <html>
 <head>
 <title>CIAX-XML</title>
 <link rel="stylesheet" type="text/css" href="/ciax-style.css" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script type="text/javascript">
+function update(){
+  jQuery.ajax({
+    url : "status_$obj.json",
+    dataType : 'json',
+    cache : false,
+    success : function(json){
+      for (var h in json){
+        jQuery("#"+h).text(json[h]);
+      }
+    }
+  })
+}
+jQuery(document).ready(setInterval(update,3000));
+</script>
 </head>
 <body>
 EOF
-htmltbl $*
-cat <<EOF
+>>$file htmltbl $obj $cls
+cat >> $file <<EOF
 </body>
 </html>
 EOF

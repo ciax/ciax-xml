ef1cdab37652e608852f668eab4fa8be1efa4bbe
CIAX <devnull@localhost>
2011-08-02 15:25:27 -1000

    update:symconv/ apply range symbol conversion

--- script/symconv.js
+++ script/symconv.js
@@ -1,3 +1,45 @@
+function Range(str){
+    var eq,max,min,min_ex,max_ex;
+    if(/:/.test(str)){
+        var a=str.split(':');
+        min=a[0];max=a[1];
+        if(min){
+            var m=min.replace(/<$/,'');
+            if(m != min)
+                min_ex=1;
+            min=Number(m);
+        }
+        if(max){
+            var m=max.replace(/^</,'')
+            if(m != max)
+                max_ex=1;
+            max=Number(m);
+        }
+    }else{
+        eq=Number(str);
+    }
+    this.cmp = function(num){
+        if(eq){
+            if(eq > num){
+                return 1;
+            }else if(eq < num){
+                return -1;
+            }else{
+                return 0;
+            }
+        }else if(min_ex && min >= num){
+            return 1;
+        }else if(min && min > num){
+            return 1;
+        }else if(max_ex && max <= num){
+            return -1;
+        }else if(max && max < num){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+}
 function convert(table,val){
     var view;
     var tbl=table.record;
@@ -10,6 +52,15 @@ function convert(table,val){
         for(var key in tbl){
             view=tbl[key];
             if(RegExp(key).test(val))
+                break;
+        }
+        break;
+    case 'range':
+        for(var range in tbl){
+            var v=tbl[range];
+            view={msg:v.msg+'('+val+')',class:v.class};
+            var re=new Range(range);
+            if(re.cmp(val) == 0)
                 break;
         }
         break;

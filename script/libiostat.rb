7daff9b87c0f7f48653c6753607dc15bfe67a883
CIAX <devnull@localhost>
2011-08-16 11:34:55 -1000

    update:libiostat/ get save,load function from iofile

--- script/libiostat.rb
+++ script/libiostat.rb
@@ -1,36 +1,70 @@
 #!/usr/bin/ruby
 require 'libverbose'
 require 'libstat'
-require 'libiofile'
 
 class IoStat < Stat
-  def initialize(id,fname)
+  VarDir="#{ENV['HOME']}/.var"
+  def initialize(id,type)
     @v=Verbose.new("stat",5)
     raise SelectID," No ID" unless id
-    fname+="_#{id}"
+    @type=type+"_#{id}"
     begin
-      @fd=IoFile.new(fname)
       load
     rescue
-      @v.warn("----- No #{fname}.json")
+      @v.warn("----- No #{@type}.json")
     end
   end
 
   def load(tag=nil)
-    update(@fd.load_stat(tag))
+    base=[@type,tag].compact.join('_')
+    @v.msg{"Status Loading for [#{base}]"}
+    fname=VarDir+"/#{base}.json"
+    raise SelectID,list_stat unless !tag || FileTest.exist?(fname)
+    stat=JSON.load(IO.read(fname))
+    raise "No status in File" unless stat
+    update(stat)
+  end
+
+  def save(tag=nil,keylist=nil)
+    base=[@type,tag].compact.join('_')
+    fname=VarDir+"/#{base}.json"
+    if keylist
+      json=JSON.dump(pick(keylist))
+    else
+      json=to_j
+    end
+    open(fname,'w') {|f|
+      @v.msg{"Status Saving for [#{base}]"}
+      f << json
+    }
+    mklink(fname,tag)
     self
   end
 
-  def save(tag=nil,keys=nil)
-    if keys
-      stat={}
-      keys.each{|k|
-        stat[k]=self[k] if key?(k)
-      }
-      @fd.save_stat(stat,tag)
-    else
-      @fd.save_stat(to_h)
-    end
-    self
+  private
+  def pick(ary)
+    stat={}
+    ary.each{|k|
+      stat[k]=self[k] if key?(k)
+    }
+    stat
+  end
+
+  def mklink(fname,tag)
+    return unless tag
+    sbase=[@type,'latest'].compact.join('_')
+    sname=VarDir+"/#{sbase}.json"
+    File.unlink(sname) if File.exist?(sname)
+    File.symlink(fname,sname)
+    @v.msg{"Symboliclink to [#{sbase}]"}
+  end
+
+  def list_stat
+    list=[]
+    Dir.glob(VarDir+"/#{@type}_*.json"){|f|
+      tag=f.slice(/#{@type}_(.+)\.json/,1)
+      list << tag
+    }
+    "Tag=#{list}"
   end
 end

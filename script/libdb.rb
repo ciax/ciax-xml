235f9e1c047fb19e90b5c4e765b91c1687f34ca2
CIAX <devnull@localhost>
2011-08-18 10:12:34 -1000

    update:libdb/ add cover() to merge recursively

--- script/libdb.rb
+++ script/libdb.rb
@@ -14,4 +14,15 @@ class Db < Hash
   def to_s
     Verbose.view_struct(self)
   end
+
+  def cover(hash) # override with hash
+    replace(rec_merge(self,hash))
+  end
+
+  private
+  def rec_merge(me,oth)
+    me.merge(oth){|k,s,h|
+      Hash === h ? rec_merge(s,h) : s
+    }
+  end
 end

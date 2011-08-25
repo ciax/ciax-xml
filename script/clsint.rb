d691f567a1b413d62d70180b7e69d17cf6788367
CIAX <devnull@localhost>
2011-08-24 14:01:07 -1000

    update:clsint/ using optparse

--- script/clsint.rb
+++ script/clsint.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+require "optparse"
 require "json"
 require "libobjdb"
 require "libclsdb"
@@ -13,9 +14,12 @@ require "libiocmd"
 require "libiostat"
 require "libinteract"
 
-opt,arg = ARGV.partition{|s| /^-/ === s}
-optstr=opt.join('')
-obj,iocmd=arg
+opt={}
+OptionParser.new{|op|
+  op.on('s'){|v| opt[:s]=v}
+  op.parse!(ARGV)
+}
+obj,iocmd=ARGV
 
 begin
   odb=ObjDb.new(obj)
@@ -28,7 +32,7 @@ end
 stat=IoStat.new(obj,'json/status')
 field=IoStat.new(obj,'field')
 
-io=IoCmd.new(odb['client'],obj,fdb['wait'],1)
+io=IoCmd.new(iocmd.empty? ? odb['client'] : iocmd,obj,fdb['wait'],1)
 fobj=FrmObj.new(fdb,field,io)
 
 cobj=ClsObj.new(odb,stat,field){|cmd|
@@ -39,7 +43,7 @@ al=Alias.new(odb)
 view=View.new(stat).opt('als',odb[:status])
 prt=Print.new(view)
 
-port=optstr.include?('s') ? odb["port"] : nil
+port=opt[:s] ? odb["port"] : nil
 
 Interact.new(port||cobj.prompt){|line|
   cobj.dispatch(line){|cmd| al.alias(cmd)}||\

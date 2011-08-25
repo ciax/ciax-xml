bfba802a85a75dbaf5e435cae18d63bb62f10d97
CIAX <devnull@localhost>
2011-08-25 08:24:13 -1000

    update:frmint/ take iocmd,port from xml; frmsh/gives only obj to frmint

--- script/frmint.rb
+++ script/frmint.rb
@@ -1,20 +1,31 @@
 #!/usr/bin/ruby
-require "libiostat"
-require "libiocmd"
+require "optparse"
+require "libobjdb"
+require "libclsdb"
 require "libfrmdb"
 require "libfrmobj"
+require "libiocmd"
+require "libiostat"
 require "libinteract"
 
-dev,id,iocmd,port=ARGV
+opt={}
+OptionParser.new{|op|
+  op.on('-s'){|v| opt[:s]=v}
+  op.parse!(ARGV)
+}
+obj,iocmd=ARGV
 begin
-  fdb=FrmDb.new(dev)
-  field=IoStat.new(id,'field')
-  io=IoCmd.new(iocmd,id,fdb['wait'],1)
+  odb=ObjDb.new(obj)
+  odb >> ClsDb.new(odb['class'])
+  fdb=FrmDb.new(odb['frame'])
+  field=IoStat.new(obj,'field')
+  io=IoCmd.new(iocmd||odb['client'],obj,fdb['wait'],1)
   fobj=FrmObj.new(fdb,field,io)
 rescue SelectID
-  abort "Usage: frmshell [dev] [id] [iocmd] (port)\n#{$!}"
+  abort "Usage: frmint (-s) [obj] (iocmd)\n#{$!}"
 end
-Interact.new(port||[dev,'>']){|line|
+port=opt[:s] ? odb["port"] : nil
+Interact.new([odb['frame'],'>'],port){|line|
   (line && fobj.request(line.split(' ')))||\
   (port ? field.to_j : field)
 }
--- script/frmsh.sh
+++ script/frmsh.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
-[[ "$1" == -* ]] && { opt=$1;shift; }
-id="$1"
-setfld $id || _usage_key "(-dlgs)"
-[ "$iodst" ] || _die "No entry in iodst field"
-echo " [$iodst]" >&2
-iocmd="socat - $iodst"
-case "$opt" in
-    -d)
-        fh="$HOME/.var/field_"
-        iocmd="frmsim $id"
-        org=$id
-        id="dmy-$id"
-        cp $fh$org.json $fh$id.json
-        ;;
-    *) ;;
-esac
-frmint $dev $id "$iocmd"
+
+while getopts "d" opt; do
+    case $opt in
+        d) export NOLOG=1;dmy=1;;
+        *);;
+    esac
+done
+shift $(( $OPTIND -1 ))
+if [ "$dmy" ] ; then
+    VER=iocmd:client frmint $1 "frmsim $1"
+else
+    VER=iocmd:client frmint $1
+fi

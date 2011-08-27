01e7afeedcb6b554dde58db02d2fa679c67f4593
CIAX <devnull@localhost>
2011-08-26 14:20:38 -1000

    debug:exfdba/ remove opt(-) from exfdbs

--- script/exfdba.sh
+++ script/exfdba.sh
@@ -4,36 +4,35 @@ for obj in ${*:-mma mmc mix crt ml1 dts} ; do
     case $obj in
         mma)
             for i in 1 2 3 4 5; do
-                exfdbs - mma in $i
+                exfdbs mma in $i
             done;;
         mmc)
             for i in 1 2 3 4 5; do
-                exfdbs - mmc in $i
+                exfdbs mmc in $i
             done;;
         mix)
             for i in 1 2 3 4 5 6; do
-                exfdbs - mix chkrun $i
-                exfdbs - mix getp $i
-                exfdbs - mix getspd $i
-                exfdbs - mix getofs $i
+                exfdbs mix chkrun $i
+                exfdbs mix getp $i
+                exfdbs mix getspd $i
+                exfdbs mix getofs $i
             done;;
         crt)
             for i in 0 1 2 3 4 5; do
                 for j in 0 1 2 3; do
-                    exfdbs - crt get_tbl $i $j
+                    exfdbs crt get_tbl $i $j
                 done
             done;;
         dts)
             for i in get ist jak inr log; do
-                exfdbs - dts ${i}stat
+                exfdbs dts ${i}stat
             done;;
         ds*)
-            exfdbs - $obj getzerr
-            exfdbs - $obj getzlen
+            exfdbs $obj getzerr
+            exfdbs $obj getzlen
             ;;
         *)
-            exfdbs - $obj;;
+            exfdbs $obj;;
     esac
     read -t 0 && break
 done
-

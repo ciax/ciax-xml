90f1ed46c9689ae1e3356061a4579d63aeaf80b7
CIAX <devnull@localhost>
2011-08-30 11:04:43 -1000

    update:exwdb/ takes id

--- script/exwdb.sh
+++ script/exwdb.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
-set
-for i in $HOME/.var/json/status_*.json; do
+n=${1:-*}
+file=$HOME/.var/json/status_$n.json
+for i in $file ; do
     _msg `basename $i`
     watches < $i
     read -t 0 && break

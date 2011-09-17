32970a9869ed762ea5777f17d141be44e6e27c30
CIAX <devnull@localhost>
2011-09-16 14:42:57 -1000

    update:exwdb/ accept test value injection

--- script/exwdb.sh
+++ script/exwdb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
-n=${1:-*}
+n=${1:-*};shift
 file=$HOME/.var/json/status_$n.json
 for i in $file ; do
     _msg `basename $i`
-    watches < $i
+    watches $* < $i
     read -t 0 && break
 done

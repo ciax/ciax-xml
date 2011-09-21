e8df060a867c9ddb696e3d6d065586d6c342fab1
CIAX <devnull@localhost>
2011-09-21 10:44:01 -1000

    update:ciax-xml.js/ format of elapse -> HH:MM:SS; function member of object should be attached "()"

--- webapp/ciax-xml.js
+++ webapp/ciax-xml.js
@@ -7,10 +7,6 @@ function update(){
             var stat=view.stat
             for (var id in stat){
                 var val=stat[id];
-                if(id == "elapse"){
-                    var d=new Date()-new Date(stat.time);
-                    val=Math.round(d/1000+0.5);
-                }
                 if(view.symbol && view.symbol[id]){
                     var hash=view.symbol[id];
                     jQuery("#"+id).addClass(hash.class);
@@ -18,6 +14,9 @@ function update(){
                 }
                 jQuery("#"+id).text(val);
             }
+            var d=new Date(new Date()-new Date(stat.time));
+            str=""+d.toUTCString().match(/[0-9]{2}:[0-9]{2}:[0-9]{2}/);
+            jQuery("#elapse").text(str);
         }
     })
 }

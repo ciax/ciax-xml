823206a1a6ce31680d6c9d6809ac46aabff55f2c
CIAX <devnull@localhost>
2011-09-21 20:10:24 -1000

    update:ciax-xml.js/ update and period get to be independent

--- webapp/ciax-xml.js
+++ webapp/ciax-xml.js
@@ -1,12 +1,16 @@
-function period(ms){
+function period(){
+    var time=jQuery("#time").text();
+    var ms=new Date()-new Date(time);
     var t=new Date(ms);
+    var str;
     if (ms > 86400000){
-        return Math.floor(ms/8640000)/10+" days";
+        str=Math.floor(ms/8640000)/10+" days";
     }else if(ms > 3600000){
-        return t.getHours()+"h "+t.getMinutes()+'m';
+        str=t.getHours()+"h "+t.getMinutes()+'m';
     }else{
-        return t.getMinutes()+"'"+t.getSeconds()+'"';
+        str=t.getMinutes()+"'"+t.getSeconds()+'"';
     }
+    jQuery("#elapse").text(str);
 }
 function update(){
     jQuery.ajax({
@@ -24,9 +28,8 @@ function update(){
                 }
                 jQuery("#"+id).text(val);
             }
-            var ms=new Date()-new Date(stat.time);
-            jQuery("#elapse").text(period(ms));
         }
     })
 }
-jQuery(document).ready(setInterval(update,1000));
+jQuery(document).ready(setInterval(update,10000));
+jQuery(document).ready(setInterval(period,1000));

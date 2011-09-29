34e3ec08525e4bd8e50e4f6c3d2e281d3c9c5996
CIAX <devnull@localhost>
2011-09-28 23:41:58 -1000

    update:libinsdb/ includes fdb by cover_frm()

--- script/libinsdb.rb
+++ script/libinsdb.rb
@@ -37,6 +37,13 @@ class InsDb < Hash
     self
   end
 
+  def cover_frm(nocache=nil)
+    require "libfrmdb"
+    frm=FrmDb.new(self['frm_type'],nocache)
+    replace(rec_merge(frm,self))
+    self
+  end
+
   private
   def rec_merge(i,o)
     i.merge(o){|k,a,b|
@@ -47,13 +54,14 @@ end
 
 if __FILE__ == $0
   begin
-    opt=ARGV.getopts("c")
+    opt=ARGV.getopts("af")
     id=ARGV.shift
     idb=InsDb.new(id,true)
   rescue
-    warn "USAGE: #{$0} (-c) [id] (key) .."
+    warn "USAGE: #{$0} (-af) [id] (key) .."
     Msg.exit
   end
-  idb.cover_app(true) if opt["c"]
+  idb.cover_app(true) if opt["a"]
+  idb.cover_frm(true) if opt["f"]
   puts idb.path(ARGV)
 end

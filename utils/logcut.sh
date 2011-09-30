6d88a109a788cfac0051449cda19429ea20bb75f
CIAX <devnull@localhost>
2011-09-29 16:15:13 -1000

    update:logcut/ no use "-" option, [date to date] or [from date] or [all]

--- utils/logcut.sh
+++ utils/logcut.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-[ "$1" ] || { echo "Usage: logcut [id] [date_from|-] (date_to)"; exit 1; }
+[ "$1" ] || { echo "Usage: logcut [id] (date_from) (date_to)"; exit 1; }
 files="$HOME/.var/device_$1_*.log";shift
-if [ "$1" = '-' ] ; then
+if [ "$1" ] ; then
+    from=$(date -d "$1" +%s) || exit
+    cond="\$1 > $from"
     shift
     if [ "$1" ] ; then
-        st=$(date -d "$1" +%s)|| exit
-    else
-        st=$(date +%s)
-    fi
-    awk "\$1 < $st && /rcv:/ { print }" $files|tail -1
-else
-    st=$(date -d "$1" +%s) || exit
-    cond="\$1 > $st"
-    shift
-    if [ "$1" ] ; then
-        st=$(date -d "$1" +%s)|| exit
-        cond="$cond && \$1 < $st"
+        to=$(date -d "$1" +%s)|| exit
+        cond="$cond && \$1 < $to"
     fi
     awk "$cond && /rcv:/ { print }" $files
+else
+    grep "rcv:" $files
 fi

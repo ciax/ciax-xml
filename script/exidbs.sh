74416c077354bb6ef5db82992105274d1328e2c5
CIAX <devnull@localhost>
2011-10-10 12:54:41 -1000

    update:libwview/ add test module; add exidbs/ support Wview

---
+++ script/exidbs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+. ~/lib/libcsv.sh
+[[ "$1" == -* ]] && { opt=$1; shift; }
+[ "$opt" ] && rm ~/.var/json/status*
+ids=${1:-`ls ~/.var/json/field_???.json|cut -d_ -f2|cut -d. -f1`};shift
+par="$*"
+for id in $ids; do
+    aline=`~/lib/libinsdb.rb $id|tr -d '"'|grep app` || continue
+    app=${aline#*:}
+    echo "$C2#### $app($id) ####$C0"
+    file=$HOME/.var/json/field_$id.json
+    stat=$HOME/.var/json/view_$id.json
+    ~/lib/libwview.rb $app < $file > $stat
+    if [ "$opt" ]
+    then
+        v2s <$stat
+    else
+        ~/lib/libprint.rb < $stat
+    fi
+    read -t 0 && break
+done
--- script/libwview.rb
+++ script/libwview.rb
@@ -19,3 +19,21 @@ class Wview < Rview
     self
   end
 end
+
+if __FILE__ == $0
+  require "libappdb"
+  require "libfield"
+  require "libappstat"
+  app=ARGV.shift
+  ARGV.clear
+  begin
+    adb=AppDb.new(app,true)
+    str=gets(nil) || exit
+    field=Field.new.update_j(str)
+    as=AppStat.new(adb,field).upd
+    view=Wview.new(field['id'],adb,as)
+    print view.upd.to_j
+  rescue UserError
+    abort "Usage: #{$0} [app] < field_file\n#{$!}"
+  end
+end

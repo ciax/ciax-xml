b741147bb4bfd98137a7b3b3055f254ad26c0c4d
CIAX <devnull@localhost>
2011-10-16 01:33:26 -1000

    update:client/ using libinsdb and add opt -f(frm server), -h(hex server)

--- script/client.sh
+++ script/client.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 . ~/lib/libcsv.sh
+while getopts "fh" opt; do
+    case $opt in
+        f) offset="-1000";;
+        h) offset="+1000";;
+        *);;
+    esac
+done
+shift $(( $OPTIND -1 ))
 id="$1"
-setfld $id || _usage_key
+host="${2:-localhost}"
+port=`~/lib/libinsdb.rb $id|grep port` || { echo "(-fh) [id] [host]"; exit; }
+port=${port//\"/}
+port="$(( ${port#*:} $offset ))"
 echo "Connect to $host:$port" >&2
 socat READLINE udp:$host:$port

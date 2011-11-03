6d44120f70ca37ee222f478fd0e86b29192dc2c2
CIAX <devnull@localhost>
2011-11-03 10:24:54 -1000

    update:v2s/ use JSON and Msg::view_struct() instead of ModExh

--- utils/v2s.rb
+++ utils/v2s.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/ruby
-require "libmodexh"
+require "libmsg"
+require "json"
 
 abort "Usage: v2s json_file" if STDIN.tty? && ARGV.size < 1
 
 str=gets(nil) || exit
-puts Hash.new.extend(ModExh).update_j(str)
+puts Msg.view_struct(JSON.load(str))

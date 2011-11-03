0a80379e9531c6a355c37f7052bfb64e8c908a7c
CIAX <devnull@localhost>
2011-11-02 17:22:57 -1000

    add:hexsv.sh

---
+++ script/hexsv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while getopts "k" opt; do
+    case $opt in
+        k) psg -k hexint;;
+        *);;
+    esac
+done
+shift $(( $OPTIND -1 ))
+[ "$1" ] || psg hexint
+for id; do
+    appsv $id
+    d -r -t $id hexint -s $id
+done

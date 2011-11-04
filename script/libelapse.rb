c3f0a431aad18a5d352750c5ff77f37dbd6f8c85
CIAX <devnull@localhost>
2011-11-03 23:25:31 -1000

    udpate:libelapse/ show sub milli socond in to_s

--- script/libelapse.rb
+++ script/libelapse.rb
@@ -6,13 +6,15 @@ class Interval < Time
   end
 
   def to_s
-    sec=to_i
+    sec=to_f
     if sec > 86400
       "%.1f days" % (sec/86400)
     elsif sec > 3600
       Time.at(sec).utc.strftime("%H:%M")
-    else
+    elsif sec > 60
       Time.at(sec).utc.strftime("%M'%S\"")
+    else
+      Time.at(sec).utc.strftime("%S\"%L")
     end
   end
 end
@@ -23,8 +25,8 @@ class Elapse < Interval
     @stat=stat
   end
 
-  def to_i
-    diff=(Time.now-Time.at(@stat['time'].to_f)).to_i
+  def to_f
+    diff=(Time.now-Time.at(@stat['time'].to_f)).to_f
     @v.msg{"Elapse update diff #{diff} from #{@stat['time']}"}
     diff
   end

7af487c73549dcc323e9a893c80fb3e6fe58a109
CIAX <devnull@localhost>
2011-11-04 09:39:15 -1000

    update:librerange/ unify the int and float -> float

--- script/librerange.rb
+++ script/librerange.rb
@@ -7,20 +7,20 @@ class ReRange
     if /:/ === str
       min,max=str.split(':')
       if min != ''
-        @min_ex=1 if min.sub!(/<$/,'')
-        @min=s2i(min)
+        @min_ex=true if min.sub!(/<$/,'')
+        @min=s2f(min)
       end
       if max
-        @max_ex=1 if max.sub!(/^</,'')
-        @max=s2i(max)
+        @max_ex=true if max.sub!(/^</,'')
+        @max=s2f(max)
       end
     else
-      @eq=s2i(str)
+      @eq=s2f(str)
     end
   end
 
   def <=>(str)
-    num=s2i(str)
+    num=s2f(str)
     return @eq <=> num if @eq
     return 1 if @min_ex && @min >= num
     return 1 if @min && @min > num
@@ -31,14 +31,13 @@ class ReRange
 
   private
   # Accepts int,float,hexstr
-  def s2i(str)
+  # For float /^-?[0-9]+(\.[0-9]+)?$/
+  def s2f(str)
     return str if str.kind_of? Numeric
     if /^0[Xx][0-9a-fA-F]+$/ === str
-      str.to_i(0)
-    elsif /^-?[0-9]+(\.[0-9]+)?$/ === str
-      str.to_f
+      str.to_i(0).to_f
     else
-      str.to_i
+      str.to_f
     end
   end
 end

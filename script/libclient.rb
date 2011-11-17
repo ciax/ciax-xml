9bc60844105b37bca43e5f82d5e797aa2b9f3de3
CIAX <devnull@localhost>
2011-11-17 12:26:02 -1000

    update:libclient/ add verbose function

--- script/libclient.rb
+++ script/libclient.rb
@@ -5,19 +5,23 @@ require "socket"
 class Client
   attr_reader :prompt,:message,:port,:host
   def initialize(id,port,host=nil)
+    @v=Msg::Ver.new('client',1)
     @udp=UDPSocket.open()
     @port=port
     @host=host||='localhost'
     @addr=Socket.pack_sockaddr_in(@port,@host)
+    @v.msg{"Connect to #{@host}:#{@port}"}
     @prompt="#{id}>"
   end
 
   def upd(cmd)
     line=cmd.empty? ? 'strobe' : cmd.join(' ')
     @udp.send(line,0,@addr)
+    @v.msg{"Send [#{line}]"}
     ary=@udp.recv(1024).split("\n")
     @prompt.replace(ary.pop)
     @message=ary.first
+    @v.msg{"Recv #{ary}"}
     self
   end
 

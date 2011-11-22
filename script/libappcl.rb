84a61a5b35510884cb29a5155bfcea1a4a49b4fc
CIAX <devnull@localhost>
2011-11-22 10:20:48 -1000

    add:libappint/ this is parent of libappcl

--- script/libappcl.rb
+++ script/libappcl.rb
@@ -1,21 +1,19 @@
 #!/usr/bin/ruby
-require "libinsdb"
+require "libappint"
 require "libclient"
 require "librview"
 require "libparam"
 
-class AppCl < Client
-  attr_reader :view,:commands
+class AppCl < AppInt
   def initialize(adb,host=nil)
-    Msg.type?(adb,AppDb)
-    super(adb['id'],adb['port'],host||adb['host'])
-    @view=Rview.new(adb['id'],@host).load
-    @par=Param.new(adb[:command])
-    @commands=@par.list.keys
+    super(adb)
+    host||=adb['host']
+    @cl=Client.new(adb['id'],adb['port'],host)
+    @view=Rview.new(adb['id'],host).load
   end
 
   def exe(cmd)
-    @par.set(cmd) if msg=super(cmd)
+    super if msg=@cl.exe(cmd)
     @view.load
     msg
   end
---
+++ script/libappint.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+require "libparam"
+class AppInt
+  attr_reader :view,:prompt
+  def initialize(adb)
+    Msg.type?(adb,AppDb)
+    @par=Param.new(adb[:command])
+    @prompt=adb['id']+'>'
+  end
+
+  def exe(cmd)
+    @par.set(cmd)
+    'OK'
+  end
+
+  def commands
+    @par.list.keys
+  end
+
+  def to_s
+    @view.to_s
+  end
+end

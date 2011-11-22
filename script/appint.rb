c01a7b5421964eb24077e793f26c76b02a62351e
CIAX <devnull@localhost>
2011-11-22 09:54:51 -1000

    update:appint/ support for FrmSv instead of FrmInt

--- script/appint.rb
+++ script/appint.rb
@@ -1,28 +1,28 @@
 #!/usr/bin/ruby
 require "optparse"
 require "libinsdb"
-require "libfrmint"
+require "libfrmsv"
 require "libappsv"
 
-opt=ARGV.getopts("sc")
+opt=ARGV.getopts("s")
 id,*iocmd=ARGV
 begin
   adb=InsDb.new(id).cover_app
 rescue
-  warn 'Usage: appint (-sc) [id] ("iocmd")'
+  warn 'Usage: appint (-s) [id] (iocmd)'
   Msg.exit
 end
 fdb=adb.cover_frm
-fobj=FrmInt.new(fdb,opt['c'] ? nil : iocmd)
-aobj=AppSv.new(adb,fobj)
+fint=FrmSv.new(fdb,iocmd)
+aint=AppSv.new(adb,fint)
 if opt["s"]
   require 'libserver'
-  Server.new(adb["port"].to_i,aobj.prompt){|cmd|
-    aobj.exe(cmd)
+  Server.new(adb["port"].to_i,aint.prompt){|cmd|
+    aint.exe(cmd)
   }
 else
   require 'libshell'
-  Shell.new(aobj.prompt,aobj.commands){|cmd|
-    aobj.exe(cmd)||aobj
+  Shell.new(aint.prompt,aint.commands){|cmd|
+    aint.exe(cmd)||aint
   }
 end

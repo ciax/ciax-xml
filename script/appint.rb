9216d2f9633fa6340a9acbfab4f179cfa056dcec
CIAX <devnull@localhost>
2011-11-22 23:21:33 -1000

    update:appint/ using libappints (still no work)

--- script/appint.rb
+++ script/appint.rb
@@ -1,40 +1,25 @@
 #!/usr/bin/ruby
 require "optparse"
 require "libinsdb"
-require "libmodapp"
-require "libfrmints"
+require "libappints"
 
 opt=ARGV.getopts("scf")
 id,*par=ARGV
+par=par.first if opt["c"]
 begin
-  adb=InsDb.new(id).cover_app
+  aint=AppInts.new(par)[id]
 rescue
   warn 'Usage: appint (-scf) [id] (host|iocmd)'
   Msg.exit
 end
-if opt["c"]
-  require "libappcl"
+if opt["s"]
+  require 'libserver'
+  Server.new(aint.port,aint.prompt){|cmd|
+    aint.exe(cmd)
+  }
+else
   require 'libshell'
-  aint=AppCl.new(adb,par.first)
-  aint.extend(ModApp).init(adb)
   Shell.new(aint.prompt,aint.commands){|cmd|
     aint.exe(cmd)||aint
   }
-else
-  par=par.first if opt["f"]
-  fint=FrmInts.new.add(id,par)[id]
-  require "libappsv"
-  aint=AppSv.new(adb,fint)
-  if opt["s"]
-    require 'libserver'
-    Server.new(adb["port"],aint.prompt){|cmd|
-      aint.exe(cmd)
-    }
-  else
-    require 'libshell'
-    aint.extend(ModApp).init(adb)
-    Shell.new(aint.prompt,aint.commands){|cmd|
-      aint.exe(cmd)||aint
-    }
-  end
 end

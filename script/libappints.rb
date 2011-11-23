b769bc41acbb0839115a84f8481279c28838c12e
CIAX <devnull@localhost>
2011-11-22 20:05:45 -1000

    add:libappints/

---
+++ script/libappints.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+require "optparse"
+require "libinsdb"
+
+class AppInts < Hash
+  def initialize(par=[])
+    super{|h,k| add(k,par)}
+  end
+
+  def add(id,par=[])
+    adb=InsDb.new(id).cover_app
+    case par
+    when Array
+      fint=FrmInts.new.add(id,par)[id]
+      require "libappsv"
+      self[id]=AppSv.new(adb,fint)
+    else
+      require "libappcl"
+      self[id]=AppCl.new(adb,par.first)
+    end
+  end
+end
--- script/libfrmints.rb
+++ script/libfrmints.rb
@@ -3,11 +3,11 @@ require "optparse"
 require "libinsdb"
 
 class FrmInts < Hash
-  def initialize
-    super{|h,k| add(k)}
+  def initialize(par=[])
+    super{|h,k| add(k,par)}
   end
 
-  def add(id,par=nil)
+  def add(id,par=[])
     fdb=InsDb.new(id).cover_app.cover_frm
     case par
     when Array

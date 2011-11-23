e5a6bde0be1fec1148ebd34d2d791388badc7c7e
CIAX <devnull@localhost>
2011-11-23 02:47:22 -1000

    rename:hexint -> inthex; update:inthex/ using intapps

--- script/hexsv.sh
+++ script/hexsv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 while getopts "k" opt; do
     case $opt in
-        k) psg -k hexint;;
+        k) psg -k inthex;;
         *);;
     esac
 done
 shift $(( $OPTIND -1 ))
-[ "$1" ] || psg hexint
+[ "$1" ] || psg inthex
 for id; do
     appsv $id
-    d -r -t $id hexint -sc $id
+    d -r -t $id inthex -sc $id
 done
--- script/inthex.rb
+++ script/inthex.rb
@@ -2,23 +2,16 @@
 require "optparse"
 require "libinsdb"
 require "libhexpack"
+require "libintapps"
 
 opt=ARGV.getopts("sc")
 id,*par=ARGV
+par=par.first if opt['c']
 begin
-  adb=InsDb.new(id).cover_app
-rescue
-  warn "Usage: hexint (-sc) [id] (host|iocmd)"
+  aint=IntApps.new(par)[id]
+rescue UserError
+  warn "Usage: inthex (-sc) [id] (host|iocmd)"
   Msg.exit
-end
-if opt['c']
-  require "libappcl"
-  aint=AppCl.new(adb,par.first)
-else
-  require "libappsv"
-  require "libfrmsv"
-  fint=FrmSv.new(adb.cover_frm,par)
-  aint=AppSv.new(adb,fint)
 end
 hp=HexPack.new(aint.view,aint.prompt)
 if opt["s"]

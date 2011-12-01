121091c304ff2a0548fb52d5a9894e9348213fdf
CIAX <devnull@localhost>
2011-11-30 17:20:55 -1000

    update format:mdb//command/break -> goal, add goal check at the end of macro in mdb;

--- script/libmcrdb.rb
+++ script/libmcrdb.rb
@@ -12,30 +12,41 @@ class McrDb < Db
         id=e0.attr2db(self)
         @v.msg{"MACRO:[#{id}]"}
         select=((self[:select]||={})[id]||=[])
+        final={}
         e0.each{|e1,rep|
           attr=e1.to_h
-          attr['type'] = e1.name
           case e1.name
           when 'par'
             ((self[:parameter]||={})[id]||=[]) << e1.text
-          when 'break','check','wait'
-            join=e1['join']||'all'
-            e1.each{|e2|
-              (attr[join]||=[]) << e2.to_h
-            }
-            select << attr
+          when 'check','wait'
+            attr['type'] = e1.name
+            select << mkcond(e1,attr)
+          when 'goal'
+            attr['type'] = 'break'
+            select << mkcond(e1,attr)
+            final.update(attr)['type'] = 'check'
           when 'mcr','exec'
+            attr['type'] = e1.name
             attr['cmd']=getcmd(e1)
             attr.delete('name')
             select << attr
             @v.msg{"COMMAND:[#{e1['name']}]"}
           end
         }
+        select << final unless final.empty?
       }
     }
   end
 
   private
+  def mkcond(e1,attr)
+    join=e1['join']||'all'
+    e1.each{|e2|
+      (attr[join]||=[]) << e2.to_h
+    }
+    attr
+  end
+
   def getcmd(e1)
     cmd=[e1['name']]
     e1.each{|e2|
@@ -43,7 +54,6 @@ class McrDb < Db
     }
     cmd
   end
-
 end
 
 if __FILE__ == $0

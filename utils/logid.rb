3aab0bf633419a3adff9add54d4f23e884dd5d50
CIAX <devnull@localhost>
2011-12-16 12:13:57 -1000

    debug:logid/ never forget close the files

--- utils/logid.rb
+++ utils/logid.rb
@@ -1,19 +1,10 @@
 #!/usr/bin/ruby
 require 'optparse'
-abort "Usage: logid (-w) [logfiles]" if ARGV.size < 1
-opt=ARGV.getopts("w")
-ARGV.each{|fname|
+
+def filter(fname,output)
   type,id,sfx=fname.split('_')
-  /v([0-9]).log/ =~ sfx
+  /v([0-9])\.log/ =~ sfx
   ver=$1.to_i
-  if opt['w']
-    bkup=fname+'~'
-    File.rename(fname,bkup)
-    output=open(fname,'w')
-    fname=bkup
-  else
-    output=STDOUT
-  end
   open(fname){|fd|
     fd.each{|l|
       next if l.to_s.empty?
@@ -34,4 +25,19 @@ ARGV.each{|fname|
       output.puts [tm,cid,str].join("\t")
     }
   }
+end
+
+abort "Usage: logid (-w) [logfiles]" if ARGV.size < 1
+opt=ARGV.getopts("w")
+
+ARGV.each{|fname|
+  if opt['w']
+    bkup=fname+'~'
+    File.rename(fname,bkup)
+    open(fname,'w'){|output|
+      filter(bkup,output)
+    }
+  else
+    filter(fname,STDOUT)
+  end
 }

a00363a720b4160b59326cfd34724125105d97f6
CIAX <devnull@localhost>
2012-01-05 17:06:27 -1000

    update:logv/ use hd for print data

--- utils/logv.sh
+++ utils/logv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while read time id data; do
     time=`date -d @$time +"%F %X"`
-    echo -n "$time [$id] "
-    echo $data|base64 -d|visi
+    echo "$time [$id] "
+    echo $data|base64 -d|hd
 done

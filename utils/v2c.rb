19ca2e5e203f40ecd86319e5346dc39cfd5727b3
CIAX <devnull@localhost>
2012-01-06 18:28:20 -1000

    add:v2c/ json file to csv file

---
+++ utils/v2c.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+require "libmsg"
+require "json"
+def get(h,idx)
+  return h if idx.empty?
+  k=idx.shift
+  k=k.to_i if Array === h
+  get(h[k],idx)
+end
+
+abort "Usage: v2c [key:idx] json_file" if STDIN.tty? && ARGV.size < 2
+key=ARGV.shift||'t'
+str=gets(nil) || exit
+h=JSON.load(str)
+get(h,key.split(':')).each_slice(4){|a|
+  puts a.join(',')
+}
+

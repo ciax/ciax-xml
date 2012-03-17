6861cbaca525619c57ddf962f05bc7dee4718e9d
CIAX <devnull@localhost>
2012-03-16 17:38:22 -1000

    update:hexsv/ add restart option

--- script/hexsv.sh
+++ script/hexsv.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-while getopts "k" opt; do
+while getopts "kr" opt; do
     case $opt in
-        k) psg -k inthex;;
-        *);;
+        k)
+            psg -k inthex
+            exit
+            ;;
+        r)
+            KILL=1
+            ID=`psg inthex|sed 's/^.*-s //'`
+            ;;
+        *)  ;;
     esac
 done
 shift $(( $OPTIND -1 ))
-if [ "$1" ] ; then
-    for id; do
+if [ "$1" -o "$ID" ] ; then
+    for id in $ID $*; do
+        [ "$KILL" ] && psg -t -q inthex.+$id
         d -r -t $id inthex -s $id
     done
     sleep 1
+else
+    echo "hexsv -k(kill),-r(restart) [id]..."
 fi
 psg inthex

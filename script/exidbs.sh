b8459816e8681bca61d7e5a96029f70fe8a916b4
CIAX <devnull@localhost>
2012-03-28 15:56:26 -1000

    debug:exidbs/ no view file -> skip

--- script/exidbs.sh
+++ script/exidbs.sh
@@ -5,9 +5,9 @@
 ids=${1:-`ls ~/.var/json/field_???.json|cut -d_ -f2|cut -d. -f1`};shift
 par="$*"
 for id in $ids; do
-    echo "$C2#### $id ####$C0"
     view=$HOME/.var/json/view_$id.json
-    ~/lib/libwview.rb $id > $view
+    [ -e $view ] || continue
+    echo "$C2#### $id ####$C0"
     if [ "$opt" ]
     then
         v2s <$view

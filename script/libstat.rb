7402f134a9c67767d3b5dbaa064d82ece7f92c06
CIAX <devnull@localhost>
2012-03-30 12:23:02 -1000

    update:libstat/ move StatW to libstat.rb and libstatw.rb is removed

--- script/libstat.rb
+++ script/libstat.rb
@@ -40,7 +40,72 @@ class Stat < IoFile
   end
 end
 
+# Status to StatW (String with attributes)
+module StatW
+  require "libappstat"
+  require "libsymstat"
+  require "libwatch"
+  include Writable
+  def init(adb,val)
+    Msg.type?(adb,AppDb)
+    self['val']=Msg.type?(val,AppStat)
+    self['ver']=adb['app_ver'].to_i
+    @sym=SymStat.new(adb,val).upd
+    ['msg','class'].each{|k| self[k]=@sym[k] }
+    @lastsave=0
+    self['watch']=Watch.new(adb,self)
+    self
+  end
+
+  def upd
+    self['val'].upd
+    @sym.upd
+    self['watch'].upd
+    self
+  end
+
+  def save
+    time=self['val']['time'].to_f
+    if time > @lastsave
+      super
+      @sql.flush if @sql
+      @lastsave=time
+    end
+    self
+  end
+end
+
+module StatLog
+  require "libsql"
+  def init
+    # Logging if version number exists
+    @sql=SqLog.new('value',self['id'],self['ver'],self['val'])
+  end
+
+  def upd
+    super
+    @sql.upd
+    self
+  end
+end
+
 if __FILE__ == $0
-  Msg.usage "[id] (host)" if ARGV.size < 1
-  puts Stat.new(*ARGV).load
+  require "libinsdb"
+  require "libfield"
+  begin
+    id=ARGV.shift
+    host=ARGV.shift
+    ARGV.clear
+    idb=InsDb.new(id).cover_app
+    if STDIN.tty?
+      puts Stat.new(id,host).load
+    else
+      field=Field.new.load
+      val=AppStat.new(idb,field)
+      stat=Stat.new(idb['id']).extend(StatW).init(idb,val)
+      print stat.upd.to_j
+    end
+  rescue UserError
+    Msg.usage "[id] (host | < field_file)"
+  end
 end

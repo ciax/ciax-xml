d95835436c4d37784d030ca7871f0c05488fd5b6
CIAX <devnull@localhost>
2012-03-29 16:23:29 -1000

    update:libexenum/ add deep_freeze()

--- script/libexenum.rb
+++ script/libexenum.rb
@@ -17,24 +17,42 @@ module ExEnum
     self
   end
 
+  # Freeze one level deepth or more
+  def deep_freeze
+    rec_proc(self){|i| i.freeze }
+    self
+  end
+
+  # Merge self to obj
   def deep_update(obj)
     rec_merge(obj,self)
     self
   end
 
   private
+  # a will be merged to b (b is changed)
   def rec_merge(a,b)
     case a
     when Hash
       b= b.is_a?(Hash) ? b : {}
-      a.keys.each{|i| b[i]=rec_merge(a[i],b[i])}
+      a.keys.each{|i| b[i]=rec_merge(a[i],b[i]) }
     when Array
       b= b.is_a?(Array) ? b : []
-      a.size.times{|i| b[i]=rec_merge(a[i],b[i])}
+      a.size.times{|i| b[i]=rec_merge(a[i],b[i]) }
     else
       b=a||b
     end
     b
+  end
+
+  def rec_proc(db)
+    case db
+    when Hash
+      db.keys.each{|i| rec_proc(db[i]){|i| yield i} }
+    when Array
+      db.size.times{|i| rec_proc(db[i]){|i| yield i} }
+    end
+    yield db
   end
 end
 

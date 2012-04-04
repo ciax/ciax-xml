78093ded215cc6c0195bb41e60f82be2e4973d1f
CIAX <devnull@localhost>
2012-04-03 17:56:10 -1000

    debug:libfrmrsp/ need to load field at option '-m'

--- script/libfrmrsp.rb
+++ script/libfrmrsp.rb
@@ -145,7 +145,7 @@ if __FILE__ == $0
   id=IoLog.set_logline(str)[:id]
   fdb=InsDb.new(id).cover_app.cover_frm
   cobj=Command.new(fdb[:cmdframe])
-  field=Field.new(opt['m']&&id)
+  field= opt['m'] ? Field.new(id).load : Field.new
   fr=FrmRsp.new(fdb,cobj,field)
   fr.upd_logline(str)
   field.save

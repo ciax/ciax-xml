4049b3910254a8bbee7c396e33be1d20ac1a26df
CIAX <devnull@localhost>
2012-04-05 17:58:55 -1000

    add:libwtview/ replacement of libwatchprt.rb

---
+++ script/libwtview.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/ruby
+require 'libmsg'
+require 'libexenum'
+
+class WtView < ExHash
+  def initialize(adb,stat)
+    wdb=Msg.type?(adb,AppDb)[:watch] || {:stat => []}
+    wst=Msg.type?(stat,Stat)['watch']
+    self[:stat]=[]
+    if wdb[:stat].size.times{|i|
+      hash={}
+      hash[:label]=wdb[:label][i]
+      hash[:active]=wst['active'].include?(i)
+      hash[:cond]=[]
+      n=wdb[:stat][i]
+      m=wst['stat'][i]
+      n.size.times{|j|
+        h={}
+        h[:res]=m[j]['res']
+        h[:var]=n[j]['var']
+        h[:inv]=(/true|1/ === n[j]['inv'])
+        h[:type]=n[j]['type']
+        if n[j]['type'] == 'onchange'
+          h[:cmp]=m[j]['last'].inspect
+        else
+          h[:cmp]=n[j]['val'].inspect
+        end
+        h[:val]=m[j]['val'].inspect
+        hash[:cond] << h
+      }
+      self[:stat] << hash
+    } > 0
+    self[:block]=wst['block']
+    self[:int]=wst['int']
+    self[:exec]=wst['exec']
+    end
+  end
+end
+
+module WtViewPrt
+  def to_s
+    str=''
+    self[:stat].each{|i|
+      str << "  "+Msg.color(i[:label],6)+"\t: "
+      str << show_res(i[:active])+"\n"
+      i[:cond].each{|j|
+        str << "    "+show_res(j[:res],'o','x')+' '
+        str << Msg.color(j[:var],3)
+        str << "  "
+        str << "!" if j[:inv]
+        str << "(#{j[:type]}"
+        if j[:type] == 'onchange'
+          str << "/last=#{j[:cmp]},now=#{j[:val]}"
+        else
+          str << "=#{j[:cmp]},actual=#{j[:val]}"
+        end
+        str << ")\n"
+      }
+    }
+    str << "  "+Msg.color("Blocked",2)+"\t: #{self[:block]}\n"
+    str << "  "+Msg.color("Interrupt",2)+"\t: #{self[:int]}\n"
+    str << "  "+Msg.color("Issuing",2)+"\t: #{self[:exec]}\n"
+  end
+
+  private
+  def show_res(res,t=nil,f=nil)
+    res ? Msg.color(t||res,2) : Msg.color(f||res,1)
+  end
+end
+
+if __FILE__ == $0
+  require "libinsdb"
+  require "libstat"
+  Msg.usage("[stat_file]") if STDIN.tty? && ARGV.size < 1
+  stat=Stat.new.load
+  adb=InsDb.new(stat['id']).cover_app
+  puts WtView.new(adb,stat).extend(WtViewPrt)
+end

f9c32f7bedc8ac97f65c53f59704714dac03ca75
CIAX <devnull@localhost>
2012-04-19 10:29:00 -1000

    refactoring: remove init() and get those functions into initialize()

--- script/libintapps.rb
+++ script/libintapps.rb
@@ -12,31 +12,28 @@ require "libshprt"
 class IntApps < Hash
   def initialize
     $opt||={'a'=>true}
-    super(){|h,id| init(id) }
+    super(){|h,id|
+      adb=InsDb.new(id).cover_app
+      if $opt['t']
+        require "libappobj"
+        aint=AppObj.new(adb)
+      elsif $opt['a']
+        require "libappcl"
+        aint=AppCl.new(adb,$opt['h'])
+      else
+        require "libintfrms"
+        require "libappsv"
+        fint=IntFrms.new[id]
+        aint=AppSv.new(adb,fint)
+        aint.server
+      end
+      aint.extend(ShPrt).init
+    }
   end
 
   def setdef(id)
     self[nil]=self[id] if key?(id)
     self
-  end
-
-  private
-  def init(id)
-    adb=InsDb.new(id).cover_app
-    if $opt['t']
-      require "libappobj"
-      aint=AppObj.new(adb)
-    elsif $opt['a']
-      require "libappcl"
-      aint=AppCl.new(adb,$opt['h'])
-    else
-      require "libintfrms"
-      require "libappsv"
-      fint=IntFrms.new[id]
-      aint=AppSv.new(adb,fint)
-      aint.server
-    end
-    aint.extend(ShPrt).init
   end
 end
 
--- script/libintfrms.rb
+++ script/libintfrms.rb
@@ -9,21 +9,18 @@ require "libinsdb"
 class IntFrms < Hash
   def initialize
     $opt||={}
-    super(){|h,id| init(id)}
-  end
-
-  private
-  def init(id)
-    fdb=InsDb.new(id).cover_app.cover_frm
-    if $opt['f']
-      require "libfrmcl"
-      return FrmCl.new(fdb,$opt['h'])
-    elsif $opt['d']
-      par=['frmsim',id]
-    else
-      par=[]
-    end
-    require "libfrmsv"
-    FrmSv.new(fdb,par).server
+    super(){|h,id|
+      fdb=InsDb.new(id).cover_app.cover_frm
+      if $opt['f']
+        require "libfrmcl"
+        return FrmCl.new(fdb,$opt['h'])
+      elsif $opt['d']
+        par=['frmsim',id]
+      else
+        par=[]
+      end
+      require "libfrmsv"
+      FrmSv.new(fdb,par).server
+    }
   end
 end

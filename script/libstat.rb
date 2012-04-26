7cfbcdf2183c3ce4d68b17894f56b59965e5f4d1
CIAX <devnull@localhost>
2012-04-25 15:00:45 -1000

    SymStat get into Stat::Sym

--- script/libstat.rb
+++ script/libstat.rb
@@ -31,22 +31,48 @@ end
 
 # Status to Stat::Sym (String with attributes)
 module Stat::Sym
-  require "libappval"
-  require "libsymstat"
+  require "libsymdb"
   include IoFile
   def init(adb)
     super(adb['id'])
-    Msg.type?(adb,AppDb)
+    ads=Msg.type?(adb,AppDb)[:status]
     self['ver']=adb['app_ver'].to_i
-    @sym=SymStat.new(adb,@val).upd
-    self['msg']=@sym.msg
-    self['class']=@sym.cls
+    @symbol=ads[:symbol]||{}
+    @sdb=SymDb.pack(['all',ads['table']])
+    self['class']={'time' => 'normal'}
+    self['msg']={}
     @lastsave=0
     self
   end
 
   def upd
-    @sym.upd
+    @symbol.each{|key,sid|
+      unless tbl=@sdb[sid]
+        Msg.warn("Table[#{sid}] not exist")
+        next
+      end
+      @v.msg{"ID=#{key},table=#{sid}"}
+      self['class'][key]='alarm'
+      self['msg'][key]='N/A'
+      val=@val[key]
+      tbl.each{|sym|
+        case sym['type']
+        when 'range'
+          next unless ReRange.new(sym['val']) == val
+          @v.msg{"VIEW:Range:[#{sym['val']}] and [#{val}]"}
+          self['msg'][key]=sym['msg']+"(#{val})"
+        when 'pattern'
+          next unless /#{sym['val']}/ === val || val == 'default'
+          @v.msg{"VIEW:Regexp:[#{sym['val']}] and [#{val}]"}
+          self['msg'][key]=sym['msg']
+        end
+        self['class'][key]=sym['class']
+        break
+      }
+    }
+    stime=@val['time'].to_f
+    self['msg']['time']=Time.at(stime).to_s
+    @v.msg{"Update(#{stime})"}
     self
   end
 

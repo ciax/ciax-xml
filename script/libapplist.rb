dbb2ffc8542500c8865577a8e971f7c48b10d043
CIAX <devnull@localhost>
2012-05-02 17:15:25 -1000

    update: AppList::init()/ add yield to initialize new device

--- script/libapplist.rb
+++ script/libapplist.rb
@@ -13,7 +13,6 @@ class AppList < Hash
   def initialize
     $opt||={'a'=>true}
     super{|h,id|
-#      warn "NO Hash (#{id}) then created"
       adb=InsDb.new(id).cover_app
       if $opt['t']
         require "libappint"
@@ -27,6 +26,8 @@ class AppList < Hash
         aint.socket
       end
       h[id]=aint.extend(ShPrt)
+      yield aint if defined? yield
+      h[id]
     }
   end
 

5ae37e97dd9f7d6ba3c0f5c2f8621027948fa45a
CIAX <devnull@localhost>
2012-05-15 11:14:20 -1000

    intfrm/ add device change function

--- script/intfrm.rb
+++ script/intfrm.rb
@@ -6,9 +6,15 @@ require "libfrmlist"
 $opt=ARGV.getopts("fds")
 id,$opt['h']=ARGV
 begin
-  fint=Frm::List.new[id]
+  fint=Frm::List.new{|int,devs|
+    int.cmdlist.add_group('dev',"Change Device",devs)
+  }
 rescue UserError
-  Msg.usage("(-sfd) [id] (host)","-s:server","-f:client","-d:dummy")
+  Msg.usage("(-sfd) [id] (host)",
+            "-f:client",
+            "-s:server","-d:dummy")
 end
 sleep if $opt["s"]
-fint.shell
+begin
+  int=fint[id]
+end while id=int.shell

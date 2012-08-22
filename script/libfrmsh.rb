7fb72ce88cbbde1da58ed536172966fda68fb409
CIAX <devnull@localhost>
2012-08-22 12:08:16 -1000

    Frm::Sh#init() internal command parameter validation changed

--- script/libfrmsh.rb
+++ script/libfrmsh.rb
@@ -10,11 +10,12 @@ module Frm
       self['id']=fdb['id']
       @port=fdb['port'].to_i-1000
       @field=Field::Var.new.ext_file(fdb).load
-      par={:type =>'str',:list => @field.val.keys}
+      idx={:type =>'str',:list => @field.val.keys}
+      any={:type =>'reg',:list => ["."]}
       grp=@cobj.add_group('int',"Internal Command")
-      grp.add_item('set',"Set Value [key(:idx)] (val)",[par])
-      grp.add_item('unset',"Remove Value [key]",[par])
-      grp.add_item('save',"Save Field [key,key...] (tag)",[par])
+      grp.add_item('set',"Set Value [key(:idx)] [val(,val)]",[any,any])
+      grp.add_item('unset',"Remove Value [key]",[idx])
+      grp.add_item('save',"Save Field [key,key...] (tag)",[any])
       grp.add_item('load',"Load Field (tag)")
       grp.add_item('sleep',"Sleep [n] sec",'[0-9]')
     end

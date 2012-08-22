d2fdc74ff09ad725af178a623a353734060ce9e6
CIAX <devnull@localhost>
2012-08-22 12:02:57 -1000

    Command::Item#validate() modify error message

--- script/libcommand.rb
+++ script/libcommand.rb
@@ -254,23 +254,23 @@ class Command < ExHash
           end
           Command.msg{"Validate: [#{num}] Match? [#{disp}]"}
           unless par[:list].any?{|r| ReRange.new(r) == num }
-            Msg.par_err(" Parameter Invalid (#{num}) for [#{disp}]")
+            Msg.par_err("Out of range (#{num}) for [#{disp}]")
           end
           num.to_s
         when 'str'
           Command.msg{"Validate: [#{str}] Match? [#{disp}]"}
           unless par[:list].include?(str)
-            Msg.par_err("Parameter Invalid (#{str}) for [#{disp}]")
+            Msg.par_err("Parameter Invalid Str (#{str}) for [#{disp}]")
           end
           str
         when 'reg'
           Command.msg{"Validate: [#{str}] Match? [#{disp}]"}
           unless par[:list].any?{|r| /#{r}/ === str}
-            Msg.par_err("Parameter Invalid (#{str}) for [#{disp}]")
+            Msg.par_err("Parameter Invalid Reg (#{str}) for [#{disp}]")
           end
           str
         end
-      }+pary
+      }
     end
   end
 

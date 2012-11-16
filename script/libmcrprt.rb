5cdb4a25e6af7e0b51a6968d15ccfd9e2bcfb992
CIAX <devnull@localhost>
2012-11-15 17:32:37 -1000

    add single() to show single line

--- script/libmcrprt.rb
+++ script/libmcrprt.rb
@@ -5,38 +5,42 @@ module Mcr
   module Prt
     def to_s
       @line.map{|h|
-        msg='  '*h['depth']
-        case h['type']
-        when 'break'
-          msg << Msg.color('Proceed?',6)+":#{h['label']} -> "
-          msg << Msg.color(h['fault'] ? "OK": "SKIP",2)
-        when 'check'
-          msg << Msg.color('Check',6)+":#{h['label']} -> "
-          if h['fault']
-            msg << Msg.color("NG",1)+"\n"
-            msg << getcond(h)
-          else
-            msg << Msg.color("OK",2)
-          end
-        when 'wait'
-          msg << Msg.color('Waiting',6)+":#{h['label']} -> "
-          if h['timeout']
-            msg << Msg.color("Timeout(#{h['retry']})",1)+"\n"
-            msg << getcond(h)
-          elsif h['fault']
-            ret=h['retry'].to_i
-            msg << '*'*(ret/10)+'.'*(ret % 10)
-          else
-            msg << Msg.color("OK",2)
-          end
-        when 'mcr'
-          msg << Msg.color("MACRO",3)+":#{h['cmd'].join(' ')}"
-          msg << "(async)" if h['async']
-        when 'exec'
-          msg << Msg.color("EXEC",13)+":#{h['cmd'].join(' ')}(#{h['ins']})"
-        end
-        msg
+        single(h)
       }.join("\n")
+    end
+
+    def single(h)
+      msg='  '*h['depth']
+      case h['type']
+      when 'break'
+        msg << Msg.color('Proceed?',6)+":#{h['label']} -> "
+        msg << Msg.color(h['fault'] ? "OK": "SKIP",2)
+      when 'check'
+        msg << Msg.color('Check',6)+":#{h['label']} -> "
+        if h['fault']
+          msg << Msg.color("NG",1)+"\n"
+          msg << getcond(h)
+        else
+          msg << Msg.color("OK",2)
+        end
+      when 'wait'
+        msg << Msg.color('Waiting',6)+":#{h['label']} -> "
+        if h['timeout']
+          msg << Msg.color("Timeout(#{h['retry']})",1)+"\n"
+          msg << getcond(h)
+        elsif h['fault']
+          ret=h['retry'].to_i
+          msg << '*'*(ret/10)+'.'*(ret % 10)
+        else
+          msg << Msg.color("OK",2)
+        end
+      when 'mcr'
+        msg << Msg.color("MACRO",3)+":#{h['cmd'].join(' ')}"
+          msg << "(async)" if h['async']
+      when 'exec'
+        msg << Msg.color("EXEC",13)+":#{h['cmd'].join(' ')}(#{h['ins']})"
+      end
+      msg
     end
 
     private

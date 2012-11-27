e96a0ff738ea53edea67390c1c44ae5b0ad4d67a
CIAX <devnull@localhost>
2012-11-27 10:42:35 -1000

    integrate libmcrsv -> libmcrexe

--- script/libmcrexe.rb
+++ script/libmcrexe.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/ruby
 require "libint"
 require "libstatus"
+require "libapplist"
+require "libmcrcmd"
+require "thread"
 
 module Mcr
   class Exe < Int::Exe
@@ -29,5 +32,38 @@ module Mcr
       grp.add_item("set","[dev:stat=val] set variable")
       self
     end
+  end
+
+  class Sv < Exe
+    extend Msg::Ver
+    # @<< cobj,output,intcmd,int_proc,upd_proc*
+    # @< mdb,extcmd,logline*
+    # @ dryrun,aint
+    def initialize(mdb,aint,dr=nil)
+      super(mdb)
+      @aint=Msg.type?(aint,App::List)
+      @extcmd.ext_mcrcmd(@aint,@logline,dr)
+    end
+  end
+end
+
+if __FILE__ == $0
+  require "libmcrdb"
+#  ENV['VER']='appsv'
+
+  opt=Msg::GetOpts.new("t")
+  id,*cmd=ARGV
+  ARGV.clear
+  begin
+    aint=App::List.new
+    mdb=Mcr::Db.new(id) #ciax
+    mcr=Mcr::Sv.new(mdb,aint,opt['t'])
+    mcr.ext_shell.shell
+  rescue InvalidCMD
+    Msg.exit(2)
+  rescue InvalidID
+    opt.usage("[mcr] [cmd] (par)")
+  rescue UserError
+    Msg.exit(3)
   end
 end

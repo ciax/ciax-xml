eda066bea13bdcff2b3161f36038e2e78076c9cb
CIAX <devnull@localhost>
2012-11-30 15:27:05 -1000

    add block to ext_logging

--- script/libhexpack.rb
+++ script/libhexpack.rb
@@ -73,8 +73,10 @@ module HexPack
     def server(id=nil,ver=nil)
       @output=View.new(self,@stat)
       if id
-        extend(Object::Logging).init('hexpack',id,ver)
-        @buf.post_flush.add{append([],@output.to_s)}
+        ext_logging('hexpack',id,ver){
+          @output.to_s
+        }
+        @buf.post_flush.add{append([])}
       end
       super(@adb['port'].to_i+1000){@output.to_s}
     end

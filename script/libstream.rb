d0fe2ff4d21e768a23164266bb5db3efef1ff673
CIAX <devnull@localhost>
2012-11-30 14:36:12 -1000

    debug: Hash has select(), conflicting Stream::Select -> rename to IO.select() -> Stream becomes Hash

--- script/libstream.rb
+++ script/libstream.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 require "libmsg"
 
-class Stream
+class Stream < Hash
   extend Msg::Ver
   def initialize(iocmd,wait=0,timeout=nil)
     Stream.init_ver(self,1)
@@ -11,17 +11,16 @@ class Stream
     @f=IO.popen(@iocmd,'r+')
     @wait=wait.to_f
     @timeout=timeout
-    @res={}
-    @res.update({:time => Msg.now,:cid => '',:data => ''})
+    update({:time => Msg.now,:cid => '',:data => ''})
   end
 
   def snd(str,cid)
-    @res[:cid]=cid
+    self[:cid]=cid
     return if str.to_s.empty?
     sleep @wait
     Stream.msg{"Sending #{str.size} byte on #{cid}"}
     reopen{
-      @f.syswrite(@res[:data]=str)
+      @f.syswrite(self[:data]=str)
     }
     self
   end
@@ -29,11 +28,11 @@ class Stream
   def rcv
     sleep @wait
     str=reopen{
-      select([@f],nil,nil,@timeout) || next
+      IO.select([@f],nil,nil,@timeout) || next
       @f.sysread(4096)
     }||Msg.com_err("Stream:No response")
-    Stream.msg{"Recieved #{str.size} byte on #{@res[:cid]}"}
-    @res.update({:data => str,:time => Msg.now})
+    Stream.msg{"Recieved #{str.size} byte on #{self[:cid]}"}
+    update({:data => str,:time => Msg.now})
   end
 
   def reopen
@@ -62,15 +61,15 @@ class Stream
 
     def snd(str,cid)
       super
-      append(['snd',@res[:cid]],@res[:data])
+      append(['snd',self[:cid]],self[:data])
       self
     end
 
     # return hash (data,time)
     def rcv
       super
-      append(['rcv',@res[:cid]],@res[:data])
-      @res
+      append(['rcv',self[:cid]],self[:data])
+      self
     end
   end
 end

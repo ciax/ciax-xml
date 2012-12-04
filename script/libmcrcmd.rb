e58cfef487e0e32b374ecda249d5f96f99cd9bad
CIAX <devnull@localhost>
2012-12-03 20:22:16 -1000

    status and record gets into current thread hash instead of @logs.last

--- script/libmcrcmd.rb
+++ script/libmcrcmd.rb
@@ -14,35 +14,37 @@ module Mcr
       Msg.type?(obj,Command::ExtItem)
     end
 
-    def ext_mcrcmd(aint,logs,opt={})
+    def ext_mcrcmd(aint,opt={})
       @aint=Msg.type?(aint,App::List)
       @opt=Msg.type?(opt,Hash)
       self
     end
 
     def exe
-      record={:time => Time.new.to_f}
+      me=Thread.current
+      me[:time] = Time.new.to_f
       current={'type'=>'mcr','mcr'=>@cmd,'label'=>self[:label]}
-      record[:line]=[current.extend(Prt)]
+      me[:record]=[current.extend(Prt)]
       display(current)
-      macro(record)
-      self[:stat]='(done)'
+      macro
+      me[:stat]='(done)'
       super
-      record
+      self
     end
 
     # Should be public for recursive call
-    def macro(record,depth=1)
-      self[:stat]='(run)'
+    def macro(depth=1)
+      me=Thread.current
+      me[:stat]='(run)'
       @select.each{|e1|
         current={'depth'=>depth}.update(e1)
-        record[:line].push(current.extend(Prt))
+        me[:record].push(current.extend(Prt))
         case e1['type']
         when 'goal'
-          self[:stat]='(done)' unless fault?(current)
+          me[:stat]='(done)' unless fault?(current)
           display(current)
         when 'check'
-          self[:stat]="(error)" if fault?(current)
+          me[:stat]="(error)" if fault?(current)
           display(current)
         when 'wait'
           display(current.title)
@@ -50,17 +52,17 @@ module Mcr
           display(current.result)
         when 'exec'
           display(current)
-          self[:stat]="(query)"
+          me[:stat]="(query)"
           query(depth)
-          self[:stat]='(run)'
+          me[:stat]='(run)'
           @aint[e1['site']].exe(e1['cmd']).stat.refresh
         when 'mcr'
           display(current)
-          sub=@index.dup.set(e1['mcr']).macro(record,depth+1)
+          sub=@index.dup.set(e1['mcr']).macro(depth+1)
         end
         current.delete('stat')
-        current['elapsed']=elapsed(record[:time])
-        self[:stat] != '(run)' && live?(depth) && break
+        current['elapsed']=elapsed(me[:time])
+        me[:stat] != '(run)' && live?(depth) && break
       }
       self
     end
@@ -95,7 +97,8 @@ module Mcr
     end
 
     def waiting(current)
-      self[:stat]="(wait)"
+      me=Thread.current
+      me[:stat]="(wait)"
       #gives number or nil(if break)
       current['max']=current['retry']
       if current['retry'].to_i.times{|n|
@@ -109,10 +112,10 @@ module Mcr
           yield
         }
         current['timeout']=true
-        self[:stat]='(timeout)'
+        me[:stat]='(timeout)'
       else
         current.delete('fault')
-        self[:stat]='(run)'
+        me[:stat]='(run)'
       end
     end
 
@@ -147,9 +150,9 @@ module Mcr
 end
 
 class Command::ExtDom
-  def ext_mcrcmd(aint,logs,opt={})
+  def ext_mcrcmd(aint,opt={})
     values.each{|item|
-      item.extend(Mcr::Cmd).ext_mcrcmd(aint,logs,opt)
+      item.extend(Mcr::Cmd).ext_mcrcmd(aint,opt)
     }
     self
   end
@@ -157,20 +160,18 @@ end
 
 if __FILE__ == $0
   require "libmcrdb"
-#  ENV['VER']='appsv'
 
   opt=Msg::GetOpts.new("tn")
   id,*cmd=ARGV
   ARGV.clear
   opt['v']=true
   begin
-    logs=[]
     app=App::List.new
     mdb=Mcr::Db.new(id) #ciax
     mcobj=Command.new
-    mcobj.add_extdom(mdb,:macro).ext_mcrcmd(app,logs,opt)
-    rec=mcobj.set(cmd).exe
-    puts Msg.view_struct(rec)
+    mcobj.add_extdom(mdb,:macro).ext_mcrcmd(app,opt)
+    mcobj.set(cmd).exe
+    puts Msg.view_struct(Thread.current[:record])
   rescue InvalidCMD
     opt.usage("[mcr] [cmd] (par)")
   rescue UserError

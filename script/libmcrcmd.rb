feccb44c7026067e63b2eec28801ea23acb45bca
CIAX <devnull@localhost>
2012-12-07 16:46:00 -1000

    crnt in record becomes class Record

--- script/libmcrcmd.rb
+++ script/libmcrcmd.rb
@@ -4,6 +4,94 @@ require "libcmdext"
 require "libmcrprt"
 
 module Mcr
+  class Record < Hash
+    extend Msg::Ver
+    def initialize(aint,base,opt={},depth=0)
+      @aint=Msg.type?(aint,App::List)
+      Msg.type?(base,Numeric)
+      @opt=Msg.type?(opt,Hash)
+      self['depth']=depth
+      self['elapsed']="%.3f" % (Time.now.to_f-base)
+    end
+
+    def waiting
+      rec=Thread.current[:record]
+      rec[:stat]="(wait)"
+      #gives number or nil(if break)
+      self['max']=self['retry']
+      if self['retry'].to_i.times{|n|
+          self['retry']=n
+          break 1 if  @opt['t'] && n > 3
+          break if ok?(1)
+          sleep 1
+          yield
+        }
+        self['timeout']=true
+        rec[:stat]='(timeout)'
+      else
+        rec[:stat]='(run)'
+      end
+    end
+
+    def ok?(refr=nil)
+      res=(flt=scan).empty?
+      self['fault']=flt unless res
+      delete('stat') if res or !refr
+      refresh if refr
+      res
+    end
+
+    private
+    def scan
+      stats=load
+      self['stat'].map{|h|
+        flt={}
+        site=flt['site']=h['site']
+        stat=stats[site]
+        if flt['upd']=stat.update?
+          inv=flt['inv']=h['inv']
+          var=flt['var']=h['var']
+          cmp=flt['cmp']=h['val']
+          res=stat['msg'][var]||stat['val'][var]
+          Record.msg{"site=#{site},var=#{var},inv=#{inv},cmp=#{cmp},res=#{res}"}
+          next unless res
+          flt['res']=res
+          match?(res,cmp,flt['inv']) && flt
+        else
+          flt
+        end
+      }.compact
+    end
+
+    def load
+      stats={}
+      sites.each{|site|
+        stats[site]=@aint[site].stat.load
+      }
+      stats
+    end
+
+    def refresh
+      sites.each{|site|
+        @aint[site].stat.refresh
+      }
+      self
+    end
+
+    def sites
+      self['stat'].map{|h| h['site']}.uniq
+    end
+
+    def match?(res,cmp,inv)
+      i=(/true|1/ === inv)
+      if /[a-zA-Z]/ === cmp
+        (/#{cmp}/ === res) ^ i
+      else
+        (cmp == res) ^ i
+      end
+    end
+  end
+
   module Cmd
     extend Msg::Ver
     # @<< (index),(id*),(par*),cmd*,(def_proc*)
@@ -25,7 +113,8 @@ module Mcr
       base=Time.new.to_f
       rec=(Thread.current[:record]||={})
       rec.update({:id =>base.to_i,:stat => '(ready)'})
-      crnt={'type'=>'mcr','mcr'=>@cmd,'label'=>self[:label]}
+      crnt=Record.new(@aint,base,@opt)
+      crnt.update({'type'=>'mcr','mcr'=>@cmd,'label'=>self[:label]})
       rec[:sequence]=[crnt.extend(Prt)]
       display(crnt)
       macro(base)
@@ -47,21 +136,19 @@ module Mcr
       rec=Thread.current[:record]
       rec[:stat]='(run)'
       @select.each{|e1|
-        crnt={'depth'=>depth}.update(e1).extend(Prt)
-        crnt['elapsed']="%.3f" % (Time.now.to_f-base)
+        crnt=Record.new(@aint,base,@opt,depth).update(e1).extend(Prt)
         rec[:sequence].push(crnt)
         case e1['type']
         when 'goal'
-          rec[:stat]='(done)' if ok?(crnt)
+          rec[:stat]='(done)' if crnt.ok?
           display(crnt)
         when 'check'
-          rec[:stat]='(fail)' unless ok?(crnt)
+          rec[:stat]='(fail)' unless crnt.ok?
           display(crnt)
         when 'wait'
           display(crnt.title)
-          waiting(crnt){display('.')}
+          crnt.waiting{display('.')}
           display(crnt.result)
-          crnt.delete('stat')
         when 'exec'
           display(crnt)
           rec[:stat]="(query)"
@@ -102,82 +189,6 @@ module Mcr
       end
     end
 
-    def waiting(crnt)
-      rec=Thread.current[:record]
-      rec[:stat]="(wait)"
-      #gives number or nil(if break)
-      crnt['max']=crnt['retry']
-      if crnt['retry'].to_i.times{|n|
-          crnt['retry']=n
-          break 1 if  @opt['t'] && n > 3
-          break if ok?(crnt,1)
-          sleep 1
-          yield
-        }
-        crnt['timeout']=true
-        rec[:stat]='(timeout)'
-      else
-        crnt.delete('fault')
-        rec[:stat]='(run)'
-      end
-    end
-
-    def ok?(crnt,refresh=nil)
-      res=(flt=scan(crnt)).empty?
-      crnt['fault']=flt unless res
-      crnt.delete('stat') if res or !refresh
-      refresh(crnt) if refresh
-      res
-   end
-
-    def scan(crnt)
-      stats=load(crnt)
-      crnt['stat'].map{|h|
-        flt={}
-        site=flt['site']=h['site']
-        stat=stats[site]
-        if flt['upd']=stat.update?
-          inv=flt['inv']=h['inv']
-          var=flt['var']=h['var']
-          cmp=flt['cmp']=h['val']
-          res=stat['msg'][var]||stat['val'][var]
-          Cmd.msg{"site=#{site},var=#{var},inv=#{inv},cmp=#{cmp},res=#{res}"}
-          next unless res
-          flt['res']=res
-          match?(res,cmp,flt['inv']) && flt
-        else
-          flt
-        end
-      }.compact
-    end
-
-    def load(crnt)
-      stats={}
-      sites(crnt).each{|site|
-        stats[site]=@aint[site].stat.load
-      }
-      stats
-    end
-
-    def refresh(crnt)
-      sites(crnt).each{|site|
-        @aint[site].stat.refresh
-      }
-      self
-    end
-
-    def sites(crnt)
-      crnt['stat'].map{|h| h['site']}.uniq
-    end
-
-    def match?(res,cmp,inv)
-      i=(/true|1/ === inv)
-      if /[a-zA-Z]/ === cmp
-        (/#{cmp}/ === res) ^ i
-      else
-        (cmp == res) ^ i
-      end
-    end
   end
 end
 

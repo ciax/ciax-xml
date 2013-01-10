eec88c9871c2881bfd15b9499d61ef0197a33ac3
CIAX <devnull@localhost>
2013-01-10 09:51:32 -1000

    Logging: change logging format/ time get into JSON format

--- script/liblogging.rb
+++ script/liblogging.rb
@@ -10,7 +10,7 @@ class Logging
     @type=Msg.type?(type,String)
     Msg.type?(id,String)
     ver=ver.to_i
-    @header={:id => id,:ver => ver}
+    @header={'time' => Msg.now,'id' => id,'ver' => ver}
     @loghead=VarDir+"/"+type+"_#{id}"
     Logging.msg{"Init/Logging '#{type}' (#{id}/Ver.#{ver})"}
     @proc=p
@@ -20,11 +20,11 @@ class Logging
   # Return Time
   # append() uses @proc(Hash) generated data
   def append
-    time=Msg.now
+    time=@header['time']=Msg.now
     unless ENV.key?('NOLOG')
       str=JSON.dump(@header.merge(@proc.call))
       open(logfile,'a') {|f|
-        f.puts [time,str].compact.join("\t")
+        f.puts str
       }
       Logging.msg{"#{@type}/Appended #{str.size} byte"}
     end
@@ -32,21 +32,10 @@ class Logging
   end
 
   #For new format
-  def self.invert(str)
-    ary=str.split("\t")
-    h={'time' => ary.shift}
-    h.update(JSON.load(ary.shift))
+  def self.set_logline(str)
+    h=JSON.load(str)
     abort("Logline:Line is not rcv") unless /rcv/ === h['dir']
     h['data']=decode(h['data'])
-    h
-  end
-
-  def self.set_logline(str)
-    ary=str.split("\t")
-    h={:time => ary.shift}
-    h[:id],h[:ver],dir,*h[:cmd]=ary.shift.split(':')
-    abort("Logline:Line is not rcv") unless /rcv/ === dir
-    h[:data]=decode(ary.shift)
     h
   end
 

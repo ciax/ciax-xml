0ef48e7e04d6e439971624f90b6cf7a70e70b74e
CIAX <ciax@naoj.org>
2013-02-15 10:51:47 -1000

    apply to the new format

--- utils/logline.sh
+++ utils/logline.sh
@@ -3,7 +3,7 @@
 id=$1;shift
 IFS=':' cmd="$*"
 setfld $id || _usage_key '' "[cmd]"
-input="$HOME/.var/frame_${id}_*.log"
+input="$HOME/.var/stream_${id}_*.log"
 for i in $input;do
-    egrep -h "rcv:$cmd" $i
+    egrep -h "rcv" $i|egrep "${cmd:-.}"
 done | tail -1 | grep .

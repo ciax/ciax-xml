3a7d9f5e966ef31b4ee12055aeade54797363453
CIAX <ciax@naoj.org>
2013-04-12 14:38:44 -1000

    ltcfg/ add login string , remove -s option

--- utils/ltcfg.sh
+++ utils/ltcfg.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+# use nc for input to lantronix (i.e. ltcfg id | nc host 23)
 ltf=~/db/DB-ltx.csv
 rsf=~/db/DB-rs.csv
-[ "$1" ] || { echo "Usage:ltcfg (-s) [host] (range)"; exit; }
-[ "$1" = '-s' ] && { exe="nc";shift; }
-host=$1;range=$2
+[ "$1" ] || { echo "Usage:ltcfg [id] (range)"; exit; }
+host=$1
+range=$2
 IFS=,
 inrange(){
     [ "$range" ] || return 0
     for i in $range; do
-        [ ${i%-*} -le $1 ] && [ ${i#*-} -ge $1 ] && return 0
+        [ ${i%-*} -le $1 -a ${i#*-} -ge $1 ] && return 0
     done
     return 1
 }
 
+echo "s";echo "su";echo "system"
 egrep "^$1" $ltf|while read line; do
     set - $line
     p=$2 dev=$3
@@ -26,6 +28,6 @@ egrep "^$1" $ltf|while read line; do
     echo "define service rs_$p tcpport 400$p"
     echo "define service rs_$p binary en"
     echo "define service rs_$p port $p en"
-    echo "defilne port $p dedicated service rs_$p"
+    echo "define port $p dedicated service rs_$p"
 done
 echo "initialize server delay 0"

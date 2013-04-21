0330015e0a06caa0f2d4433f5422c8481845a2fb
CIAX <ciax@naoj.org>
2013-04-21 05:12:15 -1000

    debug: incorrect CmdList param at Xml::Doc

--- script/libxmldoc.rb
+++ script/libxmldoc.rb
@@ -25,7 +25,7 @@ module Xml
       @tree[@group].each{|id,e|
         list[id]=e['label']
       }.empty? && raise(InvalidID)
-      @list=Msg::CmdList.new({"caption" => "[id]"},InvalidID).update(list).sort!
+      @list=Msg::CmdList.new({"caption" => "[id]"}).update(list).sort!
       @domain={}
       @top=nil
     end

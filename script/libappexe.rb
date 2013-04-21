df992e1e7c33bfc0b05a91c7529046cd62e9fb5d
CIAX <ciax@naoj.org>
2013-04-21 04:26:37 -1000

    App::Exe#init/ add alias initialize

--- script/libappexe.rb
+++ script/libappexe.rb
@@ -16,6 +16,9 @@ module App
       super(@stat,{'auto'=>'@','watch'=>'&','isu'=>'*','na'=>'X'})
       @extdom=@cobj.add_extdom(@adb,:command)
       @watch=Watch::Var.new.ext_file(@adb['site_id'])
+      if aldb=@adb[:command][:alias]
+        aldb.each{|k,v| @cobj[k]=@cobj[v]}
+      end
       init_layer
       init_view
       self

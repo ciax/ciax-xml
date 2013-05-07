2426b8315b5ff3e1dfced9da356921cf9b9508cd
CIAX <ciax@naoj.org>
2013-05-07 11:21:05 -1000

    Mcr::Man/ get into a member of Ins::List and spawn new macro process

--- script/libmcrman.rb
+++ script/libmcrman.rb
@@ -8,22 +8,42 @@ module Mcr
       @mdb=Msg.type?(mdb,Db)
       self['layer']='mcr'
       self['id']=@mdb['id']
+      @mid=ServerID.new('mcr','0')
+
       prom=Sh::Prompt.new(self)
       super({},prom)
-      @extdom=@cobj.add_extdom(@mdb)
-      @extdom.reset_proc{|item|
-        Mcr::Sv.new(@cobj,il).shell
+
+      @mg=@shdom.add_group('mcr','Switch Macro',2)
+
+      m0=@mid.dup
+      il[m0]=self
+      @mg.add_item('0','Macro Manager').reset_proc{
+        raise(SelectID,m0)
       }
+
+      @extdom=@cobj.add_extdom(@mdb).reset_proc{|item|
+        mc=@mid.inc
+        num=mc[:site]
+        msh=il[mc]=Mcr::Sv.new(@cobj,il)
+        msh.shdom['mcr']=@mg
+        @mg.add_item(num,"Macro # #{num}").reset_proc{
+          raise(SelectID,mc)
+        }
+        msh.start_bg
+        raise(SelectID,mc)
+      }
+
+      
     end
   end
 end
 
 if __FILE__ == $0
   begin
-    il=Ins::List.new('app')
+    il=Ins::List.new('mcr')
     mdb=Mcr::Db.new('ciax')
     man=Mcr::Man.new(mdb,il)
-    man.shell
+    il.shell('0')
   rescue InvalidCMD
     $opt.usage("[mcr] [cmd] (par)")
   end

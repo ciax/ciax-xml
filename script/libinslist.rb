553b12b05c31d30704426263c38d591c5eea7547
CIAX <ciax@naoj.org>
2013-05-08 17:50:46 -1000

    Ins::List/ include and switchable Frm/App layers

--- script/libinslist.rb
+++ script/libinslist.rb
@@ -5,29 +5,31 @@ require "libappsh"
 require "libhexsh"
 
 module Ins
-  class List < Sh::List
-    def newsh(skey)
-      Loc::Db.new unless skey
-      sid=ServerID.new.upd(skey)
-      ldb=Loc::Db.new(sid.id)
-      case sid.layer
-      when 'hex'
-        ash=self[ldb.sid('app').to_s]
-        sh=Hex.new(ldb[:app],ash)
-      when 'app'
-        fsh=self[ldb.sid('frm').to_s]
-        sh=App.new(ldb[:app],fsh)
-      when 'frm'
-        sh=Frm.new(ldb[:frm])
+  module Layer
+    def newsh(id)
+      switch_layer(super,'lay',"Change Layer",{'frm'=>"Frm mode",'app'=>"App mode"})
+    end
+  end
+
+  class List < Hash
+    def initialize(id)
+      fl=self['frm']=Frm::List.new(id).extend(Layer)
+      al=self['app']=App::List.new(id).extend(Layer)
+    end
+
+    def shell
+      lyr='app'
+      begin
+        true while self[lyr].shell
+      rescue TransLayer
+        lyr=$!.to_s
+        retry
       end
-      switch_layer(sh,'lay',"Change Layer",{'frm'=>"Frm mode",'app'=>"App mode",'hex'=>"Hex mode"})
-      switch_id(sh,'dev',"Change Device",ldb.list)
-      sh
     end
   end
 end
 
 if __FILE__ == $0
   Msg::GetOpts.new('et')
-  puts Ins::List.new('app').shell(ARGV.shift)
+  puts Ins::List.new(ARGV.shift).shell
 end

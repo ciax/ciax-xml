2161f261b9fb1c067ebab286345713813b15d60d
CIAX <ciax@naoj.org>
2013-05-22 12:04:41 -1000

    Mcr::Man/ List is separated;

--- script/libmcrman.rb
+++ script/libmcrman.rb
@@ -2,51 +2,20 @@
 require "libmcrsh"
 
 module Mcr
-  class List < Sh::List
-    def initialize(mdb,il)
-      @il=Msg.type?(il,Ins::Layer)
-      cmdlist=Msg::CmdList.new('caption' => 'Macro List','color' => 2)
-      cmdlist['0']='Macro Manager'
-      super(cmdlist,'0')
-      self['0']=Man.new(mdb,self)
-    end
-
-    def newmcr(cobj)
-      num=keys.size.to_s
-      msh=self[num]=Mcr::Sv.new(cobj,@il)
-      msh['total']=num
-      msh.prompt['total']="[#{num}/%s]"
-      @idgrp.cmdlist.delete_if{|k,v| /^1-/ === k}
-      @idgrp.cmdlist["1-#{num}"]='Other Macro Process'
-      @idgrp.add_item(num).reset_proc{
-        raise(SelectID,num)
-      }
-      msh
-    end
-  end
-
   class Man < Sh::Exe
-    # @< cobj,output,upd_proc*
-    def initialize(mdb,mlist)
-      @mdb=Msg.type?(mdb,Db)
-      @mlist=Msg.type?(mlist,List)
-      self['layer']='mcr'
-      self['id']=@mdb['id']
-      self['total']='0'
-
-      output=Msg::CmdList.new('caption' => 'Macro List','color' => 2)
-      output['[0]']='Macro Manager'
-      prom=Sh::Prompt.new(self,{'total' => "[0/%s]"})
-      super(output,prom)
-      @lodom.update @mlist.shdom
-      @mlist.idgrp.add_item('0','Macro Manager').reset_proc{
-        raise(SelectID,'0')
-      }
-      @svdom.add_extgrp(@mdb).reset_proc{|item|
-        msh=@mlist.newmcr(@cobj)
-        output["[#{self['total']}]"]=msh
-        msh.start_bg
-        raise(SelectID,self['total'])
+    def initialize(mdb,total='0')
+      Msg.type?(mdb,Db)
+      update({'layer'=>'mcr','id'=>mdb['id'],'total'=>total})
+      stat=Msg::CmdList.new('caption'=>'Active Macros','color'=>2,'show_all'=>true)
+      prom=Sh::Prompt.new(self,{'total'=>"[0/%s]"})
+      super(stat,prom)
+      @svdom.add_extgrp(mdb).reset_proc{|item|
+        # item includes arbitrary mcr command
+        # Sv generated and added to list in yield part as mcr command is invoked
+        total.succ!
+        item['total']=total
+        stat["[#{total}]"]="#{item[:cmd]} (#{item['stat']})"
+        yield(item)
       }
     end
   end
@@ -54,11 +23,10 @@ end
 
 if __FILE__ == $0
   begin
-    il=Ins::Layer.new('mcr')
     mdb=Mcr::Db.new.set('ciax')
-    man=Mcr::List.new(mdb,il)
+    man=Mcr::Man.new(mdb){}
     man.shell
   rescue InvalidCMD
-    $opt.usage("[mcr] [cmd] (par)")
+    $opt.usage
   end
 end

4d983bbd139a48e358a5a8ae87dc227e45c298ca
CIAX <ciax@naoj.org>
2013-05-22 15:45:07 -1000

    add Mcr::List/ derived from Mcr::Man

---
+++ script/libmcrlist.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/ruby
+require "libmcrman"
+
+module Mcr
+  class List < Sh::List
+    attr_reader :total
+    def initialize(mdb,il)
+      @il=Msg.type?(il,Ins::Layer)
+      super('0')
+      @total='0'
+      man=self['0']=Man.new(mdb,@total){|mobj,num| newmcr(mobj,num)}
+      @swgrp=man.lodom.add_group('sw',"Switching Macros")
+      @swgrp.add_item('0',"Macro Manager").reset_proc{ raise(SelectID,'0') }
+      @swgrp.cmdlist["1.."]='Other Macro Process'
+    end
+
+    def newmcr(mobj,num)
+      msh=self[num]=Sv.new(mobj,@il)
+      msh.prompt['total']="[#{num}/%s]"
+      msh.lodom['sw']=@swgrp
+      @swgrp.add_item(num).reset_proc{raise(SelectID,num)}
+      msh
+    end
+  end
+end
+
+if __FILE__ == $0
+  begin
+    il=Ins::Layer.new('mcr')
+    mdb=Mcr::Db.new.set('ciax')
+    man=Mcr::List.new(mdb,il)
+    man.shell
+  rescue InvalidCMD
+    $opt.usage("[mcr] [cmd] (par)")
+  end
+end

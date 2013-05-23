8637b08d779a8f6d7dfe9520b4171da57aba1ee6
CIAX <ciax@naoj.org>
2013-05-22 14:43:06 -1000

    Mcr::Man/ newmcr() is added to get Mcr::Sv in def_proc; takes &post_proc for exec at end of def_proc;

--- script/libmcrman.rb
+++ script/libmcrman.rb
@@ -3,7 +3,7 @@ require "libmcrsh"
 
 module Mcr
   class Man < Sh::Exe
-    def initialize(mdb,total='0')
+    def initialize(mdb,total='0',&post_proc)
       Msg.type?(mdb,Db)
       update({'layer'=>'mcr','id'=>mdb['id'],'total'=>total})
       stat=Msg::CmdList.new('caption'=>'Active Macros','color'=>2,'show_all'=>true)
@@ -13,10 +13,16 @@ module Mcr
         # item includes arbitrary mcr command
         # Sv generated and added to list in yield part as mcr command is invoked
         total.succ!
-        item['total']=total
-        stat["[#{total}]"]="#{item[:cmd]} (#{item['stat']})"
-        yield(item)
+        msh=newmcr
+        msh['total']=total
+        stat["[#{total}]"]="#{item[:cmd]} (#{msh['stat']})"
+        post_proc.call(total) if post_proc
       }
+    end
+
+    private
+    def newmcr
+      {}
     end
   end
 end
@@ -24,7 +30,7 @@ end
 if __FILE__ == $0
   begin
     mdb=Mcr::Db.new.set('ciax')
-    man=Mcr::Man.new(mdb){}
+    man=Mcr::Man.new(mdb)
     man.shell
   rescue InvalidCMD
     $opt.usage

8d921377657125f6ad0c3b3665fa216700176da9
CIAX <ciax@naoj.org>
2013-05-23 18:43:49 -1000

    add Mcr::Stat for Mcr::Man status (Active Macro List)

--- script/libmcrman.rb
+++ script/libmcrman.rb
@@ -2,11 +2,32 @@
 require "libmcrsh"
 
 module Mcr
+  class Stat < Hash
+    def initialize
+      @caption='<<< '+Msg.color('Active Macros',2)+' >>>'
+    end
+
+    def add(num,cmd,msh)
+      self[num]=[cmd,msh]
+      self
+    end
+
+    def to_s
+      page=[@caption]
+      each{|key,ary|
+        cmd=ary[0]
+        stat=ary[1]['stat']
+        page << Msg.item("[#{key}]","#{cmd} (#{stat})")
+      }
+      page.join("\n")
+    end
+  end
+
   class Man < Sh::Exe
     def initialize(mdb,total='0')
       Msg.type?(mdb,Db)
       update({'layer'=>'mcr','id'=>mdb['id'],'total'=>total})
-      stat=Msg::CmdList.new('caption'=>'Active Macros','color'=>2,'show_all'=>true)
+      stat=Stat.new
       prom=Sh::Prompt.new(self,{'total'=>"[0/%s]"})
       super(stat,prom)
       @svdom['ext']=Command::ExtGrp.new(mdb).reset_proc{|item|
@@ -16,7 +37,7 @@ module Mcr
         num="#{total}"
         msh=yield(@cobj,num)
         msh['total']=total
-        stat["[#{num}]"]="#{item[:cmd]} (#{msh['stat']})"
+        stat.add(num,item[:cmd],msh)
         raise(SelectID,num)
       }
     end

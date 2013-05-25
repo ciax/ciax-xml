0b4124e5e0d5c4fd19f3c8ab498b18465903a7fe
CIAX <ciax@naoj.org>
2013-05-25 06:00:06 -1000

    sqlog/ directory use grp instead of cobj for get item with setcmd for field.upd

--- script/sqlog.rb
+++ script/sqlog.rb
@@ -8,40 +8,33 @@ require "libsqlog"
 require 'liblogging'
 
 
-def get_stat(ldb,field,stat)
-  Msg.warn("Application Initialize")
-  adb=ldb[:app]
-  stat.ext_file(adb['site_id'])
-  stat.ext_rsp(field,adb[:status])
-  stat.ext_sqlog($opt['t']&&"test")
-  stat
-end
-
 Msg::GetOpts.new("t",{"v"=>"verbose"})
 begin
   raise(InvalidID,'') if STDIN.tty? && ARGV.size < 1
   ldb=Loc::Db.new
   field=Field::Var.new
   stat=Status::Var.new
-  cobj=Command.new
-  svdom=cobj.add_domain('sv')
+  fgrp=nil
   site_id=nil
   readlines.grep(/rcv/).each{|str|
     hash=Logging.set_logline(str)
     if !site_id
       site_id=hash['id']
       ldb.set(hash['id'])
-      Msg.warn("Frame Initialize")
+      Msg.warn("Initialize")
       fdb=ldb[:frm]
-      svdom['ext']=Frm::ExtGrp.new(fdb)
-      field.ext_file(fdb['site_id'])
+      fgrp=Frm::ExtGrp.new(fdb)
+      field.ext_file(fdb['site_id']).load
       field.ext_rsp(fdb)
-      get_stat(ldb,field,stat)
+      adb=ldb[:app]
+      stat.ext_file(adb['site_id'])
+      stat.ext_rsp(field,adb[:status])
+      stat.ext_sqlog($opt['t']&&"test")
     elsif site_id != hash['id']
       next
     end
     begin
-      item=cobj.setcmd(hash['cmd'].split(':'))
+      item=fgrp.setcmd(hash['cmd'].split(':'))
       field.upd(item){hash}
       stat.upd
       Msg.progress

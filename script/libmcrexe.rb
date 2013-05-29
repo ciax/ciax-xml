a57ed5535a45d24155b7d062f30eb9380eed9308
CIAX <ciax@naoj.org>
2013-05-29 10:50:45 -1000

    add Mcr::Exe for interactive operation; Query moves to libmcrexe and set Step['action'] only including 'dryrun'; Record(Step) purges codes related to interactive operaton;

---
+++ script/libmcrexe.rb
@@ -0,0 +1,171 @@
+#!/usr/bin/ruby
+require "libsh"
+require "libmcrdb"
+require "libmcrrec"
+require "libcommand"
+require "libinssh"
+
+module Mcr
+  class Exe < Hash
+    attr_accessor :mobj
+    def initialize(mitem,mobj,il)
+      @mitem=Msg.type?(mitem,Command::Item)
+      @mobj=Msg.type?(mobj.dup,Command)
+      @il=Msg.type?(il,Ins::Layer)
+      @record=Record.new(self)
+      @record.extend(Prt) unless $opt['r']
+      self['layer']='mcr'
+      self['id']=@mitem[:cmd]
+    end
+
+    def start
+      self['stat']='run'
+      puts @record if Msg.fg?
+      macro(@mitem)
+      result('done')
+      self
+    rescue Interlock
+      result('error')
+      self
+    rescue Interrupt
+      @appint.exe if @appint
+      result('interrupted')
+      self
+    ensure
+      @record.fin
+    end
+
+    def to_s
+      @mitem[:cmd]+'('+self['stat']+')'
+    end
+
+    private
+    def macro(item,depth=1)
+      item.select.each{|e1|
+        begin
+          step=@record.add_step(e1,depth){|site|
+            @il['app'][site].stat
+          }
+          qry=Query.new(step,self)
+          case e1['type']
+          when 'goal'
+            res= step.skip? && !qry.dryrun?
+            puts step if Msg.fg?
+            raise(Skip) if res
+          when 'check'
+            res=step.fail?
+            puts step if Msg.fg?
+            raise(Interlock) if res && qry.done?
+          when 'wait'
+            print step.title if Msg.fg?
+            max=qry.dryrun? ? 3 : nil
+            res=step.timeout?(max){ print '.' if Msg.fg?}
+            puts step.result if Msg.fg?
+            raise(Interlock) if res && qry.done?
+          when 'exec'
+            puts step.title if Msg.fg?
+            step.exec(qry.exec?){|site,cmd,depth|
+              ash=@il['app'][site]
+              ash.exe(cmd)
+              @appint=ash.interrupt
+            }
+            puts step.action if Msg.fg?
+          when 'mcr'
+            puts step if Msg.fg?
+            Msg.warn('Async') if /true|1/ === e1['async']
+            macro(@mobj.setcmd(e1['cmd']),depth+1)
+          end
+        rescue Retry
+          retry
+        rescue Skip
+          return
+        end
+      }
+      self
+    end
+
+    def result(str)
+      self['stat']=str
+      @record['result']=str
+      puts str if Msg.fg?
+    end
+  end
+
+  class Query
+    def initialize(step,sh)
+      @step=Msg.type?(step,Step)
+      @sh=Msg.type?(sh,Exe)
+    end
+
+    def exec?
+      return false if dryrun?
+      while ! $opt['n']
+        case query(['Exec','Skip'])
+        when /^E/i
+          break
+        when /^S/i
+          @step['action']='skip'
+          return false
+        end
+      end
+      @step['action']='exec'
+    end
+
+    def done?
+      return true if $opt['n']
+      loop{
+        case query(['Done','Force','Retry'])
+        when /^D/i
+          @step['action']='done'
+          return true
+        when /^F/i
+          @step['action']='forced'
+          return false
+        when /^R/i
+          @step['action']='retry'
+          raise(Retry)
+        end
+      }
+    end
+
+    def dryrun?
+      ! ['e','s','t'].any?{|i| $opt[i]} && @step['action']='dryrun'
+    end
+
+    private
+    def query(cmds)
+      thc=Thread.current
+      thc[:valid_keys]=cmds.map{|s| s[0].downcase}
+      @sh['stat']='query'
+      if Msg.fg?
+        prompt=Msg.color('['+cmds.join('/')+']?',5)
+        print Msg.indent(@step['depth'].to_i+1)
+        res=Readline.readline(prompt,true)
+      else
+        @step['option']=cmds
+        sleep
+        @step.delete('option')
+        res=thc[:query]
+      end
+      @sh['stat']='run'
+      thc[:valid_keys].clear
+      res
+    end
+  end
+end
+
+if __FILE__ == $0
+  Msg::GetOpts.new('rest',{'n' => 'nonstop mode','i' => 'interactive mode'})
+  begin
+    il=Ins::Layer.new('app')
+    mdb=Mcr::Db.new.set('ciax')
+    mobj=Command.new
+    svdom=mobj.add_domain('sv',6)
+    svdom['ext']=Command::ExtGrp.new(mdb)
+    mitem=mobj.setcmd(ARGV)
+    msh=Mcr::Exe.new(mitem,mobj,il)
+    msh.start
+  rescue InvalidCMD
+    $opt.usage("[mcr] [cmd] (par)")
+  end
+end
--- script/libmcrrec.rb
+++ script/libmcrrec.rb
@@ -10,7 +10,7 @@ module Mcr
     attr_reader :crnt
     def initialize(obj)
       super('mcr')
-      @obj=Msg.type?(obj,Sv)
+      @obj=Msg.type?(obj,Exe)
       obj[:base]=Time.new.to_f
       self['id']=@obj[:base].to_i
       self['cmd']=obj.mobj.current.cmd
@@ -33,63 +33,42 @@ module Mcr
 
   class Step < ExHash
     def initialize(db,obj,depth=0,p)
-      obj=Msg.type?(obj,Sv)
       @stat_proc=Msg.type?(p,Proc)
       self['time']=Msg.elps_sec(obj[:base])
       self['depth']=depth
       update(Msg.type?(db,Hash))
       @condition=delete('stat')
-      @query=Query.new(self,obj)
-      @dryrun=! ['e','s','t'].any?{|i| $opt[i]}
     end
 
-    def exec
-      puts title if Msg.fg?
-      if @query.exec?(@dryrun)
+    def exec(go=nil)
+      if go
         yield(self['site'],self['cmd'],self['depth'])
         self['result']='done'
       else
         self['result']='skip'
       end
-      puts action if Msg.fg?
     end
 
-    def timeout?
+    def timeout?(max=nil,&p) # Print Progress Proc
       #gives number or nil(if break)
-      print title if Msg.fg?
-      max=self['max']=self['retry']
-      if @dryrun
-        max = 3
-        self['action']='dryrun'
-      end
+      self['max']=self['retry']
+      max||=self['max']
       max.to_i.times{|n|
         self['retry']=n
-        if ok?('pass','wait')
-          puts result if Msg.fg?
-          return
-        end
+        return if ok?('pass','wait')
         refresh
         sleep 1
-        print '.' if Msg.fg?
+        p.call if p
       }
       self['result']='timeout'
-      puts result if Msg.fg?
-      @query.done?
     end
 
     def skip?
-      return unless ok?('skip','pass')
-      return true unless @dryrun
-      self['action']='dryrun'
-      false
-    ensure
-      puts to_s if Msg.fg?
+      ok?('skip','pass')
     end
 
     def fail?
-      return if ok?('pass','failed')
-      puts to_s if Msg.fg?
-      @query.done?
+      ! ok?('pass','failed')
     end
 
     def title ; self['label']||self['cmd']; end
@@ -145,69 +124,6 @@ module Mcr
       else
         (cmp == act) ^ i
       end
-    end
-  end
-
-  class Query
-    def initialize(step,sh)
-      @step=Msg.type?(step,Step)
-      @sh=Msg.type?(sh,Sv)
-    end
-
-    def exec?(dryrun=nil)
-      return true if $opt['n']
-      loop{
-        case query(['Exec','Skip'])
-        when /^E/i
-          if dryrun
-            @step['action']='dryrun'
-            return false
-          else
-            @step['action']='exec'
-            return true
-          end
-        when /^S/i
-          @step['action']='skip'
-          return false
-        end
-      }
-    end
-
-    def done?
-      return true if $opt['n']
-      loop{
-        case query(['Done','Force','Retry'])
-        when /^D/i
-          @step['action']='done'
-          return true
-        when /^F/i
-          @step['action']='forced'
-          return false
-        when /^R/i
-          @step['action']='retry'
-          raise(Retry)
-        end
-      }
-    end
-
-    private
-    def query(cmds)
-      inc=cmds.map{|s| s[0].downcase}
-      @sh.intgrp.valid_keys.replace(inc)
-      @sh['stat']='query'
-      if Msg.fg?
-        prompt=Msg.color('['+cmds.join('/')+']?',5)
-        print Msg.indent(@step['depth'].to_i+1)
-        res=Readline.readline(prompt,true)
-      else
-        @step['option']=cmds
-        sleep
-        @step.delete('option')
-        res=Thread.current[:query]
-      end
-      @sh['stat']='run'
-      @sh.intgrp.valid_keys.clear
-      res
     end
   end
 end

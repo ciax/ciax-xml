a485a37b7574796f75c459939691dde0bc8233f7
CIAX <ciax@naoj.org>
2013-07-17 18:00:08 -1000

    frmsim/ adapt to new JSON log format

--- utils/frmsim.rb
+++ utils/frmsim.rb
@@ -1,41 +1,38 @@
 #!/usr/bin/ruby
+require "json"
+
 abort "Usage: frmsim [id] (ver)" if ARGV.size < 1
 id=ARGV.shift
 ver=ARGV.shift
 ARGV.clear
 def pr(text)
-  STDERR.print "\033[0;34m#{text}\33[0m"
+  STDERR.print "\033[1;34m#{text}\33[0m"
 end
 
 def find_snd(fd,input,fname)
+  inp=[input.chomp].pack("m").split("\n").join('')
   while line=fd.gets
-    snd=line.split("\t")
-    rec=snd[2].chomp
-    inp=[input.chomp].pack("m").split("\n").join('')
-    if rec == inp
-      pr "#{fname}:#{snd[1]}(#{fd.lineno})"
-      rcv=fd.gets.split("\t")
-      if /rcv/ === rcv[1]
-        pr " -> rcv(#{fd.lineno})"
-        sleep rcv[0].to_i-snd[0].to_i
-        STDOUT.syswrite(rcv[2].unpack("m").first)
-      end
-      STDERR.puts
-      break 1
-    end
+    next unless /#{inp}/ === line
+    snd=JSON.load(line)
+    sl=fd.lineno
+    rcv=JSON.load(fd.gets)
+    next unless /rcv/ === rcv['dir']
+    pr "#{fname}:#{snd['cmd']}(#{sl}) -> rcv(#{fd.lineno})\n"
+    sleep rcv['time'].to_i-snd['time'].to_i
+    STDOUT.syswrite(rcv['base64'].unpack("m").first)
+    break 1
   end
 end
 
 begin
-  10.times{
-    Dir.glob(ENV['HOME']+"/.var/201?/frame_#{id}*.log").each{|fname|
+  100.times begin
+    Dir.glob(ENV['HOME']+"/.var/stream_#{id}*.log").each{|fname|
       /#{id}[^\.]*/ =~ fname
       base=$&
       open(fname){|fd|
         select([STDIN]) while find_snd(fd,STDIN.sysread(1024),base)
-        warn "no more line"
       }
     }
-  }
+  end
 rescue EOFError
 end

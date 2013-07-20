518599e1f1f74af4ee046b5a2349687c460e52d6
CIAX <ciax@naoj.org>
2013-07-19 15:41:42 -1000

    frmsim/ adapt to new format

--- utils/frmsim.rb
+++ utils/frmsim.rb
@@ -1,38 +1,94 @@
 #!/usr/bin/ruby
 require "json"
 
+class LogRing
+  attr_reader :index,:max
+  def initialize(id)
+    @logary=[{}]
+    @index=0
+    ary=[]
+    Dir.glob(ENV['HOME']+"/.var/stream_#{id}*.log").each{|fname|
+      open(fname){|fd|
+        while line=fd.gets
+          hash=JSON.load(line)||next
+          ary << hash
+        end
+      }
+    }
+    dict={}
+    ary.sort_by{|h| h['time'] }.uniq.select{|h| h['id'] == id}.each{|h|
+      data=h.delete('base64')
+      case h.delete('dir')
+      when 'snd'
+        h['snd']=data
+        dict[data]=''
+        if @logary.last.key?('rcv')
+          @logary << h
+        else
+          pr 'snd duplicated' if @logary.last.key?('snd')
+          @logary.last.update h
+        end
+      when 'rcv'
+        if @logary.last.key('rcv')
+          pr 'rcv duplicated'
+        elsif @logary.last['cmd'] == h['cmd']
+          h['rcv']=data
+          dur=h.delete('time').to_f*1000 - @logary.last['time'].to_f*1000
+          h['dur']=dur.round/1000.0
+          @logary.last.update h
+        else
+          pr 'no match'
+        end
+      end
+    }
+    @max=@logary.size-1
+    @sndary=dict.keys
+  end
+
+  def pop
+    @index+=1
+    @index=0 if @index > @max
+    @logary[@index]
+  end
+
+  def include?(str)
+    @sndary.include?(str)
+  end
+
+  def to_s
+    @logary.map{|e|
+      e.to_s
+    }.join("\n")
+  end
+end
+
+def pr(text)
+  STDERR.print "\033[1;34m#{text}\33[0m" if /sim/ === ENV['VER']
+end
+
+def input
+  select([STDIN])
+  [STDIN.sysread(1024).chomp].pack("m").split("\n").join('')
+end
+
 abort "Usage: frmsim [id] (ver)" if ARGV.size < 1
 id=ARGV.shift
 ver=ARGV.shift
 ARGV.clear
-def pr(text)
-  STDERR.print "\033[1;34m#{text}\33[0m"
-end
 
-def find_snd(fd,input,fname)
-  inp=[input.chomp].pack("m").split("\n").join('')
-  while line=fd.gets
-    next unless /#{inp}/ === line
-    snd=JSON.load(line)
-    sl=fd.lineno
-    rcv=JSON.load(fd.gets)
-    next unless /rcv/ === rcv['dir']
-    pr "#{fname}:#{snd['cmd']}(#{sl}) -> rcv(#{fd.lineno})\n"
-    sleep rcv['time'].to_i-snd['time'].to_i
-    STDOUT.syswrite(rcv['base64'].unpack("m").first)
-    break 1
-  end
-end
-
+logv=LogRing.new(id)
 begin
-  100.times begin
-    Dir.glob(ENV['HOME']+"/.var/stream_#{id}*.log").each{|fname|
-      /#{id}[^\.]*/ =~ fname
-      base=$&
-      open(fname){|fd|
-        select([STDIN]) while find_snd(fd,STDIN.sysread(1024),base)
-      }
-    }
+  while inp=input
+    if logv.include?(inp)
+      begin
+        crnt=logv.pop
+      end until crnt['snd'] == inp
+      pr "#{crnt['cmd']}(#{logv.index}/#{logv.max})\n"
+      sleep crnt['dur'].to_i
+      STDOUT.syswrite(crnt['rcv'].unpack("m").first)
+    else
+      pr "Can't find logline for input of [#{inp}]\n"
+    end
   end
 rescue EOFError
 end

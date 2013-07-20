7ef48ebc9378eedbea068f006f6e23255e32cdb3
CIAX <ciax@naoj.org>
2013-07-19 16:09:20 -1000

    remove unused utils (utils for old logfile)

--- utils/frmupd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. ~/lib/libdb.sh entity
-id=$1;shift
-setfld $id || _usage_key
-output="$HOME/.var/json/field_${id}.json"
-merging $output <<EOF
-{"id":"$id"}
-EOF
-~/lib/libfrmrsp.rb -q $dev | merging $output

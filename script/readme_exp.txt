4a9178bb3d62958b7d8c5bca18f5c4eab3883a7a
CIAX <ciax@naoj.org>
2013-09-24 13:51:35 -1000

    add pack2exp.sh for export

---
+++ script/readme_exp.txt
@@ -0,0 +1,19 @@
+## commit a485
+## Install required (Debian squeeze) ##
+ ruby1.9.1 (for using JSON)
+ libxml-ruby1.9.1 (XML instead of REXML)
+ libxml2-utils (xmllint)
+ socat
+ libsqlite3-ruby1.9.1
+ libxml-xpath-perl (xpath command)
+ coreutils:
+  grep,cut,tail
+
+## Verbose mode ##
+ set VER environment
+ VER=string1,string2... for set sum
+ VER=string1:strint2..  for set intersection
+ VER=* for All
+
+## Project mode
+  set PROJ to limit Device ID (is in idb-{PROJ}.xml)
---
+++ utils/pack2exp.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+clrdir(){
+    [ -e "$1" ] && /bin/rm -r $1
+    mkdir "$1"
+}
+
+pfx=~/package/ciax-xml
+
+mkcp(){
+    dst=$1;shift
+    clrdir $pfx/$dst
+    cd ~/ciax-xml/$dst || { echo "NO ~/ciax-xml/$dst dir"; exit; }
+    for i ;do
+        cp $i $pfx/$dst
+    done
+}
+
+
+clrdir ~/package
+
+mkcp "" '*.xml'
+mkcp script '*.rb'
+cp readme_exp.txt $pfx/script/readme.txt
+
+mkcp schema '*'
+mkcp utils chkxml.sh frmsim.rb mkhtml.sh sqlog.rb
+mkcp webapp '*'
+
+cmt=$(git log -1 --abbrev=4 --abbrev-commit|grep commit)
+
+cd ~/package
+pkg=ciax-xml-${cmt#* }.tgz
+tar cvzf $pkg ciax-xml

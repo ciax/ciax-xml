b88f8d6f896e7179c76fc5c35cc02112d8a660ab
CIAX <ciax@naoj.org>
2013-09-27 11:15:15 -1000

    Config/ takes Hash to be input to current level;

--- script/libconf.rb
+++ script/libconf.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/ruby
 module CIAX
   # Hash array of @ary: each Hash is associated with Domain,Group,Item;
-  # Usage:[]=/ add to current Hash;
+  # Usage:[]=/ add to current Hash which will override(hide) upper level Hash;
   # Usage:[]/  get val from current Hash otherwise from upper level of Hash;
   class Config < Hash
     attr_reader :ary
     alias :_org_keys :keys
     def initialize(hash=nil)
       @ary=[self]
-      @ary+=hash.ary if hash.is_a? Config
+      case hash
+      when Config
+        @ary+=hash.ary
+      when Hash
+        update hash
+      end
     end
 
     def keys

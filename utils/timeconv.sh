b9a0575e27700c97fd60dedb95700b1eb2dd2355
CIAX <ciax@naoj.org>
2013-10-23 15:49:41 -1000

    timeconv/ accepts both sqlite3 file and jsonlog file;

--- utils/timeconv.sh
+++ utils/timeconv.sh
@@ -3,10 +3,20 @@
 tempdir=~/.var/temp
 [ -d $tempdir ] || mkdir $tempdir
 n='[0-9]'
+tform="$n$n$n$n$n$n$n$n$n$n\.$n\+"
 for file ;do
-    rm $tempdir/*
-    for i in `echo .tables|sqlite3 $file`; do
-        echo ".dump $i"|sqlite3 $file| sed -e "s/'\($n$n$n$n$n$n$n$n$n$n\.$n$n$n\)'/\1/" > $tempdir/$i
-        (echo "drop table $i;";cat $tempdir/$i)|sqlite3 $file
-    done
+    echo $file
+    case $file in
+        *.sq3)
+            for i in `echo .tables|sqlite3 $file`; do
+                echo ".dump $i"|sqlite3 $file| sed -e "s/'\($tform\)'/\1/" > $tempdir/$i
+                (echo "drop table $i;";cat $tempdir/$i)|sqlite3 $file
+                rm $tempdir/$i
+            done
+            ;;
+        *.log)
+            <$file sed -e "s/\"\($tform\)\"/\1/" > $tempdir/$file
+            mv $tempdir/$file .
+            ;;
+    esac
 done

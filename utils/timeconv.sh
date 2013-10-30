04b148d79fed483f261253cc033421a5f8edd474
CIAX <ciax@naoj.org>
2013-10-29 22:36:33 -1000

    timeconv/ make it universal;

--- utils/timeconv.sh
+++ utils/timeconv.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
-# Converting type of time field in Sqlog (String -> Numeric)
+# Converting time field in Sqlog or Logfile (x 1000/Integer)
 tempdir=~/.var/temp
 [ -d $tempdir ] || mkdir $tempdir
 n='[0-9]'
-tform="$n$n$n$n$n$n$n$n$n$n\.$n\+"
+int="$n$n$n$n$n$n$n$n$n$n"
+dec="$n$n$n"
+conv(){
+    sed -e "s/['\"]*\($int\)\.*\($dec\)$n*['\"]*/\1\2/"
+}
+
 for file ;do
     echo $file
     case $file in
         *.sq3)
             for i in `echo .tables|sqlite3 $file`; do
-                echo ".dump $i"|sqlite3 $file| sed -e "s/'\($tform\)'/\1/" > $tempdir/$i
+                echo ".dump $i"|sqlite3 $file| conv > $tempdir/$i
                 (echo "drop table $i;";cat $tempdir/$i)|sqlite3 $file
                 rm $tempdir/$i
             done
             ;;
         *.log)
-            <$file sed -e "s/\"\($tform\)\"/\1/" > $tempdir/$file
+            <$file conv > $tempdir/$file
             mv $tempdir/$file .
             ;;
     esac

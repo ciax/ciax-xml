3b96d6948af3890b5062e5ebcb648ebb733038ee
CIAX <ciax@naoj.org>
2013-11-04 11:20:29 -1000

    project:DB structure libxmlshare/ add comment;

--- script/libxmlshare.rb
+++ script/libxmlshare.rb
@@ -26,7 +26,8 @@ module CIAX
         ary
       end
 
-      def attr2db(db,id='id')
+
+      def attr2db(db,id='id') # deprecated
         # <xml id='id' a='1' b='2'> => db[:a][id]='1', db[:b][id]='2'
         type?(db,Hash)
         attr={}
@@ -47,7 +48,7 @@ module CIAX
         key
       end
 
-      def add_item(db,id='id')
+      def add_item(db,id='id') # deprecated
         # <xml id='id' a='1' b='2'> => db[id][a]='1', db[id][b]='2'
         type?(db,Hash)
         attr={}

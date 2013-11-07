180d2e6e35e300d022efd82373a3f04ffaf6812c
CIAX <ciax@naoj.org>
2013-11-07 10:59:12 -1000

    project:DB structure Watch::Upd/ adapt to transposed wdb;

--- script/libwatch.rb
+++ script/libwatch.rb
@@ -63,14 +63,15 @@ module CIAX
       end
 
       def ext_upd(adb,stat)
-        @wdb=type?(adb,App::Db)[:watch] || {:stat => {}}
+        wdb=type?(adb,App::Db)[:watch]||{}
+        @windex=wdb[:index]||{}
         @stat=type?(stat,App::Status)
         reg_procs(@stat)
-        self['period']=adb['period'].to_i if adb.key?('period')
-        self['interval']=adb['interval'].to_f/10 if adb.key?('interval')
+        self['period']=wdb['period'].to_i if wdb.key?('period')
+        self['interval']=wdb['interval'].to_f/10 if wdb.key?('interval')
         # Pick usable val
-        @list=@wdb[:stat].values.flatten(1).map{|h| h['var']}.uniq
-        @list.unshift('time')
+        @list=['time']
+        @windex.values.each{|v| @list|=v[:cnd].map{|i| i["var"]}}
         # @stat.data(all) = self['crnt'](picked) > self['last']
         # upd() => self['last']<-self['crnt']
         #       => self['crnt']<-@stat.data
@@ -84,14 +85,10 @@ module CIAX
         return self unless @stat.update?
         sync
         hash={'active'=> [],'int' =>[],'exec' =>[],'block' =>[]}
-        @wdb[:stat].each{|i,v|
-          next unless check(i)
-          hash.each{|k,a|
-            if db=@wdb[k.to_sym]
-              a.concat db[i] if db[i]
-            else
-              a << i
-            end
+        @windex.each{|id,item|
+          next unless check(item)
+          item[:act].each{|key,ary|
+            hash[key.to_s].concat ary
           }
         }
         if !hash['active'].empty?
@@ -131,31 +128,39 @@ module CIAX
         }
       end
 
-      def check(i)
-        return true unless @wdb[:stat][i]
-        verbose("Watch","Check: <#{@wdb[:label][i]}>")
-        n=@wdb[:stat][i]
+      def get_crnt(id)
+        unless @list.include?(id)
+          @list << id
+          self['crnt'][id]=@stat.data[id]
+        end
+        self['crnt'][id]
+      end
+
+      def check(item)
+        return true unless item[:cnd]
+        verbose("Watch","Check: <#{item['label']}>")
+        cklst=item[:cnd]
         rary=[]
-        n.each_index{|j|
-          k=n[j]['var']
-          v=@stat.data[k]
-          case n[j]['type']
+        cklst.each{|ckitm|
+          id=ckitm['var']
+          val=@stat.data[id]
+          case ckitm['type']
           when 'onchange'
-            c=@stat.last[k]
-            res=(c != v)
-            verbose("Watch","  onChange(#{k}): [#{c}] vs <#{v}> =>#{res}")
+            cmp=self['last'][id]
+            res=(cmp != val)
+            verbose("Watch","  onChange(#{id}): [#{cmp}] vs <#{val}> =>#{res}")
           when 'pattern'
-            c=n[j]['val']
-            res=(Regexp.new(c) === v)
-            verbose("Watch","  Pattrn(#{k}): [#{c}] vs <#{v}> =>#{res}")
+            cmp=ckitm['val']
+            res=(Regexp.new(cmp) === val)
+            verbose("Watch","  Pattrn(#{id}): [#{cmp}] vs <#{val}> =>#{res}")
           when 'range'
-            c=n[j]['val']
-            f=cond[j]['val']="%.3f" % v.to_f
-            res=(ReRange.new(c) == f)
-            verbose("Watch","  Range(#{k}): [#{c}] vs <#{f}>(#{v.class}) =>#{res}")
+            cmp=ckitm['val']
+            f="%.3f" % val.to_f
+            res=(ReRange.new(cmp) == f)
+            verbose("Watch","  Range(#{id}): [#{cmp}] vs <#{f}>(#{val.class}) =>#{res}")
           end
-          res=!res if /true|1/ === n[j]['inv']
-          self['res']["#{i}:#{j}"]=res
+          res=!res if /true|1/ === ckitm['inv']
+          self['res']["#{id}:#{rary.size}"]=res
           rary << res
         }
         rary.all?
@@ -178,7 +183,7 @@ module CIAX
     stat=App::Status.new.ext_file(adb['site_id']).load
     watch=Watch::Data.new.ext_upd(adb,stat).upd
     if t=$opt['t']
-      watch.ext_file(adb['site_id'])
+      watch.ext_file
       stat.str_update(t).upd.save
     end
     puts watch

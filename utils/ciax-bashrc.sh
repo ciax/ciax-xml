098eada7bac9d5ab39b87ec75c01e8e386102fb6
CIAX <ciax@naoj.org>
2013-11-08 17:12:25 -1000

    On branch installer    util files renamed;

--- utils/ciax-bashrc.sh
+++ utils/ciax-bashrc.sh
@@ -8,11 +8,9 @@ export RUBYLIB="$HOME/lib"
 #Alias
 alias chkxml=check_xml
 alias devsim=device_simulator
-alias jv=json_view
+alias jvw=json_view
 alias jl=json_logview
 alias ltcfg=lantronix-config
 alias mkhtm=make_html.sh
-alias marv=marshal_view.rb
+alias mvw=marshal_view.rb
 alias reg=register_files
-alias sconv=sqlog_stream_to_status
-alias attgrep=xml_agrep
--- utils/json_view.rb
+++ utils/json_view.rb
@@ -2,7 +2,7 @@
 # JSON to String(Decorated)
 require "libenumx"
 
-abort "Usage: j2s json_file" if STDIN.tty? && ARGV.size < 1
+abort "Usage: json_view [json_file]" if STDIN.tty? && ARGV.size < 1
 
 str=gets(nil) || exit
 puts JSON.load(str).extend(CIAX::Enumx)

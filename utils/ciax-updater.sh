ca7f1fd94da60a59872d35beea2af7d6bf8507dc
CIAX <ciax@naoj.org>
2013-11-08 18:36:31 -1000

    On branch installer  modify file overwrite handling;

--- utils/ciax-updater.sh
+++ utils/ciax-updater.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd
+[ -e ciax-xml.tgz ] && rm ciax-xml.tgz
 wget http://ciax.sum.naoj.org/dav/ciax-xml.tgz
 tar xvzf ciax-xml.tgz
 clean_up
--- utils/clean_up.sh
+++ utils/clean_up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/.var
-/bin/rm cache/*.mar json/*.json
+/bin/rm cache/*.mar json/*.json >/dev/null 2>&1
 cd ~/ciax-xml
 register_files */

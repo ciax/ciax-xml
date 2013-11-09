1d96c5c51f6ea4f616b0eb631d202fb227b6885b
CIAX <ciax@naoj.org>
2013-11-08 16:47:56 -1000

    On branch installer  dig_dir moved into ciax-installer;

--- utils/ciax-installer.sh
+++ utils/ciax-installer.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # bin dir separated for executable link in hg repos
 # "Usage: ${0##*/} [DIR..] | [SRC..]"
+dig_dir(){
+    for i ; do
+        [ -d "$i" ] || mkdir "$i"
+        cd "$i"
+    done
+}
 cd ~/ciax-xml
 for i in */ ; do
     ./utils/register_files.sh $i
@@ -10,3 +16,6 @@ dig_dir ~/.var
 dig_dir cache
 cd ..
 dig_dir json
+
+
+
--- utils/dig_dir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-[ "$1" ] || { echo "Usage: ${0##*/} [dir..]"; exit; }
-for i ; do
-    [ -d "$i" ] || mkdir "$i"
-    cd "$i"
-done

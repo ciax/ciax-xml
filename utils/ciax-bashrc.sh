d7987ea56bedbeb2572bfab45fd7f19796654805
CIAX <ciax@naoj.org>
2013-11-12 12:39:49 -1000

    On branch installer ciax-bashrc/ addenv() avoids duplicated setting for ENV list;

--- utils/ciax-bashrc.sh
+++ utils/ciax-bashrc.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+addenv(){
+    name=$1;shift
+    list=$(IFS=: eval echo \$$name)
+    for j ; do
+        for i in $list;do
+            [ "$j" = "$i" ] && break 1
+        done
+        eval "export $name=$j:\$$name"
+    done
+}
+
 # Local functions
 umask 022
 shopt -s nullglob
 export LANG="C"
-export PATH="$PATH:$HOME/bin:$HOME/lib"
-export XMLPATH=$HOME/ciax-xml
-export RUBYLIB="$HOME/lib"
+addenv PATH "$HOME/bin" "$HOME/lib/b"
+addenv XMLPATH "$HOME/ciax-xml"
+addenv RUBYLIB "$HOME/lib"
+
 #Alias
 alias clu=clean_up
 alias chkxml=check_xml

917613d553926e8df4ae43dea4cb0e7303a75920
CIAX <ciax@naoj.org>
2013-11-19 14:06:07 -1000

    modify: json-logpick/ takes -s option for search 'snd' line;

--- work/json-logpick.sh
+++ work/json-logpick.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 . ~/lib/libdb.sh entity
+[ "$1" = '-s' ] && { dir='snd';shift; }
 id=$1;shift
 IFS=':' cmd="$*"
-setfld $id || _usage_key '' "[cmd]"
+setfld $id || _usage_key '(-s)' "[cmd]"
 input="$HOME/.var/stream_${id}_*.log"
 for i in $input;do
-    egrep -h "rcv" $i|egrep "${cmd:-.}"
+    egrep -h "${dir:-rcv}" $i|egrep "${cmd:-.}"
 done | tail -1 | grep .

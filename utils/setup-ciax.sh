407ae9b5074e4ad1200d5c3085ef849336c71f79
CIAX <ciax@naoj.org>
2014-04-22 18:39:38 -1000

    add .bashrc edit, package installation;

--- utils/setup-ciax.sh
+++ utils/setup-ciax.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+init_bashrc(){
+    grep -q '#initrc' ~/.bashrc >/dev/null 2>&1 && return
+    echo 'shopt -s nullglob;for i in ~/bin/rc.*;do . $i;done #initrc' >> ~/.bashrc
+}
 mklink(){
     for i;do
         [ -d "$i" ] && (dig_dir "$i";mklink *)
@@ -11,16 +15,31 @@ mklink(){
         esac
     done
 }
-
 dig_dir(){
     for i ; do
         [ -d "$i" ] || mkdir "$i"
         cd "$i"
     done
 }
-
+init_pkg(){
+    read dist dmy < /etc/issue
+    case "$dist" in
+        Debian)
+            sudo apt-get install ruby socat
+            ;;
+        Ubuntu)
+            sudo apt-get install socat
+            ;;
+        CentOS)
+            ;;
+        *);;
+    esac
+}
 dig_dir ~/.var cache
 /bin/rm cache/*.mar >/dev/null 2>&1
 dig_dir ~/.var/json
 /bin/rm *.json >/dev/null 2>&1
+dig_dir ~/bin
 mklink ~/ciax-xml/*
+init_bashrc
+init_pkg

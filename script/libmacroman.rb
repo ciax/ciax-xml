aa66a39c305d36cdc8426285d3bb73945b7d4d16
CIAX <ciax@debmini.subaru.nao.ac.jp>
2014-05-06 12:43:04 -1000

    adapt to Macro class;

--- script/libmacroman.rb
+++ script/libmacroman.rb
@@ -4,14 +4,14 @@ require "libmacroexe"
 
 module CIAX
   module Mcr
-    class Man < CIAX::Exe
+    class Man < Exe
       def initialize(port=nil)
         proj=ENV['PROJ']||'ciax'
         cfg=Config.new
         db=cfg[:db]=Mcr::Db.new.set(proj)
         cfg[:app]=App::List.new
         @list=List.new(proj).ext_file
-        self['sid']='' # response id
+        self['sid']='' # current session id
         super('mcr',db['id'],Command.new(cfg))
         ext_shell(@list)
       end
@@ -31,15 +31,14 @@ module CIAX
       def initialize(port=nil)
         super
         @cobj.ext_proc{|ent|
-          mobj=Exe.new(@cobj).fork(ent)
-          self['sid']=mobj.record['sid']
+          mobj=Macro.new(ent).fork
+          self['sid']=mobj[:sid]
           @list.data[self['sid']]=mobj
           "ACCEPT"
         }
-#        @cobj.save_procs{@list.save}
         @cobj.item_proc('interrupt'){|ent|
-          @list.data.each{|st|
-            st.thread.raise(Interrupt)
+          @list.data.each{|mobj|
+            mobj.thread.raise(Interrupt)
           }
           'INTERRUPT'
         }
@@ -48,10 +47,10 @@ module CIAX
         ig.set_proc{|ent|
           n=ent.par[0]||@list.data.keys.last||""
           self['sid']=n
-          if st=@list.data[n]
-            if st[:stat] == 'query'
-              st.cmd_que << ent.id
-              st.res_que.pop
+          if mobj=@list.data[n]
+            if mobj[:stat] == 'query'
+              mobj.ques.cmd << ent.id
+              mobj.ques.res.pop
             else
               "IGNORE"
             end
@@ -68,6 +67,7 @@ module CIAX
             'NOSID'
           end
         }
+        @post_procs << proc{@list.save}
         ext_server(port||@cobj.cfg[:db]['port']||55555)
       end
     end
@@ -82,8 +82,8 @@ module CIAX
 
       def clean
         res=nil
-        @data.each{|key,st|
-          unless st.thread.alive?
+        @data.each{|key,mobj|
+          unless mobj.thread.alive?
             @data.delete(key)
             res=1
           end
@@ -93,10 +93,10 @@ module CIAX
 
       def to_s
         page=[@caption]
-        @data.each{|key,st|
+        @data.each{|key,mobj|
           title="[#{key}]"
-          msg="#{st[:cid]} (#{st[:stat]})"
-          msg << "[#{st[:option]}]?" if st[:option]
+          msg="#{mobj[:cid]} (#{mobj[:stat]})"
+          msg << "[#{mobj[:option]}]?" if mobj[:option]
           page << Msg.item(title,msg)
         }
         page.join("\n")

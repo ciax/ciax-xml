9feb47b4e15d2e2d60a5041a1524b71e799952ea
Koji Omata <omata@naoj.org>
2014-05-13 22:22:33 -1000

    Layer,Site for List is spin off from ExtGrp and ExeList;

---
+++ script/liblayer.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/ruby
+require "libmsg"
+require "libcommand"
+
+module CIAX
+  module Layer
+    # Layer List
+    class List < Hashx
+      def initialize(cfg=Config.new)
+        @cfg=type?(cfg,Config)
+        @ljgrp=JumpGrp.new
+      end
+
+      def add_layer(layer)
+        type?(layer,Module)
+        str=layer.to_s.split(':').last
+        id=str.downcase
+        lst=(@cfg[id]||=layer::List.new(@cfg))
+        @ljgrp.add_item(id,str+" mode")
+        lst.init_procs << proc{|exe| exe.cobj.lodom.join_group(@ljgrp) }
+        self[id]=lst
+      end
+
+      def shell(site)
+        layer=keys.last
+        begin
+          self[layer][site].shell
+        rescue SiteJump
+          site=$!.to_s
+          retry
+        rescue LayerJump
+          layer=$!.to_s
+          retry
+        end
+      rescue InvalidID
+        $opt.usage('(opt) [id]')
+      end
+    end
+
+    class JumpGrp < Group
+      def initialize(upper=Config.new,crnt={})
+        super
+        @cfg['caption']='Switch Layer'
+        @cfg['color']=5
+        @cfg['column']=5
+        set_proc{|ent| raise(LayerJump,ent.id) }
+      end
+    end
+  end
+end
---
+++ script/libsite.rb
@@ -0,0 +1,69 @@
+#!/usr/bin/ruby
+require "libmsg"
+require "libcommand"
+require "libsitedb"
+
+module CIAX
+  module Site
+    # Site List
+    class List < Hashx
+      attr_reader :init_procs
+      # shdom: Domain for Shared Command Groups
+      def initialize(upper=Config.new)
+        upper[:ldb]||=Db.new
+        @cfg=Config.new(upper)
+        # initialize exe (mostly add new menu) at new key generated
+        @init_procs=[proc{|exe| exe.cobj.lodom.add_group(:group_class =>JumpGrp)}]
+        $opt||=GetOpts.new
+      end
+
+      def [](key)
+        if key?(key)
+          super
+        else
+          exe=self[key]=new_val(key)
+          @init_procs.each{|p| p.call(exe)}
+          exe
+        end
+      end
+
+      def server(ary)
+        ary.each{|i|
+          sleep 0.3
+          self[i]
+        }.empty? && self[nil]
+        sleep
+      rescue InvalidID
+        $opt.usage('(opt) [id] ....')
+      end
+
+      def shell(site)
+        @site=site||@site
+        begin
+          self[@site].shell
+        rescue SiteJump
+          @site=$!.to_s
+          retry
+        end
+      rescue InvalidID
+        $opt.usage('(opt) [id]')
+      end
+
+      private
+      # For generate Exe (allows nil)
+      def new_val(key)
+      end
+    end
+
+    class JumpGrp < Group
+      def initialize(upper,crnt={})
+        super
+        @cfg['caption']='Switch Sites'
+        @cfg['color']=5
+        @cfg['column']=2
+        update_items(@cfg[:ldb].list)
+        set_proc{|ent| raise(SiteJump,ent.id)}
+      end
+    end
+  end
+end

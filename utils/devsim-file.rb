bbb37621a4dd5f74836525e2c3e903b9c21c547d
Koji Omata <omata@naoj.org>
2014-06-08 09:22:02 -1000

    use devsim-file for simulate;

--- utils/devsim-file.rb
+++ utils/devsim-file.rb
@@ -1,17 +1,17 @@
 #!/bin/bash
+getcmd(){
+    read input
+    echo "$input"|base64
+}
 [ "$1" ] ||{  echo "Usage:devsim-file [site]";exit 1; }
 site=$1;shift
-tty=$(tty)
-read input < $tty
-cmd=$(echo -n "$input"|base64)
+cmd="$(getcmd)"
 while read line ;do
     if [ "$cmd" ]; then
-        echo $line
         [[ "$line" =~ "$cmd" ]] && unset cmd
     else
         echo "$line"
-        read input < $tty
-        cmd=$(echo -n "$input"|base64)
+        cmd="$(getcmd)"
     fi
-done <  ~/.var/stream_${site}_2013.log
-echo "No find $cmd"
+done <  ~/.var/stream_${site}_*.log
+echo "No find $cmd" > /dev/stderr

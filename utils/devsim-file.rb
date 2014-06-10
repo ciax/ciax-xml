0f844d5d87d4b2a67af3157741c811615e487030
CIAX <ciax@leonid.subaru.nao.ac.jp>
2014-06-09 15:59:07 -1000

    use head -1 instead of read;

--- utils/devsim-file.rb
+++ utils/devsim-file.rb
@@ -1,8 +1,6 @@
 #!/bin/bash
 getcmd(){
-    read input
-    cmd=$(echo "$input"|base64)
-    echo "$cmd" >/dev/stderr
+    cmd=$(head -1|base64|tr -d '=')
 }
 [ "$1" ] ||{  echo "Usage:devsim-file [site]";exit 1; }
 site=$1;shift
@@ -10,7 +8,7 @@ getcmd
 while read -u 3 line ;do
     if [ "$cmd" ]; then
         [[ "$line" =~ "$cmd" ]] && unset cmd
-    else
+    elif [[ "$line" =~ rcv ]]; then
         echo "$line"
         getcmd
     fi

8c2393152c96022e9795f6bfdc348695f7de03f5
CIAX <ciax@leonid.subaru.nao.ac.jp>
2014-06-16 11:46:09 -1000

    devsim-file/add timelimit for search command;

--- utils/devsim-file.sh
+++ utils/devsim-file.sh
@@ -15,19 +15,30 @@ warn(){
 site=$1;shift
 input=$(input64)
 search=0
+num=0
 warn "Init"
 trap 'warn "SIGINT"' 2
 while [ $search -lt 2 ]  ;do
     while read -u 3 line ;do
         jset $line
         if [ "$input" ]; then
-            [[ "$base64" =~ "$input" ]] && unset input
+            if [[ "$dir" =~ snd ]] ; then
+                num=$(( $num + 1 ))
+                if [ $num -gt 10000 ] ; then
+                    warn "Timeout for $input"
+                    unset input
+                    num=0
+                fi
+                [[ "$base64" =~ "$input" ]] && unset input
+            fi
         elif [[ "$dir" =~ rcv ]]; then
             echo -n "$base64"|base64 -d
             input=$(input64) || exit 1
             search=0
+            num=0
         fi
     done 3< <(grep -h . ~/.var/stream_${site}_*.log)
+    warn "Pass <$search>"
     search=$(( $search + 1 ))
 done
 warn "No find $cmd"

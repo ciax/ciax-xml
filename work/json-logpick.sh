faa73f2df03a13776d6d42fb25937437a7c9e4b5
CIAX <ciax@leonid.subaru.nao.ac.jp>
2014-06-20 11:56:32 -1000

    json-logpick was moved;

--- work/json-logpick.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-[ "$1" = '-s' ] && { dir='snd';shift; }
-[ "$1" ] || { echo "Usage: ${0##*/} (-s:snd) [site]"; exit 1; }
-id=$1;shift
-IFS=':' cmd="$*"
-input="$HOME/.var/stream_${id}_*.log"
-for i in $input;do
-    egrep -h "${dir:-rcv}" $i|egrep "${cmd:-.}"
-done | tail -1

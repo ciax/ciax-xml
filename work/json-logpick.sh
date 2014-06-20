169aef6a0d1619f5888dc86065822ed153e6b015
CIAX <ciax@leonid.subaru.nao.ac.jp>
2014-06-20 11:35:00 -1000

    update/ remove deprecated libdb.sh;

--- work/json-logpick.sh
+++ work/json-logpick.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-. ~/lib/libdb.sh entity
 [ "$1" = '-s' ] && { dir='snd';shift; }
+[ "$1" ] || { echo "Usage: ${0##*/} (-s:snd) [site]"; exit 1; }
 id=$1;shift
 IFS=':' cmd="$*"
-setfld $id || _usage_key '(-s)' "[cmd]"
 input="$HOME/.var/stream_${id}_*.log"
 for i in $input;do
     egrep -h "${dir:-rcv}" $i|egrep "${cmd:-.}"
-done | tail -1 | grep .
+done | tail -1

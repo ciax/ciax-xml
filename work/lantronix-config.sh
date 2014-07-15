395b22c248480bf6e788350bef91c3062315418d
CIAX <ciax@leonid.subaru.nao.ac.jp>
2014-07-14 16:44:10 -1000

    correct db path;

--- work/lantronix-config.sh
+++ work/lantronix-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # use nc for input to lantronix (i.e. ltcfg id | nc host 23)
-ltf=~/db/DB-ltx.csv
-rsf=~/db/DB-rs.csv
+ltf=$XMLPATH/DB-ltx.csv
+rsf=$XMLPATH/DB-rs.csv
 [ "$1" ] || {
     echo "Usage:ltcfg [id] (range)"
     echo -n "   "

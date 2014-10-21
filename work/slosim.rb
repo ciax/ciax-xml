9545532ea6d60a354487c654082c872c1796b584
CIAX <ciax@naoj.org>
2014-10-20 22:56:13 -1000

    slosim/ simulate mma; ldb-all/ add mma dummy as sdm; adb-mma/ correct command (ini -> jn, etc.)

--- work/slosim.rb
+++ work/slosim.rb
@@ -1,49 +1,97 @@
 #!/usr/bin/ruby
-Thread.abort_on_exception=true
-$target=0
-$pulse=0
-Thread.new{
-  loop{
-    sleep 0.2
-    diff=$target-$pulse
-    if diff!=0
-      $pulse+=diff/diff.abs
-      if $pulse > 999
-        $pulse-=10000
-      elsif $pulse < 0
-        $pulse+=10000
+require 'gserver'
+
+class Slosyn < GServer
+  def initialize(port=10001,*args)
+    super(port,*args)
+    Thread.abort_on_exception=true
+    @target=0
+    @pulse=0
+    @bs=0
+    Thread.new{
+      loop{
+        sleep 0.2
+        diff=@target-@pulse
+        if diff!=0
+          @bs=1
+          @pulse+=diff/diff.abs
+          if @pulse > 999
+            @pulse-=10000
+          elsif @pulse < 0
+            @pulse+=10000
+          end
+        else
+          @bs=0
+        end
+      }
+    }
+  end
+
+  def serve(io)
+    while str=io.gets("\r").chomp
+      case str
+      when /^abspos=/
+        @target=@pulse=set(str)
+      when /^p=/
+        @target=@pulse=set(str)
+      when /^ma=/
+        @target=set(str)
+      when /^mi=/
+        @target+=set(str)
+      when 'j=1'
+        @target=200
+      when 'j=-1'
+        @target=0
+      when 'stop'
+        @target=@pulse
+      when 'in1'
+        io.print within(122.5,123.5)
+        next
+      when 'in2'
+        io.print within(12.3,13.2)
+        next
+      when 'in3'
+        io.print within(200,201)
+        next
+      when 'in4'
+        io.print within(0,0.5)
+        next
+      when 'in5'
+        io.print within(184.5,185.5)
+        next
+      when 'spd'
+        io.print '0.1'
+        next
+      when 'err'
+        io.print '0'
+        next
+      when 'bs'
+        io.print @bs
+        next
+      when 'p'
+        io.print "%.1f" % (@pulse.to_f/10)
+        next
       end
+      io.print ">"
     end
-  }
-}
+  end
 
-def sett(n)
-  if n > 9999
-    $target=9999
-  elsif n < 0
-    $target=0
-  else
-    $target=n
+  def set(str)
+    a=str.split('=')
+    num=(a[1].to_f*10).to_i
+    if num > 9999
+      num=9999
+    elsif num < 0
+      num=0
+    end
+    num
+  end
+
+  def within(a,b)
+    (@pulse <= b && @pulse >= a) ? '1' : '0'
   end
 end
 
-while a=gets
-  b=a.split('=')
-  num=(b[1].to_f*10).to_i
-  case b[0]
-  when 'abspos'
-    sett($pulse=num)
-  when 'p'
-    sett($pulse=num)
-  when 'ma'
-    sett(num)
-  when 'mi'
-    sett($target+num)
-  when 'stop'
-    sett($pulse)
-  when /^q/
-    break
-  else
-    puts "%.1f" % ($pulse.to_f/10)
-  end
-end
+sv=Slosyn.new(*ARGV)
+sv.start
+sleep

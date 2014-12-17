9ff4d79cd029bac0417b1217f92e15af431f56b6
CIAX <ciax@leonid.subaru.nao.ac.jp>
2014-12-16 15:12:40 -1000

    marshal-view/ include lacking class (CmdList);

--- utils/marshal-view.rb
+++ utils/marshal-view.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
-require "libenumx"
+# libcommand includes both Enumx and CmdList
+require "libcommand"
+
 abort "Usage: m2s marshal_file" if STDIN.tty? && ARGV.size < 1
 
 puts Marshal.load(gets(nil)).extend(CIAX::Enumx)

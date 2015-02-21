4d16f6ebe8528d9035f4c0c9c7afee19adb8b889
CIAX <ciax@leonid.subaru.nao.ac.jp>
2015-02-20 14:37:11 -1000

    correct Hex::List;

--- script/libhexlist.rb
+++ script/libhexlist.rb
@@ -6,9 +6,8 @@ require "libwatlist"
 module CIAX
   module Hex
     class List < Site::List
-      def initialize(layer_cfg={})
-        layer_cfg[:layer_db]=Ins::Db.new
-        super(Hex,layer_cfg)
+      def initialize(inter_cfg)
+        super(Hex,{:layer_db => Ins::Db.new},inter_cfg)
         @cfg.layers[:hex]=self
       end
     end
@@ -18,8 +17,13 @@ module CIAX
     if __FILE__ == $0
       ENV['VER']||='initialize'
       GetOpts.new('chset')
+      cfg=Config.new('test')
+      cfg[:site_stat]=Prompt.new
+      Frm::List.new(cfg)
+      App::List.new(cfg)
+      Wat::List.new(cfg)
       begin
-        puts List.new.shell(ARGV.shift)
+        puts List.new(cfg).shell(ARGV.shift)
       rescue InvalidID
         $opt.usage('(opt) [id]')
       end

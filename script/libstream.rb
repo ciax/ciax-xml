8feca241268cb117ba664bc31753b65afc7edf17
CIAX <ciax@leonid.subaru.nao.ac.jp>
2015-03-11 12:39:50 -1000

    Stream#reopen() just open stream, not include block;

--- script/libstream.rb
+++ script/libstream.rb
@@ -25,15 +25,15 @@ module CIAX
       Signal.trap(:CHLD){
         verbose("Client","#@iocmd is terminated")
       }
+      reopen
     end
 
     def snd(str,cid)
       return if str.to_s.empty?
       verbose("Client","Sending #{str.size} byte on #{cid}")
       verbose("Client","Binary Sending",str.inspect)
-      reopen{
-        @f.write(str)
-      }
+      reopen
+      @f.write(str)
       convert('snd',str,cid)
       self
     end
@@ -42,16 +42,14 @@ module CIAX
       verbose("Client","Wait to Recieve #{@wait} sec")
       sleep @wait
       verbose("Client","Wait for Recieving")
-      unless str=reopen{
-          if IO.select([@f],nil,nil,@timeout)
-            @f.sysread(4096)
-          else
-            @f.close
-            Msg.com_err("Stream:No response")
-          end
-        }
-        Process.kill(1,@f.pid)
+      reopen
+      if IO.select([@f],nil,nil,@timeout)
+        str=@f.sysread(4096)
+      else
+        @f.close
+        Msg.com_err("Stream:No response")
       end
+#        Process.kill(1,@f.pid)
       verbose("Client","Recieved #{str.size} byte on #{self['cmd']}")
       verbose("Client","Binary Recieving",str.inspect)
       convert('rcv',str)
@@ -61,16 +59,15 @@ module CIAX
     def reopen
       int=0
       begin
-        openstrm unless @f
+        openstrm if !@f || @f.closed?
       rescue
         warn $!
-        Msg.str_err("Stream Open failed") if int > 1
+        Msg.str_err("Stream Open failed") if int > 2
         warning("Client","Try to reopen")
         sleep int
         int=(int+1)*2
         retry
       end
-      yield
     end
 
     private

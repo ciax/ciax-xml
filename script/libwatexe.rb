4ac5a0f249053c0bceb3d50767bc8f4b9f11ab27
CIAX <ciax@leonid.subaru.nao.ac.jp>
2015-03-12 09:33:37 -1000

    Wat::Exe#auto_update()/ need Event#upd after queueing;

--- script/libwatexe.rb
+++ script/libwatexe.rb
@@ -106,7 +106,7 @@ module CIAX
           if @event.get('exec').empty?
             verbose("Auto","Update(#{@ash.stat['time']})")
             begin
-              @event.queue('auto',3,[['upd']]).exec
+              @event.queue('auto',3,[['upd']]).upd.exec
             rescue InvalidID
               errmsg
             rescue

a84f331d73c5f3191e560164e6b48512d399bbf2
CIAX <ciax@leonid.subaru.nao.ac.jp>
2015-04-28 19:09:21 -1000

    using list-db,list-item;

--- work/exadbc.sh
+++ work/exadbc.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 xmldir=~/ciax-xml
 appcmd=$xmldir/script/libappcmd.rb
-list(){
-    for f in $xmldir/adb-*.xml; do
-        a=${f%.*}
-        echo ${a##*-}
-    done
-}
-
 show(){
     for site ;do
         echo "$C2#### $site ####$C0"
-        while read dmy cmd dmy2; do
-            eval $cmd
+        for id in $(list-item adb $site); do
             echo "$C3$id$C0"
             $appcmd $site $id 1 1
-        done < <(grep '<item' $xmldir/adb-$site.xml)
+        done
     done
 }
 out=`mktemp`
 trap "rm $out" EXIT
 $appcmd $* >$out 2>&1
 case "$?$2:$1" in
-    1:) show `list`;;
+    1:) show `list-db adb`;;
     2:*) show $1;;
     0*) cat $out;;
     *) $appcmd $*;;

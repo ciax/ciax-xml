d39c2ee27ac0d0940d1990a7b27bf3931db11441
Koji Omata <omata@naoj.org>
2015-06-15 14:34:24 -1000

    update exidbs for status display;

--- work/exidbs.sh
+++ work/exidbs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-. ~/lib/libdb.sh entity
 [[ "$1" == -* ]] && { opt=$1; shift; }
 [ "$opt" ] && rm ~/.var/json/status_*
 ids=${1:-`ls ~/.var/json/field_???.json|cut -d_ -f2|cut -d. -f1`};shift
@@ -12,7 +11,7 @@ for id in $ids; do
     then
         json_view <$stat
     else
-        ~/lib/libappview.rb < $stat
+        libappview < $stat
     fi
     read -t 0 && break
 done

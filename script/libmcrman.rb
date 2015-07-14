8bae664eea207a8acb30a250ac8ba31b117d3835
Koji Omata <omata@naoj.org>
2015-07-13 15:01:27 -1000

    Mcr::Man: adapt to new conf;

--- script/libmcrman.rb
+++ script/libmcrman.rb
@@ -5,83 +5,82 @@ require "libmcrlist"
 module CIAX
   module Mcr
     module Man
-      def self.new(inter_cfg=Config.new('mcr'))
+      def self.new(cfg=Config.new('mcr'))
         if $opt['l']
           $opt.delete('l')
-          inter_cfg['host']='localhost'
-          Sv.new(inter_cfg)
-          Cl.new(inter_cfg)
+          cfg['host']='localhost'
+          Sv.new(cfg)
+          Cl.new(cfg)
         elsif $opt['c'] || $opt['h']
-          Cl.new(inter_cfg)
+          Cl.new(cfg)
         elsif $opt['t']
-          Exe.new(inter_cfg)
+          Exe.new(cfg)
         else
-          Sv.new(inter_cfg)
+          Sv.new(cfg)
         end
       end
 
-      class Exe < Exe
-        def initialize(inter_cfg)
-          id=ENV['PROJ']||'ciax'
-          type?(inter_cfg,Config)
-          Wat::List.new(inter_cfg) unless inter_cfg.layers.key?(:wat)
-          inter_cfg[:db]||=Db.new.get(id)
-          super(id,inter_cfg)
-          @cobj.add_extgrp
+      class Exe < CIAX::Exe
+        # cfg should have [:jump_groups],[:sub_list](App::List)
+        def initialize(cfg,attr={})
+          proj=ENV['PROJ']||'ciax'
+          type?(cfg,Config)
+          @output=@list=List.new(cfg).ext_shell
+          super(proj,@list.cfg)
+          @cobj=Index.new(@cfg)
+          @cobj.add_rem.add_hid
+          @cobj.rem.add_ext(Db.new.get(proj))
           @cobj.rem.add_int
-          lc=inter_cfg[:list_class]||List
-          @output=@list=lc.new(@cobj)
-          @valid_pars=@cobj.intgrp.valid_pars
-          ext_shell
-          @cobj.loc.add(@list.jumpgrp)
-          @post_exe_procs << proc{
-            @valid_pars.replace(@list.keys)
+          @cfg[:submcr_proc]=proc{|args,id|
+            @list.add(@cobj.set_cmd(args),id)
           }
-          # View Seq mode
-          @smode=false
+          @valid_pars=@cobj.rem.int.valid_pars
+          ext_shell
+          @post_exe_procs << proc{
+#            @valid_pars.replace(@list.keys)
+          }
         end
 
         private
         def ext_shell
           super
           @current=@lastsize=0
-          @prompt_proc=proc{
-            ("[%d]" % @list.index)+optlist(@list.option)
-          }
+#          @prompt_proc=proc{
+#            ("[%d]" % @list.index)+optlist(@list.option)
+#          }
           @post_exe_procs << proc{
-            @output=@smode ? @list.output : @list
+            @output=@list.to_v
           }
-          @shell_input_proc=proc{|args| @list.conv_cmd(args,@cobj.intgrp)}
-          vg=@cobj.loc.add('caption'=>"Change View Mode",'color' => 9)
+#          @shell_input_proc=proc{|args| @list.conv_cmd(args,@cobj.intgrp)}
+          vg=@cobj.loc.add_view
           vg.add_item('lst',"List mode").def_proc{@smode=false;''}
           vg.add_item('seq',"Sequencer mode").def_proc{@smode=true;''}
-          vg.add_item('vis',"Visual mode").def_proc{@output.vmode='v';''}
-          vg.add_item('raw',"Raw mode").def_proc{@output.vmode='r';''}
+          vg.get('vis').def_proc{@output.vmode='v';''}
+          vg.get('raw').def_proc{@output.vmode='r';''}
           self
         end
       end
 
       class Cl < Exe
-        def initialize(inter_cfg)
-          inter_cfg[:list_class]=ClList
+        def initialize(cfg)
+          cfg[:list_class]=ClList
           super
-          host=inter_cfg['host']||@cobj.cfg[:db]['host']||'localhost'
-          port=inter_cfg['port']||@cobj.cfg[:db]['port']||55555
+          host=cfg['host']||@cobj.cfg[:dbi]['host']||'localhost'
+          port=cfg['port']||@cobj.cfg[:dbi]['port']||55555
           @pre_exe_procs << proc{@list.upd}
           ext_client(host,port)
         end
       end
 
       class Sv < Exe
-        def initialize(inter_cfg)
-          inter_cfg[:list_class]=SvList
-          inter_cfg[:submcr_proc]=proc{|args,src| exe(args,src)}
+        def initialize(cfg)
+          cfg[:submcr_proc]=proc{|args,src| exe(args,src)}
           super
-          port=inter_cfg['port']||@cobj.cfg[:db]['port']||55555
+          port=cfg['port']||@cobj.rem.ext.cfg[:dbi]['port']||55555
           self['sid']='' # For server response
           @pre_exe_procs << proc{ self['sid']='' }
           # Internal Command Group
-          @cobj.intgrp.def_proc{|ent|
+          @cobj.rem.int.def_proc{|ent|
             sid=ent.par[0]
             if sobj=@list.get(sid)
               self['sid']=sid
@@ -92,8 +91,7 @@ module CIAX
           }
           # External Command Group
           @cobj.rem.ext.def_proc{|ent|
-            mobj=@list.add_ent(ent).lastval
-            self['sid']=mobj.id
+            @list.add(ent).fork
             "ACCEPT"
           }
           @cobj.get('interrupt').def_proc{|ent|
@@ -107,7 +105,11 @@ module CIAX
       if __FILE__ == $0
         GetOpts.new('mnlrt')
         begin
-          Man.new.shell
+          cfg=Config.new
+          cfg[:jump_groups]=[]
+          wl=Wat::List.new(cfg)
+          cfg[:sub_list]=wl.cfg[:sub_list] #Take App List
+          Man.new(cfg).shell
         rescue InvalidCMD
           $opt.usage("[mcr] [cmd] (par)")
         end

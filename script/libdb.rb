f778bacbfa474d65a8396b5e6fbca0c67d3dc5b9
Koji Omata <omata@naoj.org>
2015-09-22 22:09:04 -1000

    Db: show wrong id in case of No ID error;

--- script/libdb.rb
+++ script/libdb.rb
@@ -37,7 +37,7 @@ module CIAX
     end
 
     def get(id)
-      raise(InvalidID,"No ID in #@type\n"+@displist.to_s) unless @displist.key?(id)
+      raise(InvalidID,"No such ID (#{id}) in #@type\n"+@displist.to_s) unless @displist.key?(id)
       cache(id){|doc|
         doc_to_db(doc.set(id))
       }

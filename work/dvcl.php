ca5f1b5cc1ef89ecabdb132f8120b92dd81b5a66
MOIRCS user <moircs@moircsobcp.sum.subaru.nao.ac.jp>
2015-09-26 18:27:05 -1000

    add php for web app;

---
+++ work/dvcl.php
@@ -0,0 +1,12 @@
+<?php
+$port=(int)$_POST['port'];
+$cmd=$_POST['cmd'];
+$soc=socket_create(AF_INET,SOCK_DGRAM,SOL_UDP);
+$msg='["'+$cmd+'"]';
+$len=strlen($msg);
+socket_sendto($soc,$msg,$len,0,"127.0.0.1",$port);
+$from=""
+socket_recvfrom($soc,$buf,12,0,$from,$port)
+print($buf);
+print("OK");
+?>

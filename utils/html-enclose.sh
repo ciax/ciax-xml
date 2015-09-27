149e00516795896c71092533a45e1c8418bb12f0
Koji Omata <omata@naoj.org>
2015-09-27 11:46:45 -1000

    add html enclosure part;

---
+++ utils/html-enclose.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+JQUERY=1.7.2
+cat <<EOF
+<html>
+<head>
+<title>CIAX-XML</title>
+<link rel="stylesheet" type="text/css" href="ciax-xml.css" />
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script type="text/javascript">var Type="status",Site="$id";</script>
+<script type="text/javascript" src="ciax-xml.js"></script>
+</head>
+<body>
+EOF
+cat
+cat <<EOF
+</body>
+</html>
+EOF

e755d24e736b33b99531ae369704e3ea9cb3a5e6
Koji Omata <omata@naoj.org>
2015-09-28 01:54:29 -1000

    debug: now dvctl-udp.php works;

--- webapp/dvctl-udp.php
+++ webapp/dvctl-udp.php
@@ -1,17 +1,30 @@
 <?php
 function getarg($key){
-  $res=$_POST[$key] ;
-  $res=($res) ? $res : $_GET[$key];
+  global $args;
+  $res=(isset($_POST[$key])) ? $_POST[$key] :
+    (
+      (isset($_GET[$key])) ? $_GET[$key] :
+        (
+          (isset($args[$key])) ? $args[$key] : null
+        )
+    );
   return $res;
+}
+$args=array();
+foreach($argv as &$e){
+  $ary=split("=",$e);
+  if(count($ary)>1){
+    $args[$ary[0]]=$ary[1];
+  }
 }
 $port=(int)getarg('port');
 $cmd=getarg('cmd');
 $soc=socket_create(AF_INET,SOCK_DGRAM,SOL_UDP);
-$msg='["'+$cmd+'"]';
-$len=strlen($msg);
-socket_sendto($soc,$msg,$len,0,"127.0.0.1",$port);
-$from=""
-socket_recvfrom($soc,$buf,12,0,$from,$port)
+$buf='["'.$cmd.'"]';
+$len=strlen($buf);
+socket_sendto($soc,$buf,$len,0,"127.0.0.1",$port);
+$from="";
+$port=0;
+socket_recvfrom($soc,$buf,128,0,$from,$port);
 print($buf);
-print("OK");
 ?>

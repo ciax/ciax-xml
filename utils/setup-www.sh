08bdfacd65f87de6ed9c02bb7d252d2a45b4ec29
Koji Omata <omata@naoj.org>
2015-09-28 17:10:32 -1000

    rm unused file;

--- utils/setup-www.sh
+++ utils/setup-www.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 src=$HOME/ciax-xml/webapp
 dir=$HOME/.var/json
+for i in $dir/*; do
+    [ -L "$i" -a ! -e "$i" ] && rm "$i"
+done
 ln -sf $src/*  $dir/
 sudo ln -sf ~/ciax-xml /var/www/html
 sudo ln -sf ~/.var/json /var/www/html

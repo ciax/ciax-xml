57c41cd6f4d680eed21b095d9d082b0b10894acc
Koji Omata <omata@naoj.org>
2015-10-05 18:34:41 -1000

    refactoring: Msg#varbose allows Array param;

--- script/libmsg.rb
+++ script/libmsg.rb
@@ -11,17 +11,19 @@ module CIAX
     attr_accessor :cls_color
     Start_time = Time.now
     @indent_base = 1
-    # block takes array (shown by each line)
+    # block takes array (shown by each line) or string
     # Description of values
     #   [val] -> taken from  xml (criteria)
     #   <val> -> taken from status (incoming)
     #   (val) -> calcurated from status
     def verbose(cond = true)
-      return if !ENV['VER'] || !cond
-      msg, data = make_title(yield)
-      return unless @show_inside || condition(msg)
-      prt_lines(msg)
-      true
+      return if !ENV['VER'] || !cond || @hide_inside
+      data = yield
+      (data.is_a?(Array) ? data : [data]).map do|line|
+        msg = make_msg(line)
+        next unless condition(msg)
+        prt_lines(msg)
+      end.compact.empty?
     end
 
     def warning(title)
@@ -38,37 +40,27 @@ module CIAX
       Kernel.warn make_msg(Msg.color("#{$ERROR_INFO} at #{$ERROR_POSITION}", 1))
     end
 
+    # @hide_inside is flag for hiding inside of enclose
     def enclose(title1, title2)
-      @show_inside = verbose { title1 }
+      @hide_inside = verbose { title1 }
       Msg.ver_indent(1)
       res = yield
     ensure
       Msg.ver_indent(-1)
       verbose { Kernel.format(title2, res) }
-      @show_inside = false
+      @hide_inside = false
     end
-
-    # Private Method
 
     private
 
-    def make_title(title)
-      if title.is_a? Array
-        data = title
-        title = data.shift
-      else
-        data = []
-      end
-      [make_msg(title), data]
-    end
-
     def prt_lines(data)
-      ind=0
-      base=Msg.ver_indent
+      ind = 0
+      base = Msg.ver_indent
       data.each_line do|line|
-        Kernel.warn Msg.indent(base+ind) + line
-        ind=2
+        Kernel.warn Msg.indent(base + ind) + line
+        ind = 2
       end
+      true
     end
 
     def make_msg(title)
@@ -109,12 +101,17 @@ module CIAX
     # VER= makes setenv "" to VER otherwise nil
     def condition(msg)
       return if !ENV['VER'] || !msg
-      return true if Regexp.new('\*').match(ENV['VER'])
+      return true if match_all
+      title = msg.split("\n").first.upcase
       ENV['VER'].split(',').any? do|s|
         s.split(':').all? do|e|
-          msg.upcase.include?(e.upcase)
+          title.include?(e.upcase)
         end
       end
+    end
+
+    def match_all
+      Regexp.new('\*').match(ENV['VER'])
     end
 
     def self.ver_indent(add = 0)

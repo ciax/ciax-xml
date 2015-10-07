a020d602e524b0e38d902e4216419cdf994dacfb
Koji Omata <omata@naoj.org>
2015-10-07 12:50:03 -1000

    refactoring;

--- work/json_getval.rb
+++ work/json_getval.rb
@@ -1,31 +1,33 @@
 #!/usr/bin/ruby
 # Get Value from JSON file
 require 'json'
+def _ga(h, k)
+  if k == '0' || k.to_i > 0
+    return get(h[k.to_i], idx) if k.to_i <= h.size
+    warn('Out of range')
+  else
+    warn('Not number')
+  end
+  []
+end
+
+def _gh(h, k)
+  return get(h[k], idx) if h.key?(k)
+  warn("No such key [#{k}]")
+  h.keys
+end
+
 def get(h, idx)
   return h if idx.empty?
   k = idx.shift
   case h
   when Array
-    if k == '0' || k.to_i > 0
-      if k.to_i > h.size
-        warn('Out of range')
-      else
-        return get(h[k.to_i], idx)
-      end
-    else
-      warn('Not number')
-    end
+    return _ga(h, k)
   when Hash
-    if h.key?(k)
-      return get(h[k], idx)
-    else
-      warn("No such key [#{k}]")
-      return h.keys
-    end
+    return _gh(h, k)
   else
     [h]
   end
-  []
 end
 
 abort 'Usage: json_getval [key:idx] json_file' if STDIN.tty? && ARGV.size < 2

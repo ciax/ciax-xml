19f2c63117e274e2de71c6822e3d93f607de38e2
Koji Omata <omata@naoj.org>
2015-10-07 20:24:45 -1000

    refactoring: fpsim;

--- simulator/fpsim.rb
+++ simulator/fpsim.rb
@@ -54,34 +54,43 @@ class FPIO < GServer
   end
 
   def serve(io)
-    while str = io.gets("\r")
+    while (str = io.gets("\r"))
       sleep 0.1
       warn str
-      case str
-      when /^>02!JCD/
-        base = @output.to_x + @output.xbcc
-      when /^>03!JCE/
-        base = @input.to_x + @input.xbcc
-      when /^>02!L/
-        base = nil
-        cmask = $'[0, 4].hex
-        data = $'[4, 4].hex
-        @output.mask(cmask, data)
-        @drvtbl.each_with_index do|p, i|
-          if @input[i] != @output[p]
-            Thread.new do
-              sleep (i < 4 ? 1 : 0)
-              @input[i] = @output[p]
-            end
-          end
-        end
-      end
-      res = "A#{base}\r"
+      res = dispatch(str)
       io.print res
       warn res
     end
   rescue
     warn $ERROR_INFO
+  end
+
+  private
+
+  def dispatch(str)
+    case str
+    when /^>02!JCD/
+      base = @output.to_x + @output.xbcc
+    when /^>03!JCE/
+      base = @input.to_x + @input.xbcc
+    when /^>02!L/
+      base = nil
+      manipulate($')
+    end
+    format("A%s\r", base)
+  end
+
+  def manipulate(par)
+    cmask = par[0, 4].hex
+    data = par[4, 4].hex
+    @output.mask(cmask, data)
+    @drvtbl.each_with_index do|p, i|
+      next if @input[i] == @output[p]
+      Thread.new do
+        sleep(i < 4 ? 1 : 0)
+        @input[i] = @output[p]
+      end
+    end
   end
 end
 

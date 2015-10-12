3d403b379b48b3e8578b74335e9a01a081ff826b
Koji Omata <omata@naoj.org>
2015-10-12 12:54:46 -1000

    json_logpick: takes number of candidate;

--- utils/json_logpick.sh
+++ utils/json_logpick.sh
@@ -2,8 +2,9 @@
 [ "$1" = '-s' ] && { dir='snd';shift; }
 [ "$1" ] || { echo "Usage: ${0##*/} (-s:snd) [site] (cmd:par)"; exit 1; }
 id=$1;shift
-IFS=':' cmd="$*"
+cmd=$1;shift
+num=$1;shift
 input="$HOME/.var/log/stream_${id}_*.log"
 for i in $input;do
     egrep -h "${dir:-rcv}" $i|egrep "${cmd:-.}"
-done | tail -1
+done | tail -${num:-1} | head -1

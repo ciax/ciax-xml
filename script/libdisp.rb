df70cf480aa8beb1225ed7dec20c541fbabf7f59
Koji Omata(pingu) <koji.omata@gmail.com>
2015-10-30 00:56:47 -1000

    Disp: use keyword argument;

--- script/libdisp.rb
+++ script/libdisp.rb
@@ -3,7 +3,7 @@ require 'libenumx'
 # Display: Sortable Caption Database (Value is String)
 #   New feature: can make recursive groups
 #   Shows visiual command list categorized by sub-group
-#   Holds valid command list in @select
+#   Holds valid command list in @valid
 #   Used by Command and XmlDoc
 module CIAX
   # Index of Display (Used for validation, display)
@@ -13,36 +13,35 @@ module CIAX
     #   Attributes (one level): child(module), color(#), indent(#)
     #   Attributes (one group): caption(text), members(array)
     SEPTBL = [['****', 2], ['===', 6], ['--', 9], ['_', 14]]
-    attr_reader :select, :atrb
-    def initialize(atrb = { column: 2 }, select = [])
-      @atrb = atrb
-      @select = select
+    attr_reader :valid
+    def initialize(valid = [])
+      @valid = valid
     end
 
     def put(k, v)
-      @select << k
+      @valid << k
       super
     end
 
     # For ver 1.9 or more
     def sort!
-      @select.sort!
+      @valid.sort!
       self
     end
 
-    # Reset @select(could be shared)
+    # Reset @valid(could be shared)
     def reset!
-      @select.concat(keys).uniq!
+      @valid.concat(keys).uniq!
       self
     end
 
     def clear
-      @select.clear
+      @valid.clear
       super
     end
 
     def delete(id)
-      @select.delete(id)
+      @valid.delete(id)
       super
     end
 
@@ -50,14 +49,14 @@ module CIAX
       view
     end
 
-    def view(select = @select, level = 0, cap = nil)
+    def view(select: @valid, level: 0, cap: nil, column: 2, line_number: false)
       list = {}
-      (@select & select).compact.sort.each_with_index do|id, num|
-        title = @atrb[:line_number] ? "[#{num}](#{id})" : id
+      (@valid & select).compact.sort.each_with_index do|id, num|
+        title = line_number ? "[#{num}](#{id})" : id
         list[title] = self[id]
       end
       return if list.empty?
-      columns(list, @atrb[:column], level, mk_caption(cap, level))
+      columns(list, column, level, mk_caption(cap, level))
     end
 
     def mk_caption(cap, level)
@@ -70,16 +69,18 @@ module CIAX
     # Parent of Group
     class Section < Hashx
       attr_accessor :index
-      def initialize(index, cap = nil, level = nil)
+      def initialize(index: Disp.new, cap: nil, level: nil, column: 2, line_number: false)
         @index = type?(index, Disp)
         @caption = cap
         @level = level || -1
+        @column = column
+        @line_number = line_number
       end
 
       # add sub caption if sub is true
       def put(id, cap = nil, sub = nil)
         mod = sub ? Section : Group
-        self[id] = mod.new(@index, cap, @level + 1)
+        self[id] = mod.new(index: @index, cap: cap, level: @level + 1, column: @column, line_number: @line_number)
       end
 
       def view
@@ -112,38 +113,39 @@ module CIAX
     # It has members of item
     class Group < Arrayx
       attr_accessor :index
-      def initialize(index, cap = nil, level = nil)
+      def initialize(index: Disp.new, cap: nil, level: nil, column: 2, line_number: false)
         @index = type?(index, Disp)
         @caption = cap
         @level = level || 0
+        @column = column
+        @line_number = line_number
       end
 
       # add item
       def put(k, v)
         push k
-        @index.select << k
+        @index.valid << k
         @index[k] = v
       end
 
       def view
-        @index.view(self, @level, @caption)
+        @index.view(select: self, level: @level, cap: @caption, column: @column, line_number: @line_number)
       end
 
       def to_s
-        view
+        view.to_s
       end
     end
   end
 
   if __FILE__ == $PROGRAM_NAME
     # Top level only
-    idx0 = Disp.new(column: 3)
-    10.times { |i| idx0.put("x#{i}", "caption #{i}") }
-    puts idx0
+    grp0 = Disp::Group.new(column: 3)
+    10.times { |i| grp0.put("x#{i}", "caption #{i}") }
+    puts grp0.view
     puts
     # Three level groups
-    idx1 = Disp.new(column: 3)
-    grp1 = Disp::Section.new(idx1)
+    grp1 = Disp::Section.new(column: 3)
     2.times do |i|
       s11 = grp1.put("g#{i}", "Group#{i}",true)
       3.times do |j|
@@ -157,8 +159,7 @@ module CIAX
     puts grp1
     puts
     # Two level groups with item number
-    idx2 = Disp.new(column: 2, line_number: true)
-    grp2 = Disp::Section.new(idx2)
+    grp2 = Disp::Section.new(column: 2, line_number: true)
     3.times do |i|
       s21 = grp2.put("g2#{i}", "Gp#{i}")
       3.times do |j|
@@ -173,7 +174,7 @@ module CIAX
     puts grp1
     puts
     # Confirm merged index
-    idx1.select.delete('0-0')
+    grp1.index.valid.delete('0-0')
     puts grp1
   end
 end

4835ce1fe9ba89e352a6bc9ade52a0e24ae3c3ff
Koji Omata <omata@naoj.org>
2015-11-06 09:30:25 -1000

    refactoring;

--- script/libinterlock.rb
+++ script/libinterlock.rb
@@ -7,7 +7,7 @@ module CIAX
     # Element of Record
     class Interlock < Hashx
       include PrtShare
-      def initialize(cond,dev_list,step)
+      def initialize(cond, dev_list, step)
         super()
         @dev_list = type?(dev_list, App::List)
         @condition = cond
@@ -16,40 +16,15 @@ module CIAX
 
       # Sub methods
       def ok?(t = nil, f = nil)
-        stats = scan
+        stats = _scan
         conds = @condition.map do|h|
-          cond = {}
-          site = cond['site'] = h['site']
-          var = cond['var'] = h['var']
-          stat = stats[site]
-          cmp = cond['cmp'] = h['cmp']
-          cri = cond['cri'] = h['val']
-          form = cond['form'] = h['form']
-          case form
-          when 'class', 'msg'
-            warning("No key value [#{var}] in Status[#{form}]") unless stat[form].key?(var)
-            real = stat[form][var]
-          when 'data'
-            real = stat.get(var)
-          else
-            warning('No form specified')
-          end
-          verbose { "site=#{site},var=#{var},form=#{form},cmp=#{cmp},cri=#{cri},real=#{real}" }
-          cond['real'] = real
-          cond['res'] = match?(real, cri, cond['cmp'])
-          cond
+          stat = stats[h['site']]
+          _condition(stat, h)
         end
         res = conds.all? { |h| h['res'] }
         @step['conditions'] = conds
         @step['result'] = (res ? t : f) if t || f
         res
-      end
-
-      def scan
-        sites.each_with_object({}) do|site, hash|
-          verbose { "Scanning Status #{site}" }
-          hash[site] = @dev_list.get(site).stat
-        end
       end
 
       def refresh
@@ -59,23 +34,57 @@ module CIAX
         end
       end
 
+      private
+
+      def _scan
+        sites.each_with_object({}) do|site, hash|
+          verbose { "Scanning Status #{site}" }
+          hash[site] = @dev_list.get(site).stat
+        end
+      end
+
       def sites
         @condition.map { |h| h['site'] }.uniq
       end
 
-      def match?(real, cri, cmp)
-        case cmp
-        when 'equal'
-          cri == real
-        when 'not'
-          cri != real
-        when 'match'
-          /#{cri}/ =~ real
-        when 'unmatch'
-          /#{cri}/ !~ real
+      def _condition(stat, h)
+        cond = {}
+        %w(site var form cmp cri).each { |k| cond[k] = h[k] }
+        real = _fetch_form(stat, cond['form'], cond['var'])
+        res = method(cond['cmp']).call(real, cond['cri'])
+        cond.update('real' => real, 'res' => res)
+        verbose { cond.map { |k, v| format('%s=%s', k, v) }.join(',') }
+        cond
+      end
+
+      def _fetch_form(stat, form, var)
+        case form
+        when 'class', 'msg'
+          warning("No [#{var}] in Status[#{form}]") unless stat[form].key?(var)
+          stat[form][var]
+        when 'data'
+          stat.get(var)
         else
-          false
+          warning('No form specified')
+          stat.get(var)
         end
+      end
+
+      # Operators
+      def equal(a, b)
+        a == b
+      end
+
+      def not(a, b)
+        a != b
+      end
+
+      def match(a, b)
+        /#{a}/ =~ b
+      end
+
+      def unmatch(a, b)
+        /#{a}/ !~ b
       end
     end
   end

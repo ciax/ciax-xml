a88a0d74ff523a86cecf0b9f4d2a9cf7efe11de3
Koji Omata <omata@naoj.org>
2015-11-10 08:16:57 -1000

    Mcr::List: add shell feature;

--- script/libmcrlist.rb
+++ script/libmcrlist.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-require 'libseqexe'
+require 'libmcrexe'
 module CIAX
   module Mcr
     # Sequencer List which provides sequencer list as a server
@@ -31,17 +31,79 @@ module CIAX
         self
       end
 
+      def ext_shell
+        extend(Shell).ext_shell
+      end
+    end
+
+    module Shell
+      include CIAX::List::Shell
+      class Jump < LongJump; end
+
+      def ext_shell
+        super(Jump)
+        # Limit self level
+        # :dev_list is App::List
+        @cfg[:dev_list].ext_shell if @cfg.key?(:dev_list)
+        @cfg[:jump_mcr] = @jumpgrp
+@jumpgrp.add_item('dmy','dummy')
+        @post_upd_procs << proc do
+          verbose { 'Propagate List#upd -> JumpGrp#upd' }
+          @jumpgrp.number_item(@data.values.map { |seq| seq['id'] })
+        end
+        self
+      end
+
+      def add(ent, pid = '0')
+        seq = super.ext_shell
+warn seq.cobj.loc.cfg
+        seq.cobj.loc.add_jump
+warn seq.cobj.loc.cfg
+        seq
+      end
+
       def get_exe(num)
         n = num.to_i - 1
         par_err('Invalid ID') if n < 0 || n > @data.size
         @data[keys[n]]
       end
 
-      # Getting command ID (ex. run:1)
-      def get_cid(id)
-        return 'user' if id == '0'
-        get(id)['cid']
+      def shell
+        num = @data.size.to_s
+        begin
+          get_exe(num).shell
+        rescue Jump
+          num = $ERROR_INFO.to_s
+          retry
+        rescue InvalidID
+          OPT.usage('(opt) [site]')
+        end
       end
+    end
+
+    if __FILE__ == $PROGRAM_NAME
+      OPT.parse('tenr')
+      cfg = Config.new(jump_groups: [])
+      cfg[:dev_list] = Wat::List.new(cfg).sub_list # Take App List
+      begin
+        mobj = Remote::Index.new(cfg, dbi: Db.new.get)
+        mobj.add_rem.add_ext(Ext)
+        cfg[:submcr_proc] = proc do|args, pid|
+          ent = mobj.set_cmd(args)
+          list.add(ent, pid)
+          end
+        mobj.set_cmd if ARGV.empty?
+        list = List.new(PROJ, cfg).ext_shell
+        ARGV.each do|cid|
+          ent = mobj.set_cmd(cid.split(':'))
+          list.add(ent)
+        end
+        list.shell
+      rescue InvalidCMD
+        OPT.usage('[cmd(:par)] ...')
+      rescue InvalidID
+        OPT.usage('[proj] [cmd] (par)')
+        end
     end
   end
 end

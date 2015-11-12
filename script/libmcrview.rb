60a2f6884f634f5a4a0f493d574053608e34b1e5
Koji Omata(pingu) <koji.omata@gmail.com>
2015-11-12 08:24:06 -1000

    Mcr::View: update test module;

--- script/libmcrview.rb
+++ script/libmcrview.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/ruby
-require 'libupd'
+require 'librecord'
 module CIAX
   module Mcr
-    class View < Upd
+    class View < DataH
       def initialize(id,valid_keys)
+        super('mcr')
         @valid_keys = valid_keys
         @id = id
       end
@@ -11,8 +12,8 @@ module CIAX
       def to_v
         idx = 1
         page = ['<<< ' + Msg.color("Active Macros [#{@id}]", 2) + ' >>>']
-        each do|id, rec|
-          title = "[#{idx}] (#{id})(by #{get_cid(rec['pid'])})"
+        @data.each do|id, rec|
+          title = "[#{idx}] (#{id})(by #{rec['cid']})"
           msg = "#{rec['cid']} [#{rec['step']}/#{rec['total_steps']}]"
           msg << "(#{rec['stat']})"
           msg << optlist(rec['option'])
@@ -22,41 +23,25 @@ module CIAX
         page.join("\n")
       end
 
-      def core_upd
+      def upd_core
         @valid_keys.each do |id|
-          put(id,get_rec(id)) unless key?(id)
+          put(id,get_rec(id)) unless @data.key?(id)
         end
-        each{ |rec| rec.upd }
+        values.each{ |rec| rec.upd }
         self
       end
 
       def get_rec(id)
         Record.new(id).ext_save.ext_load
       end
+    end
 
-      if __FILE__ == $PROGRAM_NAME
-        OPT.parse('tenr')
-        cfg = Config.new
-        cfg[:dev_list] = Wat::List.new(cfg).sub_list # Take App List
-        begin
-          mobj = Remote::Index.new(cfg, dbi: Db.new.get)
-          mobj.add_rem.add_ext(Ext)
-          cfg[:submcr_proc] = proc do|args, pid|
-            ent = mobj.set_cmd(args)
-            list.add(ent, pid)
-          end
-          mobj.set_cmd if ARGV.empty?
-          list = List.new(PROJ, cfg).ext_drv.ext_shell
-          ARGV.each do|cid|
-            ent = mobj.set_cmd(cid.split(':'))
-            list.add(ent)
-          end
-          list.shell
-        rescue InvalidCMD
-          OPT.usage('[cmd(:par)] ...')
-        rescue InvalidID
-          OPT.usage('[proj] [cmd] (par)')
-        end
+    if __FILE__ == $PROGRAM_NAME
+      require 'libgetopts'
+      unless ARGV.empty?
+        puts View.new('test',ARGV).upd.to_v
+      else
+        OPT.usage('[id] ..')
       end
     end
   end

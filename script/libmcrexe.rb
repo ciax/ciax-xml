f9cda6711e4613cf4570e500a5ee5a15ce00e76c
Koji Omata <omata@naoj.org>
2015-11-13 10:56:16 -1000

    refactoring;

--- script/libmcrexe.rb
+++ script/libmcrexe.rb
@@ -1,28 +1,18 @@
 #!/usr/bin/ruby
 require 'libmcrseq'
-
+# CIAX-XML
 module CIAX
+  # Macro Layer
   module Mcr
+    # Shell interface for Seq
     class Exe < CIAX::Exe
-      # required cfg keys: app,db,body,stat,(:submcr_proc)
       attr_reader :th_mcr, :seq
-      # cfg[:submcr_proc] for executing asynchronous submacro,
-      #   which must returns hash with ['id']
-      # ent should have [:sequence]'[:dev_list],[:submcr_proc]
+      # ment should have [:sequence]'[:dev_list],[:submcr_proc]
       def initialize(ment, pid = '0')
         cfg = Config.new(jump_groups: ment[:jump_groups])
         super(type?(ment, Entity).id, cfg)
-        # For Thread mode
-        @cobj.add_rem.add_hid
-        int = @cobj.rem.add_int(Int)
-        @seq=Seq.new(ment, pid, int.valid_keys.clear)
-        int.def_proc { |ent| @seq.reply(ent.id) }
-        @th_mcr = @seq.fork
-        # interrupt is in rem.hid group
-        @cobj.get('interrupt').def_proc do
-          @th_mcr.raise(Interrupt)
-          'INTERRUPT'
-        end
+        _init_cmd_(ment, pid)
+        _init_thread_
         self
       end
 
@@ -32,6 +22,25 @@ module CIAX
         @cfg[:output] = @seq.record
         @cobj.loc.add_view
         self
+      end
+
+      private
+
+      def _init_cmd_(ment,pid)
+        @cobj.add_rem.add_hid
+        int = @cobj.rem.add_int(Int)
+        @seq = Seq.new(ment, pid, int.valid_keys.clear)
+        int.def_proc { |ent| @seq.reply(ent.id) }
+      end
+
+      # For Thread mode
+      def _init_thread_
+        @th_mcr = @seq.fork
+        # interrupt is in rem.hid group
+        @cobj.get('interrupt').def_proc do
+          @th_mcr.raise(Interrupt)
+          'INTERRUPT'
+        end
       end
     end
 

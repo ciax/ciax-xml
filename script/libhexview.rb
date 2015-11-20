caf6b8a076dae1e323d57405bb1e07a5988eeea5
Koji Omata <omata@naoj.org>
2015-11-20 12:28:36 -1000

    rubocop refactoring;

--- script/libhexview.rb
+++ script/libhexview.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/ruby
-# Ascii Pack
 require 'libvarx'
 require 'libprompt'
-
 module CIAX
+  # Ascii Hex Pack
   module Hex
+    # View class
     class View < Varx
       # hint should have server status (isu,watch,exe..) like App::Exe
       def initialize(stat, sv_stat = nil)
@@ -13,71 +13,14 @@ module CIAX
         # Server Status
         id = self['id'] || id_err("NO ID(#{id}) in Stat")
         @sv_stat = type?(sv_stat || Prompt.new('site', id), Prompt)
-        file = View.sdb(id) || id_err("Hex/Can't found sdb_#{id}.txt")
-        @res = ['%', id, '_', '0', '0', '_', '']
-        @list = []
+        @list = _get_sdb_(id)
         @vmode = 'x'
-        open(file) do|f|
-          while (line = f.gets)
-            ary = line.split(',')
-            case line
-            when /^[#]/, /^$/
-            else
-              @list << ary
-            end
-          end
-        end
-        @sv_stat.post_upd_procs << proc do
-          verbose { 'Propagate Prompt#upd -> Hex::View#upd' }
-          upd
-        end
-        @stat.post_upd_procs << proc do
-          verbose { 'Propagate Status#upd -> Hex::View#upd' }
-          upd
-        end
-        upd
-        self
+        _init_upd_
       end
 
       def self.sdb(id)
         file = ENV['HOME'] + "/config/sdb_#{id}.txt"
         test('r', file) && file
-      end
-
-      def upd_core
-        @res[2] = b2e(@sv_stat['udperr'])
-        @res[3] = b2i(@sv_stat['event'])
-        @res[4] = b2i(@sv_stat['isu'])
-        @res[5] = b2e(@sv_stat['comerr'])
-        @res[6] = ''
-        pck = 0
-        bin = 0
-        @list.each do|key, title, len, type|
-          len = len.to_i
-          if key =~ '%pck'
-            pck = len
-            bin = 0
-          elsif pck > 0
-            bin += @stat.get(key).to_i
-            bin << 1
-            pck -= 1
-            @res[6] << format('%x', bin) if pck == 0
-          else
-            v = @stat.get(key)
-            if v
-              str = get_elem(type, len, v)
-              verbose { "#{title}/#{type}(#{len}) = #{str}" }
-            else
-              str = '*' * len
-            end
-            # str can exceed specified length
-            str = str[0, len]
-            verbose { "add '#{str}' as #{key}" }
-            @res[6] << str
-          end
-        end
-        self['hex'] = @res.join('')
-        self
       end
 
       def to_x
@@ -95,18 +38,93 @@ module CIAX
 
       private
 
-      def get_elem(type, len, val)
+      def upd_core
+        _get_body_
+        self['hex'] = _get_header_ + @str
+        self
+      end
+
+      def _get_header_
+        ary = ['%', self['id']]
+        ary << b2e(@sv_stat['udperr'])
+        ary << b2i(@sv_stat['event'])
+        ary << b2i(@sv_stat['isu'])
+        ary << b2e(@sv_stat['comerr'])
+        ary.join('')
+      end
+
+      def _get_body_
+        _set_var_
+        @list.each do|key, title, len, type|
+          if /%pck/ =~ key
+            _set_var_(len)
+          elsif @pck > 0
+            @str << _get_bin_(key)
+          else
+            @str << _get_str_(key, title, len, type)
+          end
+        end
+      end
+
+      def _set_var_(pck = 0, bin = 0)
+        @pck = pck.to_i
+        @bin = bin.to_i
+        @str = ''
+      end
+
+      def _get_bin_(key)
+        @bin += @stat.get(key).to_i
+        @bin << 1
+        @pck -= 1
+        format('%x', @bin) if @pck == 0
+      end
+
+      def _get_str_(key, title, len, type)
+        v = @stat.get(key)
+        if v
+          str = _get_elem_(type, len, v)
+          verbose { "#{title}/#{type}(#{len}) = #{str}" }
+        else
+          str = '*' * len.to_i
+        end
+        # str can exceed specified length
+        str = str[0, len.to_i]
+        verbose { "add '#{str}' as #{key}" }
+        str
+      end
+
+      def _get_sdb_(id)
+        file = View.sdb(id) || id_err("Hex/Can't found sdb_#{id}.txt")
+        open(file) do|f|
+          f.readlines.grep(/^[^#].+/).map do |line|
+            line.split(',')
+          end
+        end
+      end
+
+      def _init_upd_
+        @sv_stat.post_upd_procs << proc do
+          verbose { 'Propagate Prompt#upd -> Hex::View#upd' }
+          upd
+        end
+        @stat.post_upd_procs << proc do
+          verbose { 'Propagate Status#upd -> Hex::View#upd' }
+          upd
+        end
+        upd
+      end
+
+      def _get_elem_(type, len, val)
         case type
         when /FLOAT/
-          str = format("%0#{len}.2f", val.to_f)
+          format("%0#{len}.2f", val.to_f)
         when /INT/
-          str = format("%0#{len}d", val.to_i)
+          format("%0#{len}d", val.to_i)
         when /BINARY/
-          str = format("%0#{len}b", val.to_i)
+          format("%0#{len}b", val.to_i)
         else
-          str = format("%#{len}s", val)
+          format("%#{len}s", val)
         end
-        str
       end
 
       def b2i(b) # Boolean to Integer (1,0)

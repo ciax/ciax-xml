4e621a0132df980cd8962d394ca77c50eac06284
Koji Omata(pingu) <koji.omata@gmail.com>
2015-11-23 01:45:31 -1000

    utils/json*: use JSON.parse() with :symbolize_names;

--- utils/json_logview.rb
+++ utils/json_logview.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/ruby
 # Log viewer
 # For stream log version 6 or later
-#alias jlv
+# alias jlv
 require 'json'
 
 abort 'Usage: json_logview json_log' if STDIN.tty? && ARGV.size < 1
 readlines.each do|line|
   next if line.to_s.empty?
   ary = []
-  JSON.load(line.chomp).each do|k, v|
+  JSON.parse(line.chomp, symbolize_names: true).each do|k, v|
     case k
     when 'time'
       ary << Time.at(v.to_f / 1000).to_s
--- utils/json_view.rb
+++ utils/json_view.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 # JSON to String(Decorated)
-#alias jv
+# alias jv
 require 'libenumx'
 abort 'Usage: json_view [json_file]' if STDIN.tty? && ARGV.size < 1
 str = gets(nil) || exit
-puts JSON.load(str).extend(CIAX::Enumx).to_v
+puts JSON.parse(str, symbolize_names: true).extend(CIAX::Enumx).to_v

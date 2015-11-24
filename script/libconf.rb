ac747c24d6c6b65019b2f8aa02530e50a878dcca
Koji Omata <omata@naoj.org>
2015-11-24 11:08:14 -1000

    Config: refactoring;

--- script/libconf.rb
+++ script/libconf.rb
@@ -1,13 +1,16 @@
 #!/usr/bin/ruby
 require 'libenumx'
 module CIAX
-  # Recursive hash array of @generation: each Hash is associated with Domain,Group,Item;
+  # Recursive hash array of @generation:
+  #   each Hash is associated with Domain,Group,Item;
   # Structure: Command[cfg]
   #         -> Domain[cfg,command_cfg]
   #         -> Group[cfg,domain_cfg,command_cfg]
   #         -> Item[cfg,group_cfg,domain_cfg,command_cfg]...
-  # Usage:[]=/ add to current Hash which will overwrite(hide) upper level Hash;
-  # Usage:[]/  get val from current Hash otherwise from upper generation of Hash;
+  # Usage:[]=
+  #   add to current Hash which will overwrite(hide) upper level Hash;
+  # Usage:[]
+  #   get val from current Hash otherwise from upper generation of Hash;
   class Config < Hashx
     attr_reader :generation
     alias_method :this_keys, :keys
@@ -56,9 +59,9 @@ module CIAX
     end
 
     # Check key if it is correct type. Used for argument validation.
-    def check(name, type)
-      sv_err("No such key in Config [#{name}]") unless self[name]
-      sv_err("Type is mismatch for Config key [#{name}]") unless self[name].is_a?(type)
+    def check(key, type)
+      sv_err("No such key in Config [#{key}]") unless self[key]
+      sv_err("Confi Key Type is mismatch [#{key}]") unless self[key].is_a?(type)
       true
     end
 
@@ -70,53 +73,74 @@ module CIAX
     # Show all contents of all generation
     def path(key = nil)
       i = 0
-      str = ''
-      @generation.each do|h|
-        str = "  [#{i}]{" + h.map do|k, v|
-          next if key && k != key
-          case v
-          when Hash, Proc, CmdProc
-            val = v.class
-          when Array
-            val = '[' + v.map do|e|
-              case e
-              when Enumerable
-                e.class
-              else
-                e.inspect
-              end
-            end.join(',') + "](#{v.object_id})"
-          else
-            val = v.inspect
-          end
-          k.inspect.to_s + '=>' + val.to_s
-        end.compact.join(', ') + '} (' + h.object_id.to_s + ")\n" + str
-        i += 1
+      ary = @generation.map do|h|
+        "  [#{i += 1}]{" + _show_generation_(key, h) + "} (#{h.object_id})"
       end
-      "******[Config]******(#{object_id})\n#{str}************\n"
+      _decorate_(ary.reverse)
     end
 
     # Show list of all key,val which will be taken with [] access
     def list
-      i = 0
       db = {}
-      @generation.each do|h|
-        h.each do|key, val|
-          db[key] = [i, val] unless db.key?(key)
-        end
-        i += 1
+      @generation.each_with_index do|h, i|
+        h.each { |k, v| db[k] = [i, v] unless db.key?(k) }
       end
-      "******[Config]******(#{object_id})\n" + db.map do|key, ary|
-        case v = ary[1]
-        when String, Numeric, Enumerable
-          val = v.inspect
-        when Proc
-          val = v.class
+      ary = db.map do |k, a|
+        val = _show_db_(a[1])
+        "  #{k} (#{a[0]}) = #{val}"
+      end
+      _decorate_(ary.reverse)
+    end
+
+    private
+
+    def _show_db_(v)
+      case v
+      when String, Numeric, Enumerable
+        _show_(v.inspect)
+      when Proc
+        _show_(v.class)
+      else
+        _show_(v)
+      end
+    end
+
+    def _decorate_(ary)
+      ["******[Config]******(#{object_id})", *ary, '************'].join("\n")
+    end
+
+    def _show_generation_(key, h)
+      h.map do|k, v|
+        next if key && k != key
+        val = (k == :obj) ? _show_(v.class) : _show_contents_(v)
+        "#{k.inspect.sub(/^:/, '')}: #{val}"
+      end.compact.join(', ')
+    end
+
+    def _show_contents_(v)
+      case v
+      when Hash, Proc
+        _show_(v.class)
+      when Array
+        _show_array_(v)
+      else
+        _show_(v.inspect)
+      end
+    end
+
+    def _show_array_(v)
+      '[' + v.map do|e|
+        case e
+        when Enumerable
+          _show_(e.class)
         else
-          val = v
+          _show_(e.inspect)
         end
-        "  #{key} (#{ary[0]}) = #{val}"
-      end.reverse.join("\n") + "\n************\n"
+      end.join(',') + "](#{v.object_id})"
+    end
+
+    def _show_(v)
+      v.to_s.sub('CIAX::', '')
     end
   end
 end

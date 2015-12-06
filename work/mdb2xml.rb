5c0ea06ca188274b1b85248df131b2ce4b99c547
Koji Omata(pingu) <koji.omata@gmail.com>
2015-12-06 03:56:42 -1000

    enclose includes iterator;

--- work/mdb2xml.rb
+++ work/mdb2xml.rb
@@ -10,10 +10,12 @@ def mktag(tag, atrb)
   end
 end
 
+# single line element
 def indent(tag, atrb = {}, text = nil)
   mktag(tag, atrb)
   if text
-    printf(">%s</%s>\n", text, tag)
+    printf(">%s</%s>", text, tag)
+    puts
   else
     puts '/>'
   end
@@ -27,13 +29,18 @@ end
 
 def tclose(tag)
   @indent -= 1
-  printf('  ' * @indent + "</%s>\n", tag)
-  nil
+  printf('  ' * @indent + "</%s>", tag)
+  puts
 end
 
-def enclose(tag, atrb = {})
+def enclose(tag, atrb = {}, enum = nil)
+  return if enum && enum.empty?
   topen(tag, atrb)
-  yield
+  if enum
+    enum.each{ |a| yield a}
+  else
+    yield
+  end
   tclose(tag)
 end
 
@@ -51,22 +58,20 @@ def hpick(hash, *tags)
   res
 end
 
-def prt_cond(fld, form = 'msg')
-  fld.each do|ary|
-    atrb = a2h(ary, :site, :var, :ope, :cri, :skip)
-    atrb[:form] = form
-    ope = OPETBL[atrb.delete(:ope)]
-    cri = atrb.delete(:cri)
-    indent(ope, atrb, cri)
-  end
+def prt_cond(cond, form = 'msg')
+  atrb = a2h(cond, :site, :var, :ope, :cri, :skip)
+  atrb[:form] = form
+  ope = OPETBL[atrb.delete(:ope)]
+  cri = atrb.delete(:cri)
+  indent(ope, atrb, cri)
 end
 
 def tag_wait(e)
   if e['sleep']
     indent(:wait, hpick(e, :sleep, :label))
   else
-    enclose(:wait, hpick(e, :retry, :label)) do
-      prt_cond(e['until'], 'data')
+    enclose(:wait, hpick(e, :retry, :label), e['until']) do |cond|
+      prt_cond(cond, 'data')
     end
   end
 end
@@ -88,11 +93,9 @@ end
 
 def tag_select(ary)
   atrb = { site: ary['site'], var: ary['var'], form: 'msg' }
-  enclose(:select, atrb) do
-    ary['option'].each do|val, mcr|
-      enclose(:option, val: val) do
-        indent(:mcr, name: mcr)
-      end
+  enclose(:select, atrb, ary['option']) do|val, mcr|
+    enclose(:option, val: val) do
+      indent(:mcr, name: mcr)
     end
   end
 end
@@ -102,43 +105,34 @@ def tag_item(id)
   return unless db
   atrb = { id: id }
   atrb[:label] = db['label'] if db['label']
-  enclose(:item, atrb) do
-    db.each do|key, ary|
-      case key
-      when 'goal'
-        enclose(:goal) do
-          prt_cond(ary)
-        end
-      when 'check'
-        enclose(:check) do
-          prt_cond(ary)
-        end
-      when 'seq'
-        tag_seq(ary)
-      when 'select'
-        tag_select(ary)
+  enclose(:item, atrb, db) do|key, ary|
+    case key
+    when 'goal','check'
+      enclose(key, {}, ary) do |cond|
+        prt_cond(cond)
       end
+    when 'seq'
+      tag_seq(ary)
+    when 'select'
+      tag_select(ary)
     end
   end
 end
 
 def tag_unit(uid)
-  enclose(:unit, id: uid, label: @ucap[uid]) do
-    @umem[uid].each do |id|
-      tag_item(id)
-    end
+  atrb = {id: uid, label: @ucap[uid]}
+  enclose(:unit, atrb, @umem[uid]) do |id|
+    tag_item(id)
   end
 end
 
-def tag_group(gid, mary)
-  return if mary.empty?
-  enclose(:group, id: gid, caption: @gcap[gid]) do
-    mary.each do|uid|
-      if /unit_/ =~ uid
-        tag_unit(uid)
-      else
-        tag_item(uid)
-      end
+def tag_group(gid, gary)
+atrb = {id: gid, caption: @gcap[gid]}
+  enclose(:group, atrb, gary) do|uid|
+    if /unit_/ =~ uid
+      tag_unit(uid)
+    else
+      tag_item(uid)
     end
   end
 end
@@ -154,11 +148,10 @@ abort 'Usage: mdb2xml [mdb(json) file]' if STDIN.tty? && ARGV.size < 1
 
 @indent = 0
 puts '<?xml version="1.0" encoding="utf-8"?>'
-enclose(:mdb, xmlns: 'http://ciax.sum.naoj.org/ciax-xml/mdb') do
+puts enclose(:mdb, xmlns: 'http://ciax.sum.naoj.org/ciax-xml/mdb') {
   label = "#{@mcap.upcase} Macro"
-  enclose(:macro, id: @mcap, version: '1', label: label, port: '55555') do
-    @gmem.each do |gid, mary|
-      tag_group(gid, mary)
-    end
+  atrb = { id: @mcap, version: '1', label: label, port: '55555'}
+  enclose(:macro, atrb, @gmem) do |gid, mem|
+    tag_group(gid, mem)
   end
-end
+}

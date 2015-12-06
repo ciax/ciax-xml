7125c9ade8d1671e2edc0c2ce553630b030fa87a
Koji Omata(pingu) <koji.omata@gmail.com>
2015-12-06 08:56:41 -1000

    add car check;

--- work/mkmos.sh
+++ work/mkmos.sh
@@ -2,4 +2,5 @@
 cd ~/ciax-xml
 csv2mdb -m mos mfp mma|mdb2xml > mdb-mos.xml
 check-xml mdb-mos.xml
-#csv2mdb -m car mmc|mdb2xml > ~/ciax-xml/mdb-car.xml
+csv2mdb -m car mmc|mdb2xml > mdb-car.xml
+check-xml mdb-car.xml

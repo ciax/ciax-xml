cb552eb38fed2112a5b3c787f70a41375f8d4b94
Koji Omata <omata@naoj.org>
2015-12-07 16:05:28 -1000

    json_logview: red with symbole key;

--- utils/json_logview.rb
+++ utils/json_logview.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 # Log viewer
 # For stream log version 6 or later
-# alias jlv
+#alias jlv
 require 'json'
 
 abort 'Usage: json_logview json_log' if STDIN.tty? && ARGV.size < 1
@@ -10,9 +10,9 @@ readlines.each do|line|
   ary = []
   JSON.parse(line.chomp, symbolize_names: true).each do|k, v|
     case k
-    when 'time'
+    when :time
       ary << Time.at(v.to_f / 1000).to_s
-    when 'base64'
+    when :base64
       data = v.unpack('m').first
       ary << "data=#{data.inspect}(#{data.size})"
     else

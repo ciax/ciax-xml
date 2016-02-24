6bfcd3df47fc4efb98515ba3f282b9d1196e755c
Koji Omata(pingu) <koji.omata@gmail.com>
2016-02-24 00:07:18 -1000

    mcrsv: add background option;

--- script/mcrsv.rb
+++ script/mcrsv.rb
@@ -4,7 +4,7 @@ require 'libmcrman'
 require 'libdaemon'
 # CIAX-XML Macro Server
 module CIAX
-  Daemon.new('mcrsv', 'bcenr') do |cfg|
+  Daemon.new('mcrsv', 'bcdenr') do |cfg|
     Mcr::Man.new(cfg)
   end
 end

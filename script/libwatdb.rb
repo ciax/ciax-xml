7097107ccd1ab4ed9b0de91c8feca78320cc04a7
Koji Omata <omata@naoj.org>
2016-03-03 18:23:20 -1000

    rubocop refactoring;

--- script/libwatdb.rb
+++ script/libwatdb.rb
@@ -7,52 +7,82 @@ module CIAX
   module Wat
     # Watch DB
     module Db
-      # structure of exec=[cond1,2,...]; cond=[args1,2,..]; args1=['cmd','par1',..]
+      # structure
+      #   exec  = [cond1, 2, ...]
+      #   cond  = [args1, 2, ..]
+      #   args1 = ['cmd', 'par1', ..]
       def init_watch(doc, db)
         return {} unless doc.key?(:watch)
-        wdb = doc[:watch]
-        cmdgrp = db[:command][:group]
-        idx = {}
-        regular = { period: 300, exec: [] }
-        Repeat.new.each(wdb) do|e0, r0|
+        wdoc = doc[:watch]
+        wdb = db[:watch] = wdoc.to_h
+        reg = wdb[:regular] = { period: 300, exec: [] }
+        idx = wdb[:index] = {}
+        _get_wdb(wdoc, reg, idx, db[:command][:group])
+        reg[:exec] << ['upd'] if reg[:exec].empty?
+      end
+
+      private
+
+      def _get_wdb(wdoc, reg, idx, cgrp)
+        Repeat.new.each(wdoc) do|e0, r0|
           case e0.name
           when 'regular'
-            regular.update(e0.to_h)
-            e0.each do|e1|
-              args = [e1[:name]]
-              e1.each do|e2|
-                args << e2.text
-              end
-              regular[:exec] << args
-            end
+            _make_regular(e0, reg)
           when 'event'
-            id = e0.attr2item(idx) { |_, v| r0.formatting(v) }
-            item = idx[id]
-            cnd = item[:cnd] = []
-            act = item[:act] = {}
-            e0.each do|e1|
-              case name = e1.name.to_sym
-              when :block, :int, :exec
-                args = [e1[:name]]
-                e1.each do|e2|
-                  args << r0.subst(e2.text)
-                end
-                (act[name] ||= []) << args
-              when :block_grp
-                blk = (act[:block] ||= [])
-                cmdgrp[e1[:ref]][:members].each { |k| blk << [k] }
-              else
-                h = e1.to_h
-                h.each_value { |v| v.replace(r0.formatting(v)) }
-                h[:type] = e1.name
-                cnd << h
-                h[:vars] = e1.map { |e2| e2[:var] } if name == :compare
-              end
-            end
+            _make_event(e0, r0, idx, cgrp)
           end
         end
-        regular[:exec] << ['upd'] if regular[:exec].empty?
-        db[:watch] = wdb.to_h.update(index: idx, regular: regular)
+      end
+
+      def _make_regular(e0, reg)
+        reg.update(e0.to_h)
+        e0.each do|e1|
+          args = [e1[:name]]
+          e1.each do|e2|
+            args << e2.text
+          end
+          reg[:exec] << args
+        end
+      end
+
+      def _make_event(e0, r0, idx, cgrp)
+        id = e0.attr2item(idx) { |_, v| r0.formatting(v) }
+        item = idx[id]
+        cnd = item[:cnd] = []
+        act = item[:act] = {}
+        e0.each do|e1|
+          _event_element(e1, r0, act, cnd, cgrp)
+        end
+      end
+
+      def _event_element(e1, r0, act, cnd, cgrp)
+        case name = e1.name.to_sym
+        when :block, :int, :exec
+          (act[name] ||= []) << _make_action(e1, r0)
+        when :block_grp
+          (act[:block] ||= []).concat(_make_block(e1, cgrp))
+        when :compare
+          cnd << _make_cond(e1, r0).update(vars: e1.map { |e2| e2[:var] })
+        else
+          cnd << _make_cond(e1, r0)
+        end
+      end
+
+      def _make_action(e1, r0)
+        # e1[:name] is different from e1.name (attribute vs. tag)
+        args = [e1[:name]]
+        e1.each { |e2| args << r0.subst(e2.text) }
+        args
+      end
+
+      def _make_block(e1, cgrp)
+        cgrp[e1[:ref]][:members].map { |k| [k] }
+      end
+
+      def _make_cond(e1, r0)
+        h = e1.to_h
+        h.each_value { |v| v.replace(r0.formatting(v)) }
+        h.update(type: e1.name)
       end
     end
   end

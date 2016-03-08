2dddd9ed922ac6e4de1dc981b29760b1675f029a
Koji Omata <omata@naoj.org>
2016-03-08 09:34:27 -1000

    rc.bash.ciax-xml: remove addenv(). env clean up will be done by another func;

--- utils/rc.bash.ciax-xml.sh
+++ utils/rc.bash.ciax-xml.sh
@@ -1,31 +1,18 @@
 #!/bin/bash --rcfile
-addenv(){
-    name=$1;shift
-    list=$(IFS=: eval echo \$$name)
-    if [ "$list" ] ; then
-        for j; do
-            for i in $list;do
-                [ "$j" = "$i" ] && break 2
-            done
-            eval "export $name=$j:\$$name"
-        done
-    else
-        eval "export $name=${*// /:}"
-    fi
-}
 develop(){
-    cd "$HOME/ciax-xml/script"
+    pushd "$HOME/ciax-xml/script"
     git branch |grep '* develop'
+    popd
 }
-    
+
 # Local functions
 umask 022
 shopt -s nullglob
 export LANG="C"
-addenv PATH "$HOME/bin" "$HOME/lib"
-addenv RUBYLIB "$HOME/ciax-xml/script"
+export PATH="$HOME/bin:$PATH"
+export RUBYLIB="$HOME/ciax-xml/script:$RUBYLIB"
 
 #Alias
 alias rub='rubocop -a -c .rubocop_todo.yml'
 alias rgen='rubocop --auto-gen-config'
-develop && export NOCACHE=1
+develop >/dev/null && export NOCACHE=1

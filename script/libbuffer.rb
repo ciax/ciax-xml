83177d15172cb9951242960c611ade12e8ed6a5b
Koji Omata <omata@naoj.org>
2016-03-14 13:43:27 -1000

    Buffer#server(): add site id to Thread title;

--- script/libbuffer.rb
+++ script/libbuffer.rb
@@ -53,7 +53,7 @@ module CIAX
     end
 
     def server
-      @tid = ThreadLoop.new('Buffer', 12) do
+      @tid = ThreadLoop.new("Buffer(#{@sv_stat.get(:id)})", 12) do
         exec_buf if @q.empty?
         verbose { 'Waiting' }
         pri_sort(@q.shift)

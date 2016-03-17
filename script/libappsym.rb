1f2a020e216eb4ff4db9f9bbec692037055d3ed8
Koji Omata <omata@naoj.org>
2016-03-17 10:35:49 -1000

    rubocopping;

--- script/libappsym.rb
+++ script/libappsym.rb
@@ -18,52 +18,74 @@ module CIAX
         @symdb = Sym::Db.pack(['share'] + adbs[:symtbl])
         self[:class] = {}
         self[:msg] = {}
+        _init_procs(adbs)
+        self
+      end
+
+      def _init_procs(adbs)
         @post_upd_procs << proc do # post process
           verbose { 'Propagate Status#upd -> Symbol#upd' }
           store_sym(adbs[:index].dup.update(adbs[:alias] || {}))
         end
-        self
       end
 
       def store_sym(index)
         index.each do|key, hash|
           sid = hash[:symbol] || next
-          tbl = @symdb[sid]
-          unless tbl
-            alert("Table[#{sid}] not exist")
-            next
-          end
+          tbl = _chk_tbl(sid) || next
           verbose { "ID=#{key},Table=#{sid}" }
-          self[:class][key] = 'alarm'
           val = self[:data][hash[:ref] || key]
-          self[:msg][key] = "N/A(#{val})"
-          numeric = false
-          tbl.each do|sym|
-            case sym[:type]
-            when 'numeric'
-              numeric = true
-              tol = sym[:tolerance].to_f
-              next if sym[:val].split(',').all? do|cri|
-                val.to_f > cri.to_f + tol || val.to_f < cri.to_f - tol
-              end
-              verbose { "VIEW:Numeric:[#{sym[:val]}+-#{tol}] and [#{val}]" }
-            when 'range'
-              numeric = true
-              next unless ReRange.new(sym[:val]) == val
-              verbose { "VIEW:Range:[#{sym[:val]}] and [#{val}]" }
-            when 'pattern'
-              next unless /#{sym[:val]}/ =~ val || val == 'default'
-              verbose { "VIEW:Regexp:[#{sym[:val]}] and [#{val}]" }
-            end
-            if numeric
-              self[:msg][key] = format(sym[:msg], val)
-            else
-              self[:msg][key] = sym[:msg] || "N/A(#{val})"
-            end
-            self[:class][key] = sym[:class]
-            break
-          end
+          _match_items(tbl, key, val)
         end
+      end
+
+      def _match_items(tbl, key, val)
+        tbl.each do|sym|
+          msg = _match_by_type(sym, val) || next
+          self[:msg][key] = msg || "N/A(#{val})"
+          self[:class][key] = sym[:class] || 'alarm'
+          break
+        end
+      end
+
+      def _match_by_type(sym, val)
+        cri = sym[:val]
+        case sym[:type]
+        when 'numeric'
+          _match_numeric(sym, cri, val)
+        when 'range'
+          _match_range(sym, cri, val)
+        when 'pattern'
+          _match_pattern(sym, cri, val)
+        end
+      end
+
+      def _match_numeric(sym, cri, val)
+        tol = sym[:tolerance].to_f.abs
+        verbose { "VIEW:Numeric:[#{cri}+-#{tol}] and [#{val}]" }
+        return if cri.split(',').all? do|c|
+          (val.to_f - c.to_f).abs > tol
+        end
+        format(sym[:msg], val)
+      end
+
+      def _match_range(sym, cri, val)
+        return unless ReRange.new(cri) == val
+        verbose { "VIEW:Range:[#{cri}] and [#{val}]" }
+        format(sym[:msg], val)
+      end
+
+      def _match_pattern(sym, cri, val)
+        return unless /#{cri}/ =~ val || val == 'default'
+        verbose { "VIEW:Regexp:[#{cri}] and [#{val}]" }
+        sym[:msg] || "N/A(#{val})"
+      end
+
+      def _chk_tbl(sid)
+        tbl = @symdb[sid]
+        return tbl if tbl
+        alert("Table[#{sid}] not exist")
+        nil
       end
     end
 

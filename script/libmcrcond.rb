094628fc4b78d7272f5cb0bba343a766191a2c9f
Koji Omata <omata@naoj.org>
2016-03-29 14:17:55 -1000

    Mcr::Cond: missing udp(prompt) update during waiting for mortion;

--- script/libmcrcond.rb
+++ script/libmcrcond.rb
@@ -9,41 +9,45 @@ module CIAX
       def initialize(cond, dev_list, step)
         super()
         @dev_list = type?(dev_list, Wat::List)
+        @exes = cond.map { |h| h[:site] }.uniq.map { |s| @dev_list.get(s) }
         @condition = cond
         @step = step
       end
 
       # Sub methods
       def ok?(t = nil, f = nil)
-        stats = _scan
-        conds = @condition.map do|h|
-          stat = stats[h[:site]]
-          _condition(stat, h)
-        end
-        res = conds.all? { |h| h[:res] }
-        @step[:conditions] = conds
+        res = _all_conditions?(_scan)
         @step[:result] = (res ? t : f) if t || f
         res
-      end
-
-      def refresh
-        _sites.each do|site|
-          verbose { "Refresh Status #{site}" }
-          @dev_list.get(site).stat.refresh
-        end
       end
 
       private
 
       def _scan
-        _sites.each_with_object({}) do|site, hash|
-          st = hash[site] = @dev_list.get(site).sub.stat.upd
-          verbose { "Scanning #{site} (#{st[:time]})/(#{st.object_id})" }
+        @exes.each_with_object({}) do |obj, hash|
+          _wait_busy(obj)
+          st = hash[obj.id] = obj.sub.stat.upd
+          verbose { "Scanning #{obj.id} (#{st[:time]})/(#{st.object_id})" }
         end
       end
 
-      def _sites
-        @condition.map { |h| h[:site] }.uniq
+      def _wait_busy(obj)
+        100.times do
+          sv = obj.exe([]).sv_stat
+          return unless sv.up?(:event) || sv.up?(:busy)
+          verbose { "Waiting busy for #{obj.id}" }
+          sleep 0.1
+        end
+        @step[:result] = 'timeout'
+        fail Interlock
+      end
+
+      def _all_conditions?(stats)
+        conds = @condition.map do|h|
+          _condition(stats[h[:site]], h)
+        end
+        @step[:conditions] = conds
+        conds.all? { |h| h[:res] }
       end
 
       def _condition(stat, h)

d438462841d97b01306c0fd8d9c9a60de6e7223b
Koji Omata <omata@naoj.org>
2016-03-31 11:12:09 -1000

    sim-server: add -d option;

--- simulator/sim-server.sh
+++ simulator/sim-server.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 sims="fpsim apsim carsim armsim bbsim"
 killall $sims
-for i in $sims; do $i;done
-dvsv -e tfp tap tmc tma tb2
-mcrsv -c test
+if [ "$1" == '-d' ] ; then
+    dvsv > /dev/null 2>&1
+    mcrsv > /dev/null 2>&1
+else
+    for i in $sims; do $i;done
+    dvsv -e tfp tap tmc tma tb2
+    mcrsv -c test
+fi

b8055a02f3032aca1e665fe0de2ae1a7057e53af
Koji Omata <omata@naoj.org>
2016-03-31 11:07:29 -1000

    sim-server: add kill feature;

--- simulator/sim-server.sh
+++ simulator/sim-server.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-fpsim
-apsim
-carsim
-armsim
-bbsim
+sims="fpsim apsim carsim armsim bbsim"
+killall $sims
+for i in $sims; do $i;done
 dvsv -e tfp tap tmc tma tb2
+mcrsv -c test

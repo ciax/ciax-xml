1bd860bb951d63f28efb449602f226909461fd59
Koji Omata <omata@naoj.org>
2016-04-04 13:26:43 -1000

    add mcrtest.sh;

---
+++ simulator/mcrtest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+mcrsv -c test
+mcrsh -c test
+mcrsv >/dev/null 2>&1
+
--- simulator/sim-server.sh
+++ simulator/sim-server.sh
@@ -3,9 +3,7 @@ sims="fpsim apsim carsim armsim bbsim"
 killall $sims
 if [ "$1" == '-d' ] ; then
     dvsv > /dev/null 2>&1
-    mcrsv > /dev/null 2>&1
 else
     for i in $sims; do $i;done
     dvsv -e tfp tap tmc tma tb2
-    mcrsv -c test
 fi

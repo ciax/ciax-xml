b553f17e9102afe64a7bd94d502665945c75ac80
Koji Omata <omata@naoj.org>
2016-04-08 10:59:03 -1000

    sim-server: run only simulators;

--- simulator/sim-server.sh
+++ simulator/sim-server.sh
@@ -2,9 +2,7 @@
 #alias sim
 sims="fpsim apsim carsim armsim bbsim"
 killall $sims
-if [ "$1" == '-d' ] ; then
-    dvsv > /dev/null 2>&1
-else
+if [ "$1" != '-d' ] ; then
     for i in $sims; do $i;done
-    dvsv -e tfp tap tmc tma tb2
 fi
+ps -ef|grep -v 'grep'|egrep "${sims// /|}"

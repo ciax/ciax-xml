21f90a73acd254f09833182c5cfa4dd5dc36d144
Koji Omata(pingu) <koji.omata@gmail.com>
2016-04-09 07:53:50 -1000

    refactoring;

--- script/libjslog.rb
+++ script/libjslog.rb
@@ -9,23 +9,11 @@ module CIAX
 
     def ext_log # logging with flatten
       id = self[:id]
-      ver = self[:ver]
-      verbose { "Log Initialize [#{id}/Ver.#{ver}]" }
       @queue = Queue.new
       @post_upd_procs << proc { @queue.push(JSON.dump(self)) }
-      logfile = vardir('log') + _file_base + "_#{Time.now.year}.log"
-      ThreadLoop.new("Logging(#{@type}:#{id})", 11) do
-        logary = []
-        loop do
-          logary << @queue.pop
-          break if @queue.empty?
-        end
-        open(logfile, 'a') do|f|
-          logary.each do|str|
-            f.puts str
-            verbose { "Appended #{str.size} byte" }
-          end
-        end
+      Threadx.new("Logging(#{@type}:#{id})", 11) do
+        verbose { "Log Initialize [#{id}/Ver.#{self[:ver]}]" }
+        _log_loop
       end
       self
     end
@@ -46,6 +34,17 @@ module CIAX
       # Logging if version number exists
       SqLog::Save.new(self[:id], @type).add_table(self)
       self
+    end
+
+    private
+
+    def _log_loop
+      logfile = vardir('log') + _file_base + "_#{Time.now.year}.log"
+      loop do
+        str = @queue.pop
+        open(logfile, 'a') { |f| f.puts str }
+        verbose { "Appended #{str.size} byte" }
+      end
     end
   end
 end

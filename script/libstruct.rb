fbfa6ade85c905da5ffa3a124dff8c832301d121
Koji Omata <omata@naoj.org>
2016-04-13 18:21:06 -1000

    refactoring ViewStruct (on the way);

--- script/libstruct.rb
+++ script/libstruct.rb
@@ -5,98 +5,136 @@ module CIAX
   module ViewStruct
     include Msg
 
-    COLOR_TBL = { 'true' => Msg.colorize('true', 13), 'false' => Msg.colorize('false', 8) }
-    def view_struct(show_iv = false, show_id = false, depth = 1)
-      @ind = 0
-      @column = 4
-      _recursive(self, nil, [], show_iv, show_id, depth)
+    COLOR_TBL = { true: 13, false: 8 }
+    def view_struct(show_iv = false, show_id = false)
+      @_vs_show_iv = show_iv
+      @_vs_show_id = show_id
+      @_vs_indent = 0
+      @_vs_column = 4
+      @_vs_iv_list = {}
+      @_vs_objects = []
+      @lines = []
+      # Show only top level of the instance variable
+      _recursive(self, nil)
+      @lines.join("\n")
     end
 
     private
 
-    def _recursive(data, title, object_ary, show_iv, show_id, depth)
-      str = ''
+    def _recursive(data, item)
       id = data.object_id
-      if title
-        _show_title(title, str)
-        str << colorize("(#{id})", 4) if show_id && data.is_a?(Enumerable)
-        str << " :\n"
+      if item
+        str = _indent(_mk_head(item))
+        str << colorize("(#{id})", 4) if @_vs_show_id && data.is_a?(Enumerable)
+        @lines << str + ' :'
       else
-        str << "<<#{data.class}>>\n"
+        @lines << "<<#{data.class}>>"
       end
-      _sub_structure(data, title, str, object_ary, show_iv, show_id, depth)
+      _mk_iv_list
+      _sub_structure(data, item)
     end
 
-    def _show_title(title, str)
-      case title
+    def _mk_head(item)
+      case item
       when Numeric
-        title = "[#{title}]"
-        str << indent(@ind) + colorize(format('%-6s', title), 6)
+        item = "[#{item}]"
+        colorize(format('%-6s', item), 6)
       when /@/
-        str << indent(@ind) + colorize(format('%-6s', title.inspect), 1)
+        colorize(format('%-6s', item.inspect), 1)
       else
-        c = title.is_a?(String) ? 5 : 2
-        str << indent(@ind) + colorize(format('%-6s', title.inspect), c)
+        c = item.is_a?(String) ? 5 : 2
+        colorize(format('%-6s', item.inspect), c)
       end
     end
 
-    def _sub_structure(data, title, str, object_ary, show_iv, show_id, depth)
-      @ind += 1
-      iv = {}
-      data.instance_variables.each do|n|
-        iv[n] = data.instance_variable_get(n) unless n == :object_ids
-        depth -= 1 # Show only top level of the instance variable
-      end if show_iv && depth > 0
-      _show(str, iv, object_ary, title, show_iv, show_id, depth)
-      _show(str, data, object_ary, title, show_iv, show_id, depth)
+    def _mk_iv_list
+      return unless @_vs_show_iv
+      instance_variables.each do|n|
+        @_vs_iv_list[n] = instance_variable_get(n) unless /_vs_/ =~ n.to_s
+      end
+      @_vs_show_iv = nil
+    end
+
+    def _sub_structure(data, item)
+      @_vs_indent += 1
+      #      _show_all(@_vs_iv_list, item)
+      _show_all(data, item)
     ensure
-      @ind -= 1
+      @_vs_indent -= 1
     end
 
-    def _show(str, data, object_ary, title, show_iv, show_id, depth)
-      if data.is_a?(Enumerable)
-        if object_ary.include?(data.object_id)
-          return str.chomp + " #{data.class}(Loop)\n"
-        else
-          object_ary << data.object_id
-        end
+    def _show_all(data, item)
+      return if _chk_loop(data)
+      return true if _show_enum(item, data)
+      color = COLOR_TBL[data.to_sym] if data.is_a? String
+      data = color ? colorize(data, color) : data.inspect
+      @lines.last << " #{data}"
+    end
+
+    def _chk_loop(data)
+      return unless data.is_a?(Enumerable)
+      if @_vs_objects.include?(data.object_id)
+        @lines << " #{data.class}(Loop)"
+      else
+        @_vs_objects << data.object_id
+        nil
       end
+    end
+
+    def _show_enum(item, data)
       case data
       when Array
-        return str if _mixed?(str, data, data, data.size.times, object_ary, show_iv, show_id, depth)
-        return _only_ary(str, data) if data.size > @column
+        _show_array(data)
       when Hash
-        return str if _mixed?(str, data, data.values, data.keys, object_ary, show_iv, show_id, depth)
-        return _only_hash(str, data, title) if data.size > 2
+        _show_hash(data, item)
       end
-      data = COLOR_TBL[data] || data.inspect
-      str.chomp + " #{data}\n"
     end
 
-    def _mixed?(str, data, vary, idx, object_ary, show_iv, show_id, depth)
+    def _show_array(data)
+      return true if _mixed?(data, data, data.size.times)
+      return unless data.size > @_vs_column
+      _only_ary(data)
+    end
+
+    def _show_hash(data, item)
+      return true if _mixed?(data, data.values, data.keys)
+      return unless data.size > 2
+      _only_hash(data, item)
+    end
+
+    def _mixed?(data, vary, idx)
       return unless vary.any? { |v| v.is_a?(Enumerable) }
       idx.each do|i|
-        str << _recursive(data[i], i, object_ary, show_iv, show_id, depth)
+        _recursive(data[i], i)
       end
     end
 
-    def _only_ary(str, data)
-      str << indent(@ind) + '['
+    def _only_ary(data)
+      @lines << _indent('[')
       line = []
-      data.each_slice(@column) do|a|
-        line << a.map(&:inspect).join(',')
+      data.each_slice(@_vs_column) do|a|
+        line << _indent(' ') + a.map(&:inspect).join(',')
       end
-      str << line.join(",\n " + indent(@ind)) + "]\n"
+      @lines << line.join(",\n")
+      @lines << _indent(']')
     end
 
-    def _only_hash(str, data, title)
-      data.keys.each_slice(title ? 2 : 1) do|a|
-        str << indent(@ind) + a.map do|k|
-          c = k.is_a?(String) ? 5 : 3
-          colorize(format('%-8s', k.inspect), c) + format(': %-10s', data[k].inspect)
-        end.join("\t") + "\n"
+    def _only_hash(data, item)
+      data.keys.each_slice(item ? 2 : 1) do|a|
+        @lines << _indent(_hash_line(a, data))
       end
-      str
+    end
+
+    def _hash_line(a, data)
+      a.map do|k|
+        c = k.is_a?(String) ? 5 : 3
+        val = format(': %-10s', data[k].inspect)
+        colorize(format('%-8s', k.inspect), c) + val
+      end.join("\t")
+    end
+
+    def _indent(str = '')
+      indent(@_vs_indent) + str
     end
   end
 end

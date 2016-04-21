d4ed9332e8acc729d134826b8536363aa274c83e
Koji Omata <omata@naoj.org>
2016-04-20 14:56:33 -1000

    Mcr::Conf: splitted off from libseq;

---
+++ script/libmcrconf.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/ruby
+require 'libconf'
+require 'libprompt'
+
+module CIAX
+  # Macro Layer
+  module Mcr
+    # Mcr Common Parameters
+    # Conf includes:
+    # :layer_type, :db, :command, :version, :sites, :dev_list, :sv_stat
+    # :host, :port
+    class Conf < Config
+      def initialize(cfg)
+        super(cfg)
+        db = Db.new
+        dbi = db.get
+        update(layer_type: 'mcr', db: db)
+        # pick already includes :command, :version
+        update(dbi.pick([:sites, :id]))
+        _init_net(dbi)
+        _init_dev_list(cfg)
+      end
+
+      private
+
+      def _init_net(dbi)
+        self[:host] = self[:option].host || dbi[:host]
+        self[:port] = dbi[:port] || 55_555
+      end
+
+      # Take App List
+      def _init_dev_list(cfg)
+        self[:dev_list] = Wat::List.new(cfg, src: 'macro')
+        self[:sv_stat] = Prompt.new(self[:id], self[:option])
+      end
+    end
+
+    # Prompt for Mcr
+    class Prompt < Prompt
+      def initialize(id, opt = {})
+        super('mcr', id)
+        add_array(:list)
+        add_array(:run)
+        add_str(:sid)
+        add_flg(nonstop: '(nonstop)')
+        up(:nonstop) if opt[:n]
+      end
+    end
+  end
+end
--- script/libseq.rb
+++ script/libseq.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+require 'libmcrconf'
 require 'libseqfunc'
 
 module CIAX
@@ -116,48 +117,6 @@ module CIAX
         else
           print str
         end
-      end
-    end
-
-    # Prompt for Mcr
-    class Prompt < Prompt
-      def initialize(id, opt = {})
-        super('mcr', id)
-        add_array(:list)
-        add_array(:run)
-        add_str(:sid)
-        add_flg(nonstop: '(nonstop)')
-        up(:nonstop) if opt[:n]
-      end
-    end
-
-    # Mcr Common Parameters
-    # Conf includes:
-    # :layer_type, :db, :command, :version, :sites, :dev_list, :sv_stat
-    # :host, :port
-    class Conf < Config
-      def initialize(cfg)
-        super(cfg)
-        db = Db.new
-        dbi = db.get
-        update(layer_type: 'mcr', db: db)
-        # pick already includes :command, :version
-        update(dbi.pick([:sites, :id]))
-        _init_net(dbi)
-        _init_dev_list(cfg)
-      end
-
-      private
-
-      def _init_net(dbi)
-        self[:host] = self[:option].host || dbi[:host]
-        self[:port] = dbi[:port] || 55_555
-      end
-
-      # Take App List
-      def _init_dev_list(cfg)
-        self[:dev_list] = Wat::List.new(cfg, src: 'macro')
-        self[:sv_stat] = Prompt.new(self[:id], self[:option])
       end
     end
 

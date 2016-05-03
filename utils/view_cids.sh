9e3ccc1501ef982ccdb5e59ac108158e223c7a2d
Koji Omata <omata@naoj.org>
2016-05-03 09:48:57 -1000

    view_cids: add search feature;

--- utils/view_cids.sh
+++ utils/view_cids.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Record list specified by cid
+# Usage: cids (serch word)
 #alias cids
 get_date(){
     [[ $1 =~ [0-9] ]] && date -d +@${1:0:10} || echo "$1"
 }
+search=${1:-.}
 IFS=:
 while read fpath tag val;do
     utime=${fpath#*_}
@@ -14,4 +16,4 @@ while read fpath tag val;do
         echo " -> $val"
         crnt=$utime
     fi
-done < <(egrep -o -e '"cid":[^,]*' -e '"result":[^,]*' ~/.var/json/record*)
+done < <(egrep -o -e '"cid":[^,]*' -e '"result":[^,]*' ~/.var/json/record*) | grep $search

7aa64a1a5d4c0303736b45ebad886ba2d0fd555d
Koji Omata <omata@naoj.org>
2016-05-02 16:10:17 -1000

    record.js: sub_mcr is nested; add record_latest.html: as a templete;

--- webapp/record.js
+++ webapp/record.js
@@ -1,23 +1,37 @@
 // Need var: Type,Site
+function mkstep(step, all){
+    if(step.conditions){
+        all.push("<ul>");
+        for(var k in step.conditions){
+            var cond=step.conditions[k];          
+            all.push("<li>" + cond.site + ":" + cond.var + " (" + cond.res + ")" + "</li>");
+        }
+        all.push("</ul>");
+    }else{
+        all.push(" (" + step.site + ")");
+    }
+    all.push("</li>");
+} 
+
 function j2ul(){
     $.getJSON("record_latest.json", function(data) {
         var all = [];
+        var depth = 0;
         all.push("<li>" + data.label +"</li>");
         all.push("<ul>");
         for(var j in data.steps){
             var step=data.steps[j];
-            all.push("<li>" + step.type);
-            if(step.conditions){
-                all.push("<ul>");
-                for(var k in step.conditions){
-                    var cond=step.conditions[k];          
-                    all.push("<li>" + cond.site + ":" + cond.var + " (" + cond.res + ")" + "</li>");
-                }
-                all.push("</ul>");
-            }else{
-                all.push(" (" + step.site + ")");
+            if(step.depth == depth){
+                all.push("</ul>")
+                depth-=1;
             }
-            all.push("</li>");
+            all.push("<li>" + step.type);
+            if(step.type == 'mcr'){
+                all.push("("+step.args[0]+")<ul>");
+                depth=step.depth;
+            } else{
+                mkstep(step, all);
+            }
         }
         all.push("</ul>");
         all.push("<li>" + data.result + "<li>");

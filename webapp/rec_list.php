818429ec7dedfa7798e11c41fbaad545c2c6a08f
Koji Omata <omata@naoj.org>
2016-05-05 18:38:58 -1000

    rec_list.php: add cache feature;

--- webapp/rec_list.php
+++ webapp/rec_list.php
@@ -1,10 +1,15 @@
 <?php
-$array=array();
+$cache="rec_list.json";
+$list=file_get_contents($cache);
+$array = $list ? json_decode($list,true) : array();
 foreach(glob("record*.json") as $url){
+    if(! ereg("[0-9]+",$url,$regs)) continue;
+    if($array[$regs[0]]) continue;
     $json=file_get_contents($url);
     $obj=json_decode($json,true);
-    $line=array($obj["id"],$obj["cid"],$obj["result"]);
-    array_push($array,$line);
- }
-print(json_encode($array, JSON_PRETTY_PRINT));
+    $array[(string)$obj["id"]]=array($obj["cid"],$obj["result"]);
+}
+$json=json_encode($array, JSON_PRETTY_PRINT);
+file_put_contents($cache, $json);
+print($json);
 ?>

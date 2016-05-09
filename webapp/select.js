1d3ab6a07000dec2e3d021952bab4ba2bb9128ac
Koji Omata <omata@naoj.org>
2016-05-09 11:29:57 -1000

    select.js: correct date display, coloring cmd and result;

--- webapp/select.js
+++ webapp/select.js
@@ -5,12 +5,19 @@ function update() {
     $.getJSON('rec_list.php', function(data) {
         all.push('<ul>');
         for (var j in data) {
-            var date = new Date(j+0)
-            all.push('<li>' + date +'/'+ j+ '(' + data[j][0] + ' ->' + data[j][1] +')</li>');
+            var date = new Date(j-0) // cast to num
+            var res=data[j][1];
+            all.push('<li>' + date);
+            all.push('[<span class = "label">' + data[j][0] + '</span>');
+            var co=cls[res] ? cls[res] : 'alarm';
+            all.push(' -> <span class="' + co + '">');
+            all.push(res +'</span>]');
+            all.push('</li>');
         }
         all.push('</ul>');
         $('#output')[0].innerHTML = all.join('');
     });
 }
 var all = [];
+var cls={ 'complete':'normal', 'interrupted':'warn', 'busy':'active'}
 $(document).ready(update);

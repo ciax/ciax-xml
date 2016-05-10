1f013708b5f4e3b6a49cad5e38fe8c40418eeaa3
Koji Omata <omata@naoj.org>
2016-05-09 14:26:58 -1000

    add record.php: to take log by id;

---
+++ webapp/record.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>CIAX-XML(Record)</title>
+<link rel="stylesheet" type="text/css" href="ciax-xml.css"/>
+     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+     <script type="text/javascript" src="record.js"></script>
+     <script type="text/javascript">
+<?php
+     $tag='latest';
+if(isset($_GET['id'])){ $tag=$_GET['id'];};
+echo 'tag="' . $tag . '";';
+?>
+$(document).ready(update);
+</script>
+<body>
+<div class="outline">
+    <div class="title">Latest Macro</div>
+    <div id="output"></div>
+    </div>
+    </body>
+    </html>
--- webapp/select.js
+++ webapp/select.js
@@ -1,21 +1,25 @@
 // Recommended Package: closure-linter
 // fixjsstyle select.js
-function time_list(time, cid, res) {
-    all.push('<li><span class="time">' + time.toLocaleTimeString() + '</span>');
+function time_list(key, time, cid, res) {
+    all.push('<li>');
+    all.push('<span class="time">' + time.toLocaleTimeString() + '</span>');
+    all.push('<a href="record.php?id=' + key + '" target=FRM2>');
     all.push(' [<span class="cmd">' + cid + '</span>]');
+    all.push('</a>');
     var co = cls[res] ? cls[res] : 'alarm';
     all.push(' -> <span class="' + co + '">');
     all.push(res + '</span>');
     all.push('</li>');
 }
 
-function date_list(time, cid, res) {
+function date_list(key, ary) {
+    var time=new Date(key - 0);
     var crd = time.toLocaleDateString();
     if (date != crd) {
         all.push('</ul><h4>' + crd + '</h4><ul>');
         date = crd;
     }
-    time_list(time, cid, res);
+    time_list(key, time, ary[0], ary[1]);
 }
 function update() {
     all = [];
@@ -27,7 +31,7 @@ function update() {
         for (var i = 0; i < keys.length; i++) {
             // Date(j-0) -> cast to num
             var key=keys[i];
-            date_list(new Date(key - 0), data[key][0], data[key][1]);
+            date_list(key, data[key]);
         }
         all.push('</ul>');
         $('#output')[0].innerHTML = all.join('');

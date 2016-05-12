9d53f6654cee4c0770b62cc33c769637e169c4d8
Koji Omata <omata@naoj.org>
2016-05-12 10:55:47 -1000

    add getsid.php: get arg part is separated off;

--- webapp/record.php
+++ webapp/record.php
@@ -1,25 +1,18 @@
 <!DOCTYPE html>
 <html lang="en-US">
-<head>
-<title>CIAX-XML(Record)</title>
-<link rel="stylesheet" type="text/css" href="ciax-xml.css"/>
-     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-     <script type="text/javascript" src="record.js"></script>
-     <script type="text/javascript">
-<?php
-      $tag='latest';
-if(isset($_GET['id'])){ $tag=$_GET['id'];};
-echo 'tag="' . $tag . '";';
-?>
-$(document).ready(static);
-</script>
-<body>
-<div class="outline">
-    <div class="title">
-    <span class="mcr">Macro Log</span>:
-    <span class="cmd" id="mcrcmd"></span>
-    </div>
-    <div id="output"></div>
-    </div>
+  <head>
+    <title>CIAX-XML(Record)</title>
+    <link rel="stylesheet" type="text/css" href="ciax-xml.css"/>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="record.js"></script>
+    <script type="text/javascript">tag="<?php include('getsid.php'); ?>";$(document).ready(static);</script>
+    <body>
+      <div class="outline">
+        <div class="title">
+          <span class="mcr">Macro Log</span>:
+          <span class="cmd" id="mcrcmd"></span>
+        </div>
+        <div id="output"></div>
+      </div>
     </body>
-    </html>
+</html>

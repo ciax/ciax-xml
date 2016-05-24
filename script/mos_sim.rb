7532aa89749719ca911bd8d3ab57d678fb51cce6
Koji Omata(pingu) <koji.omata@gmail.com>
2016-05-24 03:12:28 -1000

    mos_sim: add comments;

--- script/mos_sim.rb
+++ script/mos_sim.rb
@@ -5,6 +5,24 @@ require 'libsimbb'
 require 'libsimcar'
 require 'libsimfp'
 
+#### Condition for Contact Sensor ####
+# MODE  | MMA:POS | MFP:AO | MFP:RH || MMA:CON | MMC:CON
+#-------+---------+--------+--------++---------+---------
+# STORE |  -      |  -     |  -     || OFF     | ON
+# LOAD  | INIT    |  -     |  -     || OFF     | OFF
+# LOAD  | FOCUS   | OPEN   | OPEN   || OFF     | OFF
+# LOAD  | FOCUS   | CLOSE  | OPEN   || OFF     | OFF
+# LOAD  | FOCUS   | OPEN   | CLOSE  || OFF     | OFF
+# LOAD  | FOCUS   | CLOSE  | CLOSE  || ON      | OFF
+# LOAD  | ROT     |  -     |  -     || OFF     | OFF
+# LOAD  | W-S     | CLOSE  |  -     || ON      | ON
+
+#### Condition for Mode ####
+# MMA:POS | MFP:AO || MODE
+#---------+--------++------
+# STORE   | OPEN   || STORE
+# STORE   | CLOSE  || LOAD
+
 module CIAX::Simulator
   cfg = Conf.new
   mods = [Arm, Ap, BBIO, Carousel, FpDio]

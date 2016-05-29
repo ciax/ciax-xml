216cc07deac761266c714f59959d8bfcc3357fbf
Koji Omata(pingu) <koji.omata@gmail.com>
2016-05-29 12:38:20 -1000

    JS: record: make_step() is turned to closure;

--- webapp/record.js
+++ webapp/record.js
@@ -2,105 +2,107 @@
 // fixjsstyle record.js
 // ********* Steps **********
 // step header section
-function step_title(type) {
-    html_rec.push('<span class="head ' + type + '">' + type + '</span>');
-    html_rec.push('<span class="cmd">');
-}
-function step_label(step) {
-    if (step.label) {html_rec.push(': ' + step.label);}
-}
-function step_cmd(step) {
-    var ary = [];
-    if (step.site) { ary.push(step.site); }
-    if (step.args) { ary = ary.concat(step.args); }
-    if (step.val) { ary.push(step.val); }
-    if (ary.length > 0) {html_rec.push(': [' + ary.join(':') + ']');}
-    html_rec.push('</span>');
-}
-// result section
-function step_result(res) {
-    if (res) {
-        html_rec.push(' -> ');
-        html_rec.push('<em class="res ' + res + '">' + res + '</em>');
-    }
-}
-function step_action(step) {
-    if (step.action) {
-        html_rec.push(' <span class="action">(');
-        html_rec.push(step.action);
-        html_rec.push(')</span>');
-    }
-}
-// elapsed time section
-function step_time(step) {
-    var now = new Date(step.time);
-    var elps = ((now - start_time) / 1000).toFixed(2);
-    html_rec.push('<span class="elps tail">[' + elps + ']</span>');
-}
-// waiting step
-function step_meter(step, max) {
-    html_rec.push(' <meter value="' + step.count / max * 100 + '" max="100"');
-    if (step.retry) html_rec.push('low="70" high="99"');
-    html_rec.push('>(' + step.count + '/' + max + ')</meter>');
-}
-function step_count(step) {
-    if (step.count) {
-        var max = step.retry || step.val;
-        if (step.type != 'mcr') step_meter(step, max);
-        html_rec.push('<span>(' + step.count + '/' + max + ')</span>');
-        if (step.busy) html_rec.push(' -> <em class="res active">Busy</em>');
-    }
-}
-// other steps
-function step_exe(step) {
-    html_rec.push('<h4>');
-    step_title(step.type);
-    step_label(step);
-    step_cmd(step);
-    step_count(step);
-    step_result(step.result);
-    step_action(step);
-    step_time(step);
-    html_rec.push('</h4>');
-}
-// condition step
-function operator(ope, cri) {
-    switch (ope) {
-    case 'equal': return ('== ' + cri); break;
-    case 'not' : return ('!= ' + cri); break;
-    case 'match' : return ('=~ /' + cri + '/'); break;
-    case 'unmatch' : return ('!~ /' + cri + '/'); break;
-    default:
-    }
-}
-function cond_list(conds, type) {
-    for (var k in conds) {
-        var cond = conds[k];
-        var res = cond.res;
-        html_rec.push('<li>');
-        html_rec.push('<var>' + cond.site + ':' + cond.var + '(' + cond.form + ')</var>');
-        html_rec.push('<code>' + operator(cond.cmp, cond.cri) + '?</code>  ');
-        if (type == 'goal' && res == false) res = 'warn';
-        html_rec.push('<em class="' + res + '"> (' + cond.real + ')</em>');
-        html_rec.push('</li>');
-    }
-}
-function step_cond(step) {
-    html_rec.push('<ul>');
-    cond_list(step.conditions, step.type);
-    html_rec.push('</ul>');
-}
-// Make Step Line
 function make_step(step) {
-    html_rec = ['<li>'];
+    var html_rec = ['<li>'];
     step_exe(step);
     if (step.conditions) {
         step_cond(step);
     }else if (step.type == 'mcr') {
-        html_rec.push('<ul class="depth' + (step.depth - 0 + 1) + '"></ul>');
+        step_mcr(step);
     }
     html_rec.push('</li>');
     $('.depth' + step.depth + ':last').append(html_rec.join(''));
+
+    function step_title(type) {
+        html_rec.push('<span class="head ' + type + '">' + type + '</span>');
+        html_rec.push('<span class="cmd">');
+    }
+    function step_label(step) {
+        if (step.label) {html_rec.push(': ' + step.label);}
+    }
+    function step_cmd(step) {
+        var ary = [];
+        if (step.site) { ary.push(step.site); }
+        if (step.args) { ary = ary.concat(step.args); }
+        if (step.val) { ary.push(step.val); }
+        if (ary.length > 0) {html_rec.push(': [' + ary.join(':') + ']');}
+        html_rec.push('</span>');
+    }
+    // result section
+    function step_result(res) {
+        if (res) {
+            html_rec.push(' -> ');
+            html_rec.push('<em class="res ' + res + '">' + res + '</em>');
+        }
+    }
+    function step_action(step) {
+        if (step.action) {
+            html_rec.push(' <span class="action">(');
+            html_rec.push(step.action);
+            html_rec.push(')</span>');
+        }
+    }
+    // elapsed time section
+    function step_time(step) {
+        var now = new Date(step.time);
+        var elps = ((now - start_time) / 1000).toFixed(2);
+        html_rec.push('<span class="elps tail">[' + elps + ']</span>');
+    }
+    // waiting step
+    function step_meter(step, max) {
+        html_rec.push(' <meter value="' + step.count / max * 100 + '" max="100"');
+        if (step.retry) html_rec.push('low="70" high="99"');
+        html_rec.push('>(' + step.count + '/' + max + ')</meter>');
+    }
+    function step_count(step) {
+        if (step.count) {
+            var max = step.retry || step.val;
+            if (step.type != 'mcr') step_meter(step, max);
+            html_rec.push('<span>(' + step.count + '/' + max + ')</span>');
+            if (step.busy) html_rec.push(' -> <em class="res active">Busy</em>');
+        }
+    }
+    // other steps
+    function step_exe(step) {
+        html_rec.push('<h4>');
+        step_title(step.type);
+        step_label(step);
+        step_cmd(step);
+        step_count(step);
+        step_result(step.result);
+        step_action(step);
+        step_time(step);
+        html_rec.push('</h4>');
+    }
+    // condition step
+    function operator(ope, cri) {
+        switch (ope) {
+        case 'equal': return ('== ' + cri); break;
+        case 'not' : return ('!= ' + cri); break;
+        case 'match' : return ('=~ /' + cri + '/'); break;
+        case 'unmatch' : return ('!~ /' + cri + '/'); break;
+        default:
+        }
+    }
+    function step_cond(step) {
+        var conds = step.conditions;
+        html_rec.push('<ul>');
+        for (var k in conds) {
+            var cond = conds[k];
+            var res = cond.res;
+            html_rec.push('<li>');
+            html_rec.push('<var>' + cond.site + ':' + cond.var + '(' + cond.form + ')</var>');
+            html_rec.push('<code>' + operator(cond.cmp, cond.cri) + '?</code>  ');
+            if (step.type == 'goal' && res == false) res = 'warn';
+            html_rec.push('<em class="' + res + '"> (' + cond.real + ')</em>');
+            html_rec.push('</li>');
+        }
+        html_rec.push('</ul>');
+    }
+    function step_mcr(step) {
+        html_rec.push('<ul class="depth' + (step.depth - 0 + 1) + '"></ul>');
+    }
+
 }
 // ********* Record **********
 // Macro Body
@@ -194,11 +196,9 @@ function static_page(data) {
 }
 // ******** HTML ********
 function archive(tag) {
-    depth = 1;
     $.getJSON('record_' + tag + '.json', static_page);
 }
 function update() {
-    depth = 1;
     $.getJSON('record_latest.json', dynamic_page);
     remain_msg();
 }
@@ -222,12 +222,9 @@ function init_record() {
     update();
 }
 // Var setting
-var html_rec;
-var depth = 1;
 var start_time = ''; // For elapsed time
 var last_time = '';  // For detecting update
 var last_upd = '';   // For prevent multiple update during no data changes
 var first_time = ''; // For first time at a new macro;
 var tag = 'latest';
-var option = [];
 var port;

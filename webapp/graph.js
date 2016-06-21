a10ad8e8e13d8d3c16bdf21099da5bc2221b9cf6
Koji Omata(pingu) <koji.omata@gmail.com>
2016-06-21 04:18:10 -1000

    JS: graph: add zoom feature;

--- webapp/graph.js
+++ webapp/graph.js
@@ -1,7 +1,17 @@
 function init() { get_graph('tap', 'pres'); }
 function get_graph(site, vid) {
     $.getJSON('get-log.php?site=' + site + '&vid=' + vid, function(data) {
-        var options = { xaxis: { mode: 'time'} };
+        var options = {
+            xaxis: {
+                mode: 'time'
+            },
+            zoom: {
+                interactive: true
+            },
+            pan: {
+                interactive: true
+            }
+        };
         $.plot($('#placeholder'), [data], options);
     });
 }

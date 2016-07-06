088911ba1d66cc2484a3cc70ba70e94587ff2256
CIAX <ciax@naoj.org>
2016-07-05 14:24:22 -1000

    setup-www: add flot install feature;

--- utils/setup-www.sh
+++ utils/setup-www.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Required packages: php5-sqlite
+# Required packages: php5-sqlite yui-compressor
 dir=$HOME/.var/json
 for i in $dir/*; do
     [ -L "$i" -a ! -e "$i" ] && rm "$i"
@@ -10,3 +10,12 @@ for i in json log record; do
     sudo ln -sf ~/.var/$i /var/www/html
 done
 sudo ln -sf $(pwd -P)/*.js $dir
+# install jslib
+[ -d ~/jslib ] || mkdir ~/jslib
+cd ~/jslib
+if [ ! -d flot ] ; then
+    git clone http://github.com/flot/flot.git
+    cd flot
+    make
+    sudo ln -sf $(pwd -P)/*.js $dir
+fi    

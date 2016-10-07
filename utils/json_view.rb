2fa1117b2b5d9c411b9cda67a89e467c1ace1a64
omata <omata@localhost.localdomain>
2016-10-06 16:39:28 -1000

    rubocopping in utils;

--- utils/json_view.rb
+++ utils/json_view.rb
@@ -2,6 +2,6 @@
 # JSON to String(Decorated)
 #alias jv
 require 'libenumx'
-abort 'Usage: json_view [json_file]' if STDIN.tty? && ARGV.size < 1
+abort 'Usage: json_view [json_file]' if STDIN.tty? && ARGV.empty?
 str = gets(nil) || exit
 puts JSON.parse(str, symbolize_names: true).extend(CIAX::Enumx).to_v

81247af97780bf0b121ac7b29c3177ae52e82d64
MOIRCS user <moircs@moircsobcp.sum.subaru.nao.ac.jp>
2016-10-31 12:42:03 -1000

    add dvlog.php for log view;

---
+++ webapp/dvlog.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8"/>
+    <script type="text/javascript" src="jquery-3.0.0.min.js"></script>
+    <script type="text/javascript" src="graph.js"></script>
+    <script type="text/javascript">
+      var par = <?php echo json_encode($_GET); ?>;
+      $(get_graph);
+    </script>
+   </head>
+  <body>
+    <div id="placeholder" style="width:600px;height:300px;"></div>
+    <div>
+      <input id="date" type="date" onchange="mv_date(this)"/>
+      <button class="log" id="log" onclick="get_log();">LOG</button>
+    </div>
+  </body>
+</html>

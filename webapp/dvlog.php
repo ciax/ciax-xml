0893d0863fb621a1053c3d504db47e7c73f26583
omata <omata@localhost.localdomain>
2016-11-23 12:19:36 -1000

    dvlog.php: update appearance;

--- webapp/dvlog.php
+++ webapp/dvlog.php
@@ -7,21 +7,23 @@
     <script type="text/javascript">
       var par = <?php echo json_encode($_GET); ?>;
       $.getJSON('sqlog.php', par, function(obj) {
-      console.log(par);
-      obj[0].data.forEach(function(pair) {
-      var date = new Date(pair[0] - 0);
-      var time = date.toLocaleTimeString('en-US', {hour12: false})
-      $("tbody").append('<tr><td>' + time + '</td><td>' + pair[1] + '</td></tr>');
-      });
+        $(".title").append('<span class="head">' + par.site + ':' + par.vid + '</span>');
+        obj[0].data.forEach(function(pair) {
+          var date = new Date(pair[0] - 0);
+          var time = date.toLocaleTimeString('en-US', {hour12: false})
+          $("tbody").append('<tr><td><span class="label">' + time + '</span></td><td class="item"><var>' + pair[1] + '</var></td></tr>');
+        });
       });
     </script>
    </head>
   <body>
     <div class="outline">
-    <table>
-      <tbody>
-      </tbody>
-    </table>
-    </div>
+      <div class="title"></div>
+      <table>
+        <tbody>
+          <tr><th>Time</th><th>Value</th></tr>
+        </tbody>
+      </table>
+      </div>
   </body>
 </html>

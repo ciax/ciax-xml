99c32ce00d074b3019e113a65cad35f037975fe4
CIAX <ciax@naoj.org>
2017-06-06 13:30:20 -1000

    add json_logdate.sh: pick up stream log by date;

---
+++ utils/json_logdate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Pick up by date from Json Log
+#alias jld
+[ "$2" ] || { echo "Usage: ${0##*/} [site] [date]"; exit 1; }
+id=$1;shift
+date=$1;shift
+year=$(date -d $date +%Y)
+sec=$(date -d $date +%s)
+exp=:${sec:0:6}
+egrep "$exp" ~/.var/log/stream_${id}_$year.log

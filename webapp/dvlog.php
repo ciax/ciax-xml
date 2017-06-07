4c7687eaba9512ee2ba04c07a5f4add28ced976b
CIAX <ciax@naoj.org>
2017-06-06 14:29:58 -1000

    dvlog.js was separated from dvlog.php;

--- webapp/dvlog.php
+++ webapp/dvlog.php
@@ -4,21 +4,10 @@
     <meta charset="utf-8"/>
     <link rel="stylesheet" type="text/css" href="ciax-xml.css"/>
     <script type="text/javascript" src="jquery-3.0.0.min.js"></script>
-      <script type="text/javascript">
-        var par = <?php echo json_encode($_GET); ?>;
-        $.getJSON('sqlog.php', par, function(obj) {
-        $(".title").append('<span class="head">' + par.site + ':' + par.vid + '</span>');
-        obj[0].data.forEach(function(pair) {
-        var datetime = new Date(pair[0] - 0);
-        var tr = '<tr><td>';
-            tr += '<span class="label" title="' + datetime.toLocaleDateString('en-US') + '">';
-              tr += datetime.toLocaleTimeString('en-US', {hour12: false});
-              tr += '</span></td><td class="item"><var>';
-              tr += pair[1] + '</var></td></tr>';
-        $("tbody").append(tr);
-        });
-        });
-      </script>
+    <script type="text/javascript">
+      var par = <?php echo json_encode($_GET); ?>;
+    </script>
+    <script type="text/javascript" src="dvlog.js"></script>
   </head>
   <body>
     <div class="outline">

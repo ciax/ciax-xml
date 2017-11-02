e069e43d027f79fa709a211e0addb389bf988399
Omata@leonid <omata@naoj.org>
2017-11-01 15:56:55 -1000

    libfrm: sub modules (Codec, CheckCode) are gets into Frame;

--- script/libfrmcheck.rb
+++ script/libfrmcheck.rb
@@ -4,68 +4,71 @@ require 'libmsg'
 module CIAX
   # Frame Layer
   module Frm
-    # Check Code
-    class CheckCode
-      include Msg
-      def initialize(ccmethod)
-        @method = ccmethod
-        @ccrange = nil
-        @checkcode = ''
-      end
-
-      # Push to check code
-      def push(str) # returns self
-        @ccrange << str if @ccrange
-        verbose { "Cc Add to Range Frame [#{str.inspect}]" }
-        self
-      end
-
-      def enclose
-        verbose { 'Cc Mark Range Start' }
-        @ccrange = ''
-        yield self
-        verbose { "Cc Frame [#{@ccrange.inspect}]" }
-        @checkcode = _calcurate_.to_s
-        verbose { "Cc Calc [#{@method.upcase}] -> (#{@checkcode})" }
-        @ccrange = nil
-        self
-      end
-
-      def check(cc)
-        return self unless cc
-        if cc == @checkcode
-          verbose { "Cc Verify OK [#{cc}]" }
-        else
-          fmt = 'CC Mismatch:[%s] (should be [%s]) in [%s]'
-          cc_err(format(fmt, cc, @checkcode, @ccrange.inspect))
+    # For Command/Response Frame
+    class Frame
+      # Check Code
+      class CheckCode
+        include Msg
+        def initialize(ccmethod)
+          @method = ccmethod
+          @ccrange = nil
+          @checkcode = ''
         end
-        self
-      end
 
-      def to_s
-        @checkcode
-      end
+        # Push to check code
+        def push(str) # returns self
+          @ccrange << str if @ccrange
+          verbose { "Cc Add to Range Frame [#{str.inspect}]" }
+          self
+        end
 
-      private
+        def enclose
+          verbose { 'Cc Mark Range Start' }
+          @ccrange = ''
+          yield self
+          verbose { "Cc Frame [#{@ccrange.inspect}]" }
+          @checkcode = _calcurate_.to_s
+          verbose { "Cc Calc [#{@method.upcase}] -> (#{@checkcode})" }
+          @ccrange = nil
+          self
+        end
 
-      def _calcurate_ # Check Code End
-        method("cc_#{@method}").call
-      rescue NameError
-        Msg.cfg_err("No such CC method #{@method}")
-      end
+        def check(cc)
+          return self unless cc
+          if cc == @checkcode
+            verbose { "Cc Verify OK [#{cc}]" }
+          else
+            fmt = 'CC Mismatch:[%s] (should be [%s]) in [%s]'
+            cc_err(format(fmt, cc, @checkcode, @ccrange.inspect))
+          end
+          self
+        end
 
-      def cc_len
-        @ccrange.length
-      end
+        def to_s
+          @checkcode
+        end
 
-      def cc_bcc
-        chk = 0
-        @ccrange.each_byte { |c| chk ^= c }
-        chk
-      end
+        private
 
-      def cc_sum
-        @ccrange.sum(8)
+        def _calcurate_ # Check Code End
+          method("cc_#{@method}").call
+        rescue NameError
+          Msg.cfg_err("No such CC method #{@method}")
+        end
+
+        def cc_len
+          @ccrange.length
+        end
+
+        def cc_bcc
+          chk = 0
+          @ccrange.each_byte { |c| chk ^= c }
+          chk
+        end
+
+        def cc_sum
+          @ccrange.sum(8)
+        end
       end
     end
   end
--- script/libfrmcode.rb
+++ script/libfrmcode.rb
@@ -5,69 +5,72 @@ module CIAX
   # Frame Layer
   module Frm
     # For Command/Response Frame
-    module Codec
-      attr_reader :endian
-      def decode(code, e0) # Chr -> Num
-        cdc = e0[:decode]
-        return code.to_s unless cdc
-        num, base = _num_by_type_(code, cdc)
-        if e0[:sign] == 'msb'
-          range = base**code.size
-          num = num < range / 2 ? num : num - range
+    class Frame
+      # For Codec module
+      module Codec
+        attr_reader :endian
+        def decode(code, e0) # Chr -> Num
+          cdc = e0[:decode]
+          return code.to_s unless cdc
+          num, base = _num_by_type_(code, cdc)
+          if e0[:sign] == 'msb'
+            range = base**code.size
+            num = num < range / 2 ? num : num - range
+          end
+          verbose { "Decode:(#{cdc}) [#{code.inspect}] -> [#{num}]" }
+          num.to_s
         end
-        verbose { "Decode:(#{cdc}) [#{code.inspect}] -> [#{num}]" }
-        num.to_s
-      end
 
-      def encode(str, e0) # Num -> Chr
-        str = e0[:format] % expr(str) if e0[:format]
-        _conv_len_(e0[:length], str)
-      end
-
-      private
-
-      def _conv_len_(len, str)
-        return str unless len
-        code = ''
-        num = expr(str)
-        len.to_i.times do
-          c = (num % 256).chr
-          num /= 256
-          code = @endian == 'little' ? code + c : c + code
+        def encode(str, e0) # Num -> Chr
+          str = e0[:format] % expr(str) if e0[:format]
+          _conv_len_(e0[:length], str)
         end
-        verbose { "Encode:[#{str}](#{len}) -> [#{code.inspect}]" }
-        code
-      end
 
-      def _num_by_type_(code, type)
-        method("dec_#{type}").call(code)
-      rescue NameError
-        dec_integer(code)
-      end
+        private
 
-      def dec_hexstr(code) # "FF" -> "255"
-        [code.hex, 16]
-      end
+        def _conv_len_(len, str)
+          return str unless len
+          code = ''
+          num = expr(str)
+          len.to_i.times do
+            c = (num % 256).chr
+            num /= 256
+            code = @endian == 'little' ? code + c : c + code
+          end
+          verbose { "Encode:[#{str}](#{len}) -> [#{code.inspect}]" }
+          code
+        end
 
-      def dec_decstr(code) # "80000123" -> "-123"
-        # sign: k3n=F, oss=8,
-        sign = /[8Ff]/ =~ code.slice!(0) ? '-' : ''
-        code.sub!(/^0+/, '')
-        num = code.empty? ? '0' : sign + code
-        [num, 10]
-      end
+        def _num_by_type_(code, type)
+          method("dec_#{type}").call(code)
+        rescue NameError
+          dec_integer(code)
+        end
 
-      def dec_binstr(code)
-        num = [code].pack('b*').ord
-        [num, 2]
-      end
+        def dec_hexstr(code) # "FF" -> "255"
+          [code.hex, 16]
+        end
 
-      def dec_integer(code)
-        # integer
-        ary = code.unpack('C*')
-        ary.reverse! if @endian == 'little'
-        num = ary.inject(0) { |a, e| a * 256 + e }
-        [num, 256]
+        def dec_decstr(code) # "80000123" -> "-123"
+          # sign: k3n=F, oss=8,
+          sign = /[8Ff]/ =~ code.slice!(0) ? '-' : ''
+          code.sub!(/^0+/, '')
+          num = code.empty? ? '0' : sign + code
+          [num, 10]
+        end
+
+        def dec_binstr(code)
+          num = [code].pack('b*').ord
+          [num, 2]
+        end
+
+        def dec_integer(code)
+          # integer
+          ary = code.unpack('C*')
+          ary.reverse! if @endian == 'little'
+          num = ary.inject(0) { |a, e| a * 256 + e }
+          [num, 256]
+        end
       end
     end
   end

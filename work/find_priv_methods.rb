4db463cd86ecdc22bf194161e7769bda4d93bc1d
Omata@leonid <omata@naoj.org>
2017-11-08 16:23:25 -1000

    find_priv_method: add -d(def line only), -i (include exp) options;

--- work/find_priv_methods.rb
+++ work/find_priv_methods.rb
@@ -1,20 +1,38 @@
 #!/usr/bin/ruby
 #alias fpm
+require 'optparse'
 def getline
-  while (line = ARGF.gets)
-    line =~ /^ */
+  base = nil
+  ary = []
+  while (input = ARGF.gets)
+    /^ */ =~ input
     splen = $&.length
-    next if splen < 1 || $' =~ /^#/
-    yield line, splen
+    next if splen < 1 || /^#/ =~ $'
+    break if (base && (splen < base)) || /^ *(class|module)/ =~ input
+    ary << [ARGF.filename, input]
+    break true if base && splen <= base
+    base = splen unless base
+  end
+ensure
+  puts show(ary)
+end
+
+def show(aryary)
+  return [] if (exp = @opt['i']) && !aryary.any? { |_f, line| /#{exp}/ =~ line }
+  if @opt['d']
+    aryary.map do |f, line|
+      format("%s\t%s", Regexp.last_match(1), f) if /^ *def (\w+)/ =~ line
+    end.compact
+  else
+    aryary.map { |f, line| format("%s\t%s", f, line) }
   end
 end
 
-getline do |line1, base|
-  next if line1 !~ /^ *private/
-  getline do |line2, ind|
-    break if base > ind || line2 =~ /^ *(class|module)/
-    next if line2 !~ /^ *def/
-    ($' =~ /\w+/)
-    puts format("%s\t%s", $&, ARGF.filename)
-  end
+# -d       : pick up method name
+# -i [exp] : show method which include [exp]
+@opt = {}
+ARGV.getopts('di:').each { |k, v| @opt[k] = v }
+while (input = ARGF.gets)
+  next if /^ *private/ !~ input
+  true while getline
 end

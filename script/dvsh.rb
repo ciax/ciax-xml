fc24d57af482b88207e45f982d9f1fa61e911943
Omata@leonid <omata@naoj.org>
2017-11-16 16:29:36 -1000

    dvsh: default layer is changed to watch;

--- script/dvsh.rb
+++ script/dvsh.rb
@@ -4,7 +4,7 @@ require 'liblayer'
 require 'libhexlist'
 # CIAX-XML Device Shell
 module CIAX
-  Layer.new('[id]', options: 'fwxelrchs', default: 'a') do |cfg, args|
+  Layer.new('[id]', options: 'fwxelrchs', default: 'w') do |cfg, args|
     cfg[:opt].layer_mod::List.new(cfg, sites: args)
   end.ext_shell.shell
 end

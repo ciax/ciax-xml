514882fab27e2c7dd7f26188aa3c08f52f73aae9
Omata@leonid <omata@naoj.org>
2017-11-16 15:33:01 -1000

    input64: no need block by select(). readpartial() does it;

--- simulator/input64.rb
+++ simulator/input64.rb
@@ -2,12 +2,6 @@
 # Input from STDIN
 # Output to base64
 begin
-  select([STDIN])
-rescue Interrupt
-  warn 'Input64:SIGINT'
-  retry
-end
-begin
   puts [STDIN.readpartial(1024)].pack('m').split("\n").join('')
 rescue EOFError
   exit 1

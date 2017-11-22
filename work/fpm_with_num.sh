ed71e757a8b3175d42f7c2657ad9a9f08d3d7410
Omata@leonid <omata@naoj.org>
2017-11-22 12:19:55 -1000

    fpm_with_num: use -w option for grep instead of enclosing var with \b;

--- work/fpm_with_num.sh
+++ work/fpm_with_num.sh
@@ -15,7 +15,7 @@ while read method file; do
         else
             [[ $line =~ def ]] && odef=$(( odef + 1 )) || ouse=$(( ouse + 1 ))
         fi        
-    done < <(egrep \\b$method\\b $ARGV)
+    done < <(grep -w $method $ARGV)
     echo -e "$method\t$file\t$mdef/$muse $muse/$ouse"
 done < <(find_priv_methods -d $ARGV)|expand -t 20|sort
 echo -e "Method\tFile\tdef/use(self) def/use(others)"|expand -t 20

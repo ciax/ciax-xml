ff936acb4ce66119bb68676c460afbd9edbbb425
Omata@leonid <omata@naoj.org>
2017-12-07 14:52:58 -1000

    dvsh: accept -a option;

--- script/dvsh.rb
+++ script/dvsh.rb
@@ -4,7 +4,7 @@ require 'liblayer'
 require 'libhexlist'
 # CIAX-XML Device Shell
 module CIAX
-  Layer.new('[id]', options: 'fwxelrchs', default: 'w') do |cfg, args|
+  Layer.new('[id]', options: 'fawxelrchs', default: 'w') do |cfg, args|
     cfg[:opt].init_layer_mod::List.new(cfg, sites: args)
   end.ext_shell.shell
 end

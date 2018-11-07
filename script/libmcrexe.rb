ff2c799377719adbdd8df8d5719d7e747114dc22
Omata@leonid <omata@naoj.org>
2018-11-07 13:00:06 -1000

    Update: libmcrexe: @id is sid (prompt shows sid);

--- script/libmcrexe.rb
+++ script/libmcrexe.rb
@@ -11,14 +11,17 @@ module CIAX
       def initialize(super_cfg, &submcr_proc)
         super(super_cfg)
         verbose { 'Initiate New Macro' }
-        _init_dbi2cfg
-        @sv_stat = (@cfg[:sv_stat] ||= Prompt.new(@id, @opt))
         ___init_cmd
         ___init_seq(submcr_proc)
+        ___init_prompt
+      end
+
+      def fork
+        @thread = Msg.type?(@seq.fork, Threadx::Fork)
+        self
       end
 
       # Mode Extention by Option
-
       def ext_shell
         require 'libmcrsh'
         extend(Shell).ext_shell
@@ -34,16 +37,20 @@ module CIAX
         rem.add_sys
         @int = rem.add_int
         @cfg[:valid_keys] = @int.valid_keys.clear
-        @cobj.get('nonstop').def_proc { @sv_stat.up(:nonstop) }
-        @cobj.get('interactive').def_proc { @sv_stat.dw(:nonstop) }
       end
 
       def ___init_seq(submcr_proc)
         @seq = Sequencer.new(@cfg, &submcr_proc)
         @int.def_proc { |ent| @seq.reply(ent.id) }
-        @sv_stat.push(:list, @seq.id).repl(:sid, @seq.id)
         @stat = @seq.record
-        @thread = Msg.type?(@seq.fork, Threadx::Fork)
+        @id = @stat.id
+      end
+
+      def ___init_prompt
+        @sv_stat = type?(@cfg[:sv_stat], Prompt)
+        @cobj.get('nonstop').def_proc { @sv_stat.up(:nonstop) }
+        @cobj.get('interactive').def_proc { @sv_stat.dw(:nonstop) }
+        @sv_stat.push(:list, @seq.id).repl(:sid, @seq.id)
       end
     end
 
@@ -52,7 +59,7 @@ module CIAX
         mobj = Index.new(cfg)
         mobj.add_rem.add_ext.dev_list
         ent = mobj.set_cmd(args)
-        Exe.new(ent).ext_shell.shell
+        Exe.new(ent).ext_shell.fork.shell
       end
     end
   end

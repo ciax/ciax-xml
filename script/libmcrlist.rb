cdac529c9e92fc2089d79a49c90330377781a44a
Omata@leonid <omata@naoj.org>
2018-11-15 10:30:22 -1000

    Update: libmcrlist: add @sub_list for Layer;

--- script/libmcrlist.rb
+++ script/libmcrlist.rb
@@ -7,26 +7,13 @@ module CIAX
     # List for Running Macro
     class List < List
       attr_reader :cfg
-      # level can be Layer or Site
       # @cfg should have [:sv_stat]
-      # @cfg[:db] associated site/layer should be set
       def initialize(super_cfg, atrb = Hashx.new)
-        #      @cfg = super_cfg.gen(self).update(atrb)
-        #      @cfg[:jump_groups] ||= []
-        #      super(m2id(@cfg[:obj].class, -2))
-        #      verbose { 'Initiate List (option:' + @cfg[:opt].keys.join + ')' }
-        #      self[:list] = Hashx.new
         super
         @sv_stat = Msg.type?(@cfg[:sv_stat], Prompt)
+        @sub_list = @cfg[:dev_list]
         @cfg[:rec_arc].ext_local
         #        @man = self[:list]['0'] = Man.new(@cfg, mcr_list: self)
-      end
-
-      # this is separated for Daemon
-      # restart background threads which will be killed by Daemon
-      def run
-        @sub_list.run
-        self
       end
 
       # pid is Parent ID (user=0,mcr_id,etc.) which is source of command issued

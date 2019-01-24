aff2c5e4bfefe2acfb10f2350a1b8c8aba851252
CIAX <ciax@naoj.org>
2019-01-24 11:46:29 -1000

    Bugfix: setup-pkg: adapt to CentOs;

--- utils/setup-pkg.sh
+++ utils/setup-pkg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Required packages(Debian,Raspbian,Ubuntu): make gcc socat sqlite3 ruby ruby-all-dev ruby-libxml libxml2-utils apache2 libapache2-mod-php php-sqlite3 php-elisp socat libxml-xpath-perl
-# Required packages(CentOs): ruby-devel libxml2-devel httpd socat sqlite httpd php php-pear perl-XML-XPath
+# Required packages(CentOs): make gcc ruby-devel libxml2-devel httpd socat sqlite httpd php php-pear perl-XML-XPath
 # Required modules(Ruby): json libxml-ruby
 echo $C3"Install required packages"$C0
 if [ -f /etc/centos-release ]; then
-    dist=CentOS
+    dist=CentOs
 else
     read dist dmy < /etc/issue
 fi
@@ -13,13 +13,12 @@ case "$dist" in
     *bian|Ubuntu)
         sudo apt-get install $pkgs
         ;;
-    CentOS)
+    CentOs)
         sudo yum install $pkgs
         sudo gem install json libxml-ruby
         ;;
-    *)
-        return
+    *)   
         ;;
 esac
 # Install gem apps
-sudo gem install rubocop
+sudo gem install ox rubocop

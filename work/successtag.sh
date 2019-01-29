7166d0d3a497a6a8595c334396793935bc71978c
CIAX <ciax@naoj.org>
2019-01-28 16:45:02 -1000

    Update: successtag;

--- work/successtag.sh
+++ work/successtag.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-git tag "Success(${PROJ^^})@$HOSTNAME$(date +%Y%m%d)"
+# Add git tag for the version of successful operation
+# You can Specify date
+date=$(date ${1:+-d $1} +%Y/%m/%d)
+tag="Success!${PROJ^^}@$HOSTNAME($date)"
+git check-ref-format "$tag" || exit
+echo "$tag"
+git tag $tag
+git push --tag

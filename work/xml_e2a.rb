162a38c15d7a21bf055d5c819df8a2e66ba06119
Koji Omata(pingu) <koji.omata@gmail.com>
2019-03-05 07:09:00 -1000

    Add: xml_e2a;

---
+++ work/xml_e2a.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# XML Element to Attribute
+require 'optparse'
+require 'rexml/document'
+include REXML
+
+if ARGV.size < 2
+  abort <<EOF
+Usage: xml_e2a [xpath] [element] [attr] < xml
+       <str... -> <element attr="str"...
+EOF
+end
+
+xpath = ARGV.shift
+elem = ARGV.shift || abort('No element')
+attr = ARGV.shift || abort('No attr')
+doc = Document.new(gets(nil))
+doc.each_element(xpath) do |e0|
+  e0.each_element do |e|
+    str = e.name
+    e.name = elem
+    e.add_attribute(attr, str)
+  end
+end
+doc.write

8a61c5e140b27323de6debc2ee3643d79798c6d9
CIAX <ciax@naoj.org>
2019-03-12 16:48:39 -1000

    Update: gen2mkprt: adapt to source include to gen2prt;         gen2prt: use function of gen2mkprt;

--- utils/gen2mkprt.sh
+++ utils/gen2mkprt.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 # show oss print args
-case "$1" in
-    cfg)
-        cfg=~/drivers/config/cfg_tscst.txt
-        if [ -f $cfg ] ; then
-            echo `grep -v "^#" $cfg| cut -d, -f1`
-        else
-            echo "No CFG File"
-        fi
-        ;;
-    all)
-        echo CXWS.TSCV.TELDRIVE CXWS.TSCV.0_SENSOR CXWS.TSCV.POWER_V2 CXWS.TSCV.POWER_V1 CXWS.TSCV.CIAX_MLP3_FAULT CXWS.TSCS.EL CXWS.TSCS.INSROT TSCV.InsRotRotation TSCV.RotatorType TSCV.TSC.LOGIN0 TSCV.TSC.LOGIN1 TSCV.TSC.LOGIN2 TSCV.TSC.LOGIN3 CXWS.TSCV.SHUTTER CXWS.TSCV.STOW_1 CXWS.TSCL.Z_SENSOR
-        ;;
-    iid)
-        echo CXWS.TSCV.OBE_INR
-        ;;
-    login)
-        echo TSCV.TSC.LOGIN0 TSCV.TSC.LOGIN1 TSCV.TSC.LOGIN2 TSCV.TSC.LOGIN3
-        ;;
-    *)
-        echo "Usage: gen2prt (-c) [option]"
-        echo "       all, iid, login, cfg"
-        exit 1;;
-esac
+usage(){
+    echo "Usage: ${0##*/} [option]" > /dev/stderr
+    echo "       all, iid, login, cfg" > /dev/stderr
+    exit 1
+}
+
+selprt(){
+    case "$1" in
+        cfg)
+            cfg=~/drivers/config/cfg_tscst.txt
+            if [ -f $cfg ] ; then
+                echo `grep -v "^#" $cfg| cut -d, -f1`
+            else
+                echo "No CFG File"
+            fi
+            ;;
+        all)
+            echo CXWS.TSCV.TELDRIVE CXWS.TSCV.0_SENSOR CXWS.TSCV.POWER_V2 CXWS.TSCV.POWER_V1 CXWS.TSCV.CIAX_MLP3_FAULT CXWS.TSCS.EL CXWS.TSCS.INSROT TSCV.InsRotRotation TSCV.RotatorType TSCV.TSC.LOGIN0 TSCV.TSC.LOGIN1 TSCV.TSC.LOGIN2 TSCV.TSC.LOGIN3 CXWS.TSCV.SHUTTER CXWS.TSCV.STOW_1 CXWS.TSCL.Z_SENSOR
+            ;;
+        iid)
+            echo CXWS.TSCV.OBE_INR
+            ;;
+        login)
+            echo TSCV.TSC.LOGIN0 TSCV.TSC.LOGIN1 TSCV.TSC.LOGIN2 TSCV.TSC.LOGIN3
+            ;;
+        *) return 1;;
+    esac
+}
+[ ${BASH_SOURCE[0]} =  $0 ] && { selprt $* || usage; }
--- utils/gen2prt.sh
+++ utils/gen2prt.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
-. gen2env
-cfg=~/drivers/config/cfg_tscst.txt
-[ "$1" = -r ] && { shift; opt=-R; }
-case "$1" in
-    -c)
-        if [ -f $cfg ] ; then
-            $prt $opt `grep -v "^#" $cfg| cut -d, -f1`|visible
-        else
-            echo "No CFG File"
-        fi
-        ;;
-    all)
-        $prt $opt CXWS.TSCV.TELDRIVE CXWS.TSCV.0_SENSOR CXWS.TSCV.POWER_V2 CXWS.TSCV.POWER_V1 CXWS.TSCV.CIAX_MLP3_FAULT CXWS.TSCS.EL CXWS.TSCS.INSROT TSCV.InsRotRotation TSCV.RotatorType TSCV.TSC.LOGIN0 TSCV.TSC.LOGIN1 TSCV.TSC.LOGIN2 TSCV.TSC.LOGIN3 CXWS.TSCV.SHUTTER CXWS.TSCV.STOW_1 CXWS.TSCL.Z_SENSOR
-        ;;
-    iid)
-        $prt $opt CXWS.TSCV.OBE_INR
-        ;;
-    login)
-        $prt $opt TSCV.TSC.LOGIN0 TSCV.TSC.LOGIN1 TSCV.TSC.LOGIN2 TSCV.TSC.LOGIN3
-        ;;
-    "")
-        echo "Usage: gen2prt (-r,-c) [print]"
-        echo "    all,iid,login,(rawcmd)"
-        exit;;
-    *)
-        $prt -R $*
-        ;;
-esac
+source ~/gen2/conf/bashrc
+source gen2mkprt
+prt=$PYTHONPATH/SOSS/status/screenPrint.py
+[ "$1" ] || usage
+arg=$(selprt $*) && set - $arg || opt='-R'
+$prt $opt $*

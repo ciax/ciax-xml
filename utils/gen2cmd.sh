5eba181684410224ddb7d154e06cfb3dc8bf911d
CIAX <ciax@naoj.org>
2019-03-21 17:27:58 -1000

    Bugfix: gen2cmd should be run background when actuator moving (jack/rotator);

--- utils/gen2cmd.sh
+++ utils/gen2cmd.sh
@@ -3,4 +3,6 @@ source ~/gen2/conf/bashrc
 source gen2mkcmd
 cmd=$PYTHONPATH/Gen2/client/g2cmd.py
 arg=$(selcmd $*) || exit 1
-exelog $cmd $arg
+[ "${arg##* }" -gt 10 ] && opt=-b
+# Long term command should be done backgroup to update status
+exelog $opt $cmd $arg

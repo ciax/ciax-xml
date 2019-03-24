d07e57efdb94260bfb8cdfa1915cbfbd0903aad9
Koji Omata(pingu) <koji.omata@gmail.com>
2019-03-23 16:20:03 -1000

    Update: gen2exe: improve log appearance;

--- utils/gen2exe.sh
+++ utils/gen2exe.sh
@@ -8,40 +8,60 @@
 #link exe
 exit=~/.var/exit.txt
 pid=~/.var/pid.txt
-#gen2log=/dev/stdout
-gen2log=~/.var/gen2log.txt
+exelog=~/.var/gen2log.txt
 [ -f $exit ] || echo "0" > $exit
 [ -f $pid ] || touch $pid
-exelog(){
-    echo "[$(date +%D-%T)]"
-    echo "% $@ ($$)"
-    echo "$$" > $pid
+loghead(){
+    echo -en "[$(date +%D-%T)]% $@" >> $exelog
+}
+doexe(){
     eval $* 2>&1
     code="$?"
+}
+setexit(){
     echo "$code" > $exit
-    echo -e "[exitcode=$code]\n"
-    : > $pid #clear
+    echo " [exitcode=$code]" >> $exelog
+}
+bglog(){
+    loghead "$@" "(pid=$$)\n"
+    echo "$$" > $pid
+    code=0
+    bgexe "$@" &
+}
+bgexe(){
+    doexe "$@"
+    loghead "(pid=$$)"
+    setexit
+    > $pid
+}
+fglog(){
+    loghead "$@"
+    doexe "$@"
+    setexit
+    echo $code
 }
 reject(){
-    echo "Rejected" >&2
-    code=1
+    loghead "$@" "[Rejected by duplication!]\n"
+}
+# Check background running
+# Back ground task is alive when $pid is not empty
+chkbg(){
+    code=$(<$pid)
+    [ "$code" ] || code=0
+    return $code
 }
 case "$1" in
+    '') cat $exit;;
     -b)
         shift
         if [ "$1" ] ; then
-            if [ -s $pid ]; then
-                reject
-            else
-                exelog "$@" >> $gen2log & code=0
-            fi
-        elif [ -s $pid ] ; then
-            # Back ground task is alive when $pid is not empty
-            code=$(<$pid)
+            chkbg && bglog "$@" || reject "$@"
         else
-            code=0
-        fi;;
-    '') code=$(<$exit);;
-    *) exelog "$@" >> $gen2log;;
+            chkbg
+        fi
+        echo $code;;
+    -v) #For maintenance
+        cat $exelog
+        ;;
+    *) fglog "$@";;
 esac
-echo $code

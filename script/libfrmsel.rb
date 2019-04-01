778cb57f4fee06a589e5fc91232251cb845b0d6a
Koji Omata(pingu) <koji.omata@gmail.com>
2019-03-31 22:03:02 -1000

    Update: libfrmsel: works for noaffix but Hashx#attributes doesn't work;

--- script/libfrmsel.rb
+++ script/libfrmsel.rb
@@ -5,17 +5,15 @@ module CIAX
   # Frame Layer
   module Frm
     # Frame DB Selector
-    class Select < Upd
-      include Dic
+    class Select < Hashx
       def initialize(dbi)
         super()
-        ext_dic(:sel) { Hashx.new }
         # Ent is needed which includes response_id and cmd_parameters
         dbi = type?(dbi, Dbi)
         res = dbi[:response]
         frm = res[:frame]
-        tmp = [dbi, res, frm].inject(Hashx.new) { |h, e| h.update(e.attributes) }
-        ___make_sel(frm, res[:index], tmp)
+        tmp = [dbi, res, frm].inject(Hashx.new) { |h, e| h.merge(e.attributes) }
+        ___mk_sel(frm, res[:index], tmp)
       end
 
       def get(ent)
@@ -29,10 +27,30 @@ module CIAX
 
       # @sel structure:
       #   { terminator, :main{}, ccrange{}, :body{} <- changes on every upd }
-      def ___make_sel(dbe, index, tmp)
-        index.each do |id, val|
-          body = val[:body].map {|e| tmp.merge(e) } # Array
-          put(id, ___mk_main(dbe, body, tmp))
+      def ___mk_sel(dbe, index, tmp)
+        index.each do |id, item|
+          p item.attributes
+          put(id, ___mk_body(dbe, item, tmp.merge(item.attributes)))
+        end
+      end
+
+      def ___mk_body(dbe, item, tmp)
+        body = (item[:body] || []).map { |e| tmp.merge(e) } # Array
+        p body
+        p tmp
+        tmp[:noaffix] ? body : ___mk_main(dbe, body, tmp)
+      end
+
+      def ___mk_main(dbe, body, tmp)
+        dbe[:main].inject([]) do |a, e|
+          case e[:type]
+          when 'ccrange'
+            a << ___mk_ccr(dbe, body, tmp)
+          when 'body'
+            a.concat(body)
+          else
+            a << tmp.merge(e)
+          end
         end
       end
 
@@ -41,31 +59,6 @@ module CIAX
         dbe[:ccrange].inject([]) do |a, e|
           e[:type] == 'body' ? a.concat(body) : a << tmp.merge(e)
         end
-      end
-          
-      def ___mk_main(dbe, body, tmp)
-        dbe[:main].inject([]) do |a, e|
-          case e[:type]
-          when 'body'
-            a.concat(body)
-          when 'ccrange'
-            a << ___mk_ccr(dbe, body, tmp)
-          else
-            a << tmp.merge(e)
-          end
-        end
-      end
-
-
-      def ___make_data
-        @cache = _dic.deep_copy
-        if @sel.key?(:noaffix)
-          __getfield_rec(['body'])
-        else
-          __getfield_rec(@sel[:main])
-          @rspfrm.cc_check(@cache.delete('cc'))
-        end
-        _dic.replace(@cache)
       end
     end
 

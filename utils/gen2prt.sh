aac0a058769b97024f691b0cfa8149701c05b533
Omata@leonid <omata@naoj.org>
2019-05-23 10:19:29 -1000

    Update: gen2prt: comment update;

--- utils/gen2prt.sh
+++ utils/gen2prt.sh
@@ -3,5 +3,7 @@ source ~/gen2/conf/bashrc
 source gen2mkprt
 prt=$PYTHONPATH/SOSS/status/screenPrint.py
 [ "$1" ] || usage
+# Parameter: all, iid ... -> Visible mode
+# parameter: CXWS.OSS ... -> Raw mode
 arg=$(selprt $*) && set - $arg || opt='-R'
 $prt $opt $*

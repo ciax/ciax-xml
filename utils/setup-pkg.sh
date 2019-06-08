a3531c6fcaccb65c692905465a3a83d68cdecf01
Koji Omata <koji.omata@gmail.com>
2019-06-08 18:50:02 +0000

    Modify: setup-pkg: pick pkg names from all files in same dir;

--- utils/setup-pkg.sh
+++ utils/setup-pkg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Required packages(Debian,Raspbian,Ubuntu): make gcc socat sqlite3 ruby ruby-all-dev ruby-libxml libxml2-utils apache2 libapache2-mod-php php-sqlite3 php-elisp socat libxml-xpath-perl
+# Required packages(Debian,Raspbian,Ubuntu): make gcc socat sqlite3 ruby ruby-all-dev ruby-libxml libxml2-utils apache2 libapache2-mod-php5 php5-sqlite php-elisp socat libxml-xpath-perl
 # Required packages(CentOs): make gcc ruby-devel libxml2-devel httpd socat sqlite httpd php php-pear php-pdo perl-XML-XPath
 # Required modules(Ruby): json libxml-ruby
 echo $C3"Install required packages"$C0
@@ -8,7 +8,8 @@ if [ -f /etc/centos-release ]; then
 else
     read dist dmy < /etc/issue
 fi
-pkgs=$(grep "Required packages(*.$dist" $0|cut -d: -f2)
+here=$(dirname $0)
+pkgs=$(grep "Required packages(*.$dist" $here/*|cut -d: -f3)
 case "$dist" in
     *bian|Ubuntu)
         sudo apt-get install $pkgs

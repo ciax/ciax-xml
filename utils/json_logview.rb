df080216b8552a2b34ff627347f04f9210d28564
CIAX <ciax@naoj.org>
2019-07-15 15:54:24 -1000

    Bugfix: json_logview: was not worked;

--- utils/json_logview.rb
+++ utils/json_logview.rb
@@ -7,15 +7,16 @@ require 'json'
 abort 'Usage: json_logview json_log' if STDIN.tty? && ARGV.empty?
 readlines.each do |line|
   next if line.to_s.empty?
-
-  hash = JSON.parse(line.chomp, symbolize_names: true)
-  hash.each do |k, v|
+  hash = {}
+  JSON.parse(line.chomp, symbolize_names: true).each do |k, v|
     case k
     when :time
       hash[:time] = Time.at(v / 1000, (v % 1000) * 1000).strftime('%Y-%m-%d %T,%L')
     when :base64
       data = v.unpack('m').first
       hash[:data] = "#{data.inspect}(#{data.size})"
+    else
+      hash[k] = v
     end
   end
   puts JSON.dump(hash)

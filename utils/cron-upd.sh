3f762dec90c56780b9450701c7c875a776449d2a
moircs@moircsobcp <moircs@naoj.org>
2019-08-15 11:40:33 -1000

    Update: cron-upd: logging feature (~/.var/log/cron.log);

--- utils/cron-upd.sh
+++ utils/cron-upd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source $HOME/bin/rc.bash.ciax-xml 
+clog=$HOME/.var/log/cron.log
+date >> $clog
 for i; do
-    $HOME/bin/dvexe -e $i upd >/dev/null 2>&1
+    $HOME/bin/dvexe -e $i upd >> $clog 2>&1
 done

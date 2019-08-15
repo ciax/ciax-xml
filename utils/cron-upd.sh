5f330eb97db51bef2a3708195a8525a19c2818e5
moircs@moircsobcp <moircs@naoj.org>
2019-08-15 12:10:30 -1000

    Update: cron-upd: add getting env feature. add $HOSTNAME setting;

--- utils/cron-upd.sh
+++ utils/cron-upd.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-source $HOME/bin/rc.bash.ciax-xml 
 clog=$HOME/.var/log/cron.log
+[ "$1" ] || { cat $clog; exit; }
+[ -s $clog -o -t 0 ] || env > $clog  
+HOSTNAME=$(hostname)
+source $HOME/bin/rc.bash.ciax-xml 
 date >> $clog
 for i; do
     $HOME/bin/dvexe -e $i upd >> $clog 2>&1
 done
-[ -t 0 ] && cat $clog

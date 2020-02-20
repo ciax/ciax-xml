3a990f2a964176e2ea25a7a0168230208aa78ae4
Omata@leonid <omata@naoj.org>
2020-02-19 17:15:37 -1000

    Update: libmcrrem: redundant valid_key setting was removed;

--- script/libmcrrem.rb
+++ script/libmcrrem.rb
@@ -62,15 +62,8 @@ module CIAX
         def ___init_stat
           sid = @sv_stat.send(@cfg[:cid]).get(:sid)
           @stat = Record.new(sid)
-          @stat.upd_procs.append(self, 'remote') do
-            @valid_keys.replace(@stat[:option].to_a)
-          end
-          ___init_qry(sid)
-        end
-
-        def ___init_qry(sid)
           @int.pars.add_num(sid)
-          @qry = Query.new(@stat, @sv_stat, @valid_keys) do |cid|
+          @qry = Query.new(@stat, @sv_stat, @int) do |cid|
             @sv_stat.send(format('%s:%s', cid, sid))
           end
         end

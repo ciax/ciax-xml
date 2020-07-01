2f29efb00a076fbe4ccc6fb92794c4ee441107f4
moircs@moircsobcp <moircs@naoj.org>
2020-07-01 03:19:46 -1000

    Update: chk_timing: hilight word of 'macro';

--- work/chk_timing.sh
+++ work/chk_timing.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 sort ~/.var/log/{input_app,server_site}*_${1:-mix}_2020.log |\
 tail -1500|\
-EXCLUDE=id,format_ver,data_ver,host,comerr,ioerr,auto,pri,msg json_logview
+EXCLUDE=id,format_ver,data_ver,host,comerr,ioerr,auto,pri,msg json_logview|\
+grep --color=auto -e '$' -e macro

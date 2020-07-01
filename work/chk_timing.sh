7d19319d2585ba56fccc20c4ae07a253952fbd88
moircs@moircsobcp <moircs@naoj.org>
2020-07-01 03:50:19 -1000

    Update: chk_timing: add event data;

--- work/chk_timing.sh
+++ work/chk_timing.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sort ~/.var/log/{input_app,server_site}*_${1:-mix}_2020.log |\
+sort ~/.var/log/{event,input_app,server_site}*_${1:-mix}_2020.log |\
 tail -1500|\
-EXCLUDE=id,format_ver,data_ver,host,comerr,ioerr,auto,pri,msg json_logview|\
-grep --color=auto -e '$' -e macro
+EXCLUDE=id,format_ver,data_ver,host,comerr,ioerr,auto,pri,msg,act_time,upd_next,block,int \
+json_logview| grep --color=auto -e '$' -e macro
